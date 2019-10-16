fea8a623d52eb924c5b9a31bfb1d299146ff1ed2
Jeffrey Massung <massung@gmail.com>
2019-10-16 16:11:44 -0500

    Fixed filename of genefinder.html.

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  devServer: {
-    writeToDisk: true // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
-  },
-  pages: {
-    manhattan: {
-      entry: "src/views/Manhattan/main.js",
-      template: "public/index.html",
-      filename: "manhattan.html",
-      title: "Manhattan Page",
-      chunks: ["chunk-vendors", "chunk-common", "manhattan"]
+    devServer: {
+        writeToDisk: true // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
     },
-    geneFinder: {
-      entry: "src/views/GeneFinder/main.js",
-      template: "public/index.html",
-      filename: "geneFinder.html",
-      title: "Gene Finder",
-      chunks: ["chunk-vendors", "chunk-common", "geneFinder"]
+    pages: {
+        manhattan: {
+            entry: "src/views/Manhattan/main.js",
+            template: "public/index.html",
+            filename: "manhattan.html",
+            title: "Manhattan Page",
+            chunks: ["chunk-vendors", "chunk-common", "manhattan"]
+        },
+        geneFinder: {
+            entry: "src/views/GeneFinder/main.js",
+            template: "public/index.html",
+            filename: "genefinder.html",
+            title: "Gene Finder",
+            chunks: ["chunk-vendors", "chunk-common", "geneFinder"]
+        }
     }
-  }
 };
