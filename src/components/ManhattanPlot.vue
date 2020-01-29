5c86ad30e534086346db7a43e39f05e28afde093
Jeffrey Massung <massung@gmail.com>
2020-01-29 10:41:42 -0600

    Fixed PR comments.

--- src/components/ManhattanPlot.vue
+++ src/components/ManhattanPlot.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="chart"></div>
+    <div id="chart"></div>
 </template>
 
 <script>
@@ -7,141 +7,141 @@ import Vue from "vue";
 import c3 from "c3";
 
 export default Vue.component("manhattan-plot", {
-  props: ["variants", "dataset", "phenotype"],
+    props: ["variants", "dataset", "phenotype"],
 
-  data() {
-    return {
-      chart: null
-    };
-  },
+    data() {
+        return {
+            chart: null
+        };
+    },
 
-  mounted() {
-    this.chart = c3.generate({
-      size: {
-        height: 500
-      },
-      data: {
-        xs: {
-          data: "data_x"
-        },
-        type: "scatter",
-        columns: []
-      },
-      zoom: {
-        enabled: false,
-        rescale: false
-      },
-      point: {
-        r: 5
-      },
-      bindto: "#chart",
-      axis: {
-        x: {
-          label: "Chromosome",
-          tick: {
-            format(pos) {
-              for (let chrom in chromosomeStart) {
-                if (chromosomeStart[chrom] == pos) {
-                  return chrom;
-                }
-              }
+    mounted() {
+        this.chart = c3.generate({
+            size: {
+                height: 500
             },
-            values: chromosomes.map(c => chromosomeStart[c])
-          }
-        },
-        y: {
-          label: "-log10 P"
-        }
-      }
-    });
-  },
-
-  watch: {
-    variants(variants) {
-      let xs = ["data_x"];
-      let ys = ["data"];
-
-      variants.forEach(v => {
-        let p_value = v[4][this.dataset][this.phenotype.phenotype_id];
-        let pos = chromosomeStart[v[1]] + v[2];
-        xs.push(pos); // Pos
-        ys.push(-Math.log10(p_value)); // P_VALUE
-      });
-
-      if (variants.length == 0) {
-        this.chart.unload({});
-      } else {
-        this.chart.load({
-          columns: [xs, ys],
-          names: {
-            data: this.dataset
-          }
+            data: {
+                xs: {
+                    data: "data_x"
+                },
+                type: "scatter",
+                columns: []
+            },
+            zoom: {
+                enabled: false,
+                rescale: false
+            },
+            point: {
+                r: 5
+            },
+            bindto: "#chart",
+            axis: {
+                x: {
+                    label: "Chromosome",
+                    tick: {
+                        format(pos) {
+                            for (let chrom in chromosomeStart) {
+                                if (chromosomeStart[chrom] == pos) {
+                                    return chrom;
+                                }
+                            }
+                        },
+                        values: chromosomes.map(c => chromosomeStart[c])
+                    }
+                },
+                y: {
+                    label: "-log10 P"
+                }
+            }
         });
-      }
+    },
+
+    watch: {
+        variants(variants) {
+            let xs = ["data_x"];
+            let ys = ["data"];
+
+            variants.forEach(v => {
+                let p_value = v[4][this.dataset][this.phenotype.phenotype_id];
+                let pos = chromosomeStart[v[1]] + v[2];
+                xs.push(pos); // Pos
+                ys.push(-Math.log10(p_value)); // P_VALUE
+            });
+
+            if (variants.length == 0) {
+                this.chart.unload({});
+            } else {
+                this.chart.load({
+                    columns: [xs, ys],
+                    names: {
+                        data: this.dataset
+                    }
+                });
+            }
+        }
     }
-  }
 });
 
 let chromosomeLength = {
-  //chromosome name, length
-  "1": 247249719,
-  "2": 242951149,
-  "3": 199501827,
-  "4": 191273063,
-  "5": 180857866,
-  "6": 170899992,
-  "7": 158821424,
-  "8": 146274826,
-  "9": 140273252,
-  "10": 135374737,
-  "11": 134452384,
-  "12": 132349534,
-  "13": 114142980,
-  "14": 106368585,
-  "15": 100338915,
-  "16": 88827254,
-  "17": 78774742,
-  "18": 76117153,
-  "19": 63811651,
-  "20": 62435964,
-  "21": 46944323,
-  "22": 49691432,
-  X: 154913754,
-  Y: 57772954
+    //chromosome name, length
+    "1": 247249719,
+    "2": 242951149,
+    "3": 199501827,
+    "4": 191273063,
+    "5": 180857866,
+    "6": 170899992,
+    "7": 158821424,
+    "8": 146274826,
+    "9": 140273252,
+    "10": 135374737,
+    "11": 134452384,
+    "12": 132349534,
+    "13": 114142980,
+    "14": 106368585,
+    "15": 100338915,
+    "16": 88827254,
+    "17": 78774742,
+    "18": 76117153,
+    "19": 63811651,
+    "20": 62435964,
+    "21": 46944323,
+    "22": 49691432,
+    X: 154913754,
+    Y: 57772954
 };
 
 let chromosomes = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "X",
-  "Y"
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "X",
+    "Y"
 ];
 
 let chromosomeStart = {};
 let start = 0;
 for (let i in chromosomes) {
-  let chrom = chromosomes[i];
-  chromosomeStart[chrom] = start;
-  start += chromosomeLength[chrom];
+    let chrom = chromosomes[i];
+    chromosomeStart[chrom] = start;
+    start += chromosomeLength[chrom];
 }
 </script>
--- src/views/Gene/Template.vue
+++ src/views/Gene/Template.vue
@@ -4,135 +4,139 @@
         <page-header></page-header>
         <!-- body -->
         <div class="container-fluid mdkp-body">
-        <div class="gene-page-header card mdkp-card">
-            <div class="row card-body">
-                <div class="col-md-4 gene-page-header-title">
-                    Genes in the region associated with phenotypes with signal
-                    <a
-                        class="edit-btn"
-                        onclick="mdkp.utility.showHideElement('geneSearchHolder');"
-                    >Edit</a>
-                </div>
-                <div class="col-md-4 gene-page-header-title">
-                    Chromosome: Start position - End position
-                    <a
-                        class="edit-btn"
-                        onclick="mdkp.utility.showHideElement('regionSearchHolder');"
-                    >Edit</a>
-                </div>
-                <div class="col-md-4 gene-page-header-title">
-                    Phenotype
-                    <a
-                        class="edit-btn"
-                        onclick="mdkp.utility.showHideElement('phenotypeSearchHolder');"
-                    >Edit</a>
-                </div>
-                <div class="col-md-4 gene-page-header-body">
-                    <div
-                        id="geneSearchHolder"
-                        class="gene-page-header-search-holder"
-                        style="display: none;"
-                    >Genes UI</div>
-                    <span v-for="row in $parent.genesInRegion">{{row}},</span>
-                </div>
-                <div class="col-md-4 gene-page-header-body regionInfo">
-                    <div
-                        id="regionSearchHolder"
-                        class="gene-page-header-search-holder"
-                        style="display: none;"
-                    >
-                        <div class="region-search">
-                            <div class="col-md-2 input-wrapper">
-                                <input
-                                    v-model="$store.state.newChrom"
-                                    type="text"
-                                    class="form-control input-default"
-                                />
-                            </div>
-                            <div class="col-md-4 input-wrapper">
-                                <input
-                                    v-model="$store.state.newStart"
-                                    type="text"
-                                    class="form-control input-default"
-                                />
-                            </div>
-                            <div class="col-md-4 input-wrapper">
-                                <input
-                                    v-model="$store.state.newEnd"
-                                    type="text"
-                                    class="form-control input-default"
-                                />
-                            </div>
-                            <div class="col-md-2 input-wrapper">
-                                <button
-                                    id="regionSearchGo"
-                                    class="btn btn-primary"
-                                    type="button"
-                                    @click="$store.dispatch('setLocation')"
-                                >GO</button>
+            <div class="gene-page-header card mdkp-card">
+                <div class="row card-body">
+                    <div class="col-md-4 gene-page-header-title">
+                        Genes in the region associated with phenotypes with signal
+                        <a
+                            class="edit-btn"
+                            onclick="mdkp.utility.showHideElement('geneSearchHolder');"
+                        >Edit</a>
+                    </div>
+                    <div class="col-md-4 gene-page-header-title">
+                        Chromosome: Start position - End position
+                        <a
+                            class="edit-btn"
+                            onclick="mdkp.utility.showHideElement('regionSearchHolder');"
+                        >Edit</a>
+                    </div>
+                    <div class="col-md-4 gene-page-header-title">
+                        Phenotype
+                        <a
+                            class="edit-btn"
+                            onclick="mdkp.utility.showHideElement('phenotypeSearchHolder');"
+                        >Edit</a>
+                    </div>
+                    <div class="col-md-4 gene-page-header-body">
+                        <div
+                            id="geneSearchHolder"
+                            class="gene-page-header-search-holder"
+                            style="display: none;"
+                        >Genes UI</div>
+                        <span v-for="row in $parent.genesInRegion">{{row}},</span>
+                    </div>
+                    <div class="col-md-4 gene-page-header-body regionInfo">
+                        <div
+                            id="regionSearchHolder"
+                            class="gene-page-header-search-holder"
+                            style="display: none;"
+                        >
+                            <div class="region-search">
+                                <div class="col-md-2 input-wrapper">
+                                    <input
+                                        v-model="$store.state.newChrom"
+                                        type="text"
+                                        class="form-control input-default"
+                                    />
+                                </div>
+                                <div class="col-md-4 input-wrapper">
+                                    <input
+                                        v-model="$store.state.newStart"
+                                        type="text"
+                                        class="form-control input-default"
+                                    />
+                                </div>
+                                <div class="col-md-4 input-wrapper">
+                                    <input
+                                        v-model="$store.state.newEnd"
+                                        type="text"
+                                        class="form-control input-default"
+                                    />
+                                </div>
+                                <div class="col-md-2 input-wrapper">
+                                    <button
+                                        id="regionSearchGo"
+                                        class="btn btn-primary"
+                                        type="button"
+                                        @click="$store.dispatch('setLocation')"
+                                    >GO</button>
+                                </div>
                             </div>
                         </div>
+                        {{$store.state.chrom}}:{{$store.state.start}} - {{$store.state.chrom}}:{{$store.state.end}}
                     </div>
-                    {{$store.state.chrom}}:{{$store.state.start}} - {{$store.state.chrom}}:{{$store.state.end}}
+                    <div class="col-md-4 gene-page-header-body">
+                        <div
+                            id="phenotypeSearchHolder"
+                            class="gene-page-header-search-holder"
+                            style="display: none;"
+                        >
+                            <phenotype-selectpicker v-bind:phenotypes="$parent.phenotypeMap"></phenotype-selectpicker>
+                        </div>
+                        <span>{{$store.state.phenotypeName}}</span>
+                    </div>
                 </div>
-                <div class="col-md-4 gene-page-header-body">
+            </div>
+
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <h4
+                        class="card-title"
+                    >Phenotypes with signal in {{$store.state.chrom}}:{{$store.state.start}} - {{$store.state.chrom}}:{{$store.state.end}}</h4>
                     <div
-                        id="phenotypeSearchHolder"
-                        class="gene-page-header-search-holder"
-                        style="display: none;"
-                    >
-                        <phenotype-selectpicker v-bind:phenotypes="$parent.phenotypeMap"></phenotype-selectpicker>
-                    </div>
-                    <span>{{$store.state.phenotypeName}}</span>
+                        @click="$store.commit('setPhenotype',{id:row.phenotype,phenotypes:$parent.phenotypeMap})"
+                        v-for="(row, itemObjKey) in $parent.phenotypesData"
+                        :class="row.P_VALUE < 2.5e-6 ? 'phenotype-with-signal high' : 'phenotype-with-signal moderate'"
+                        :title="row.P_VALUE"
+                        :slot-scope="row"
+                    >{{row.name}}</div>
+
+                    <h4
+                        class="card-title"
+                    >Genes in the region associated with phenotypes with signal</h4>
+                    <div
+                        v-for="row in $parent.genesInRegion"
+                        class="phenotype-with-signal moderate"
+                    >{{row}}</div>
                 </div>
             </div>
-        </div>
 
-        <div class="card mdkp-card">
-            <div class="card-body">
-                <h4
-                    class="card-title"
-                >Phenotypes with signal in {{$store.state.chrom}}:{{$store.state.start}} - {{$store.state.chrom}}:{{$store.state.end}}</h4>
-                <div
-                    @click="$store.commit('setPhenotype',{id:row.phenotype,phenotypes:$parent.phenotypeMap})"
-                    v-for="(row, itemObjKey) in $parent.phenotypesData"
-                    :class="row.P_VALUE < 2.5e-6 ? 'phenotype-with-signal high' : 'phenotype-with-signal moderate'"
-                    :title="row.P_VALUE"
-                    :slot-scope="row"
-                >{{row.name}}</div>
-
-                <h4 class="card-title">Genes in the region associated with phenotypes with signal</h4>
-                <div
-                    v-for="row in $parent.genesInRegion"
-                    class="phenotype-with-signal moderate"
-                >{{row}}</div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <h5 class="card-title">LocusZoom</h5>
+                    <locuszoom
+                        ref="lz"
+                        v-bind:gene="$parent.geneSource"
+                        v-bind:constraint="$parent.constraintSource"
+                        v-bind:ld="$parent.ldSource"
+                        v-bind:recomb="$parent.recombSource"
+                        v-bind:assoc="['StaticJSON', { data: [] }]"
+                        v-bind:panels="['association','genes','intervals']"
+                        v-bind:chrom="$store.state.chrom"
+                        v-bind:start="$store.state.start"
+                        v-bind:end="$store.state.end"
+                        v-bind:intervals="$parent.intervalsSource"
+                    ></locuszoom>
+                </div>
             </div>
-        </div>
-
-        <div class="card mdkp-card">
-            <div class="card-body">
-                <h5 class="card-title">LocusZoom</h5>
-                <locuszoom
-                    ref="lz"
-                    v-bind:gene="$parent.geneSource"
-                    v-bind:constraint="$parent.constraintSource"
-                    v-bind:ld="$parent.ldSource"
-                    v-bind:recomb="$parent.recombSource"
-                    v-bind:assoc="['StaticJSON', { data: [] }]"
-                    v-bind:panels="['association','genes','intervals']"
-                    v-bind:chrom="$store.state.chrom"
-                    v-bind:start="$store.state.start"
-                    v-bind:end="$store.state.end"
-                    v-bind:intervals="$parent.intervalsSource"
-                ></locuszoom>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <h4
+                        class="card-title"
+                    >Top variants for Phenotype: {{$store.state.phenotypeName}}</h4>
+                    <variants-table v-bind:variants="$parent.variantsData"></variants-table>
+                </div>
             </div>
-        </div>
-        <div class="card mdkp-card">
-            <div class="card-body">
-                <h4 class="card-title">Top variants for Phenotype: {{$store.state.phenotypeName}}</h4>
-                <variants-table v-bind:variants="$parent.variantsData"></variants-table>
-            </div>
-        </div>
         </div>
         <page-footer></page-footer>
     </div>
