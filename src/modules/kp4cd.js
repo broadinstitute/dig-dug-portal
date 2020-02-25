ff37e783bb834b27b2b3fb0dbc8869ad51843cf4
DK Jang <dkjang@broadinstitute.org>
2020-02-25 15:26:03 -0500

    kp4cd.js module modified to simplify portal id exceptions handling.

--- src/modules/kp4cd.js
+++ src/modules/kp4cd.js
@@ -13,6 +13,13 @@ export default {
             newsFeed: [],
             frontContents: [],
             datasetsInfo: [],
+            portalID: {
+                "md": "md",
+                "cvd": "mi",
+                "cd": "stroke",
+                "t2d": "t2d",
+                "sleep": "sleep"
+            }
         };
     },
 
@@ -32,21 +39,21 @@ export default {
     // dispatch methods
     actions: {
         async getNewsFeed(context, selecteddiseaseGroup) {
-            let diseaseGroup = (selecteddiseaseGroup == "md") ? "" : (selecteddiseaseGroup == "cvd") ? "mi" : (selecteddiseaseGroup == "cd") ? "stroke" : selecteddiseaseGroup;
+            let diseaseGroup = (selecteddiseaseGroup == "md") ? "" : context.state.portalID[selecteddiseaseGroup];
             let json = await fetch(`http://kp4cd.org/rest/views/news2portals?portal=` + diseaseGroup)
                 .then(resp => resp.json());
             // set the data
             context.commit('setNewsFeed', json)
         },
         async getFrontContents(context, selecteddiseaseGroup) {
-            let diseaseGroup = (selecteddiseaseGroup == "cvd") ? "mi" : (selecteddiseaseGroup == "cd") ? "stroke" : selecteddiseaseGroup;
+            let diseaseGroup = context.state.portalID[selecteddiseaseGroup];
             let json = await fetch(`http://kp4cd.org/reset/views/portal_front?portal=` + diseaseGroup)
                 .then(resp => resp.json());
             // set the data
             context.commit('setFrontContents', json)
         },
         async getDatasetsInfo(context, selecteddiseaseGroup) {
-            let diseaseGroup = (selecteddiseaseGroup == "md") ? "" : (selecteddiseaseGroup == "cvd") ? "mi" : (selecteddiseaseGroup == "cd") ? "stroke" : selecteddiseaseGroup;
+            let diseaseGroup = (selecteddiseaseGroup == "md") ? "" : context.state.portalID[selecteddiseaseGroup];
             let json = await fetch(`http://kp4cd.org/rest/views/kpdatasets?portal=` + diseaseGroup)
                 .then(resp => resp.json());
             // set the data
