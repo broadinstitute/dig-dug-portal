7546be0147415a6f3c05375d59e177238459fdf4
Quy <quy@broad.dev>
2020-04-21 15:43:50 -0400

    simple, generic component to display error message
    with dismissible/close option

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<b-alert show fade variant="danger" :dismissible="dismissible">
+			<strong>Error:</strong>
+			{{error}}
+		</b-alert>
+	</div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.component("error-message", {
+	props: ["error", "dismissible"],
+	data() {
+		return {
+			dismissible: false
+		};
+	}
+});
+</script>
