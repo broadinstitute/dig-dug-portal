a4a0765f724fcb39e14756748094a6f16ae5c041
Quy <quy@broad.dev>
2020-04-26 11:41:09 -0400

    add check for nonempty error array

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -1,22 +1,23 @@
 <template>
-	<div v-if="errors">
-		{{errors}}
-		<button @click="errorNew(Math.random())">new error</button>
-		<b-alert
-			v-model="show"
-			fade
-			variant="danger"
-			:dismissible="dismissible"
-			@dismissed="errorClear"
-			@dismiss-count-down="countDownChanged"
-		>
-			<strong>We've encountered the following {{errorWord}}:</strong>
-			<span class="countDown" v-if="timeout">{{dismissCountDown}}</span>
-			<ul>
-				<li v-for="(error, i) in errors" :key="i">{{error}}</li>
-			</ul>
-		</b-alert>
-	</div>
+    <div v-if="errors && errors.length > 0">
+        {{errors}}
+        <button @click="errorNew(Math.random())">new error</button>
+
+        <b-alert
+            v-model="show"
+            fade
+            variant="danger"
+            :dismissible="dismissible"
+            @dismissed="errorClear"
+            @dismiss-count-down="countDownChanged"
+        >
+            <strong>We've encountered the following {{errorWord}}:</strong>
+            <span class="countDown" v-if="timeout">{{dismissCountDown}}</span>
+            <ul>
+                <li v-for="(error, i) in errors" :key="i">{{error}}</li>
+            </ul>
+        </b-alert>
+    </div>
 </template>
 
 <script>
@@ -25,43 +26,43 @@ import pluralize from "pluralize";
 import EventBus from "@/utils/eventBus";
 
 export default Vue.component("error-message", {
-	props: ["dismissible", "timeout"],
-	data() {
-		return {
-			show: false,
-			dismissCountDown: 0,
-			errors: []
-			//errors: ["error1", "error2"]
-		};
-	},
-	mounted() {
-		EventBus.$on("NEW_ERROR", this.errorNew);
-	},
-	methods: {
-		countDownChanged(dismissCountDown) {
-			this.dismissCountDown = dismissCountDown;
-		},
-		errorNew(error) {
-			this.errors.push(error);
-			this.show = this.timeout || true;
-		},
-		errorClear() {
-			this.errors = [];
-		}
-	},
-	computed: {
-		errorWord() {
-			return pluralize("error", this.errors.length);
-		}
-	}
+    props: ["dismissible", "timeout"],
+    data() {
+        return {
+            show: false,
+            dismissCountDown: 0,
+            errors: []
+            //errors: ["error1", "error2"]
+        };
+    },
+    mounted() {
+        EventBus.$on("NEW_ERROR", this.errorNew);
+    },
+    methods: {
+        countDownChanged(dismissCountDown) {
+            this.dismissCountDown = dismissCountDown;
+        },
+        errorNew(error) {
+            this.errors.push(error);
+            this.show = this.timeout || true;
+        },
+        errorClear() {
+            this.errors = [];
+        }
+    },
+    computed: {
+        errorWord() {
+            return pluralize("error", this.errors.length);
+        }
+    }
 });
 </script>
 <style scoped>
 .countDown {
-	float: right;
-	font-size: 1.5rem;
-	font-weight: 700;
-	color: rgba(255, 255, 255, 0.8);
-	line-height: 1;
+    float: right;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: rgba(255, 255, 255, 0.8);
+    line-height: 1;
 }
 </style>
