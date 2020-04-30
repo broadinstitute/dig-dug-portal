388e942f8cd370ca2c9df4d52ad332f989284928
Quy <quy@broad.dev>
2020-04-30 13:43:30 -0400

    error is show as alert instead of console log

--- src/modules/bioIndex.js
+++ src/modules/bioIndex.js
@@ -120,7 +120,7 @@ export default function(index, extend) {
                                 context.commit("setProgress", json.progress);
                             },
                             errHandler: error => {
-                                console.log(error.message);
+                                postAlertError(error.message);
                             }
                         }
                     );
