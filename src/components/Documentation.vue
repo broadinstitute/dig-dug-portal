c405b21d7f0d9573d7d4a02b4c355086052e85e8
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-05-14 14:52:41 -0700

    guard against no name

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -23,67 +23,69 @@ export default Vue.component("documentation", {
     },
 
     created() {
-        // fetch the documentation data and resolve it in data
-        let docGroup = this.group || this.$store.getters["bioPortal/diseaseGroup"].name || "md";
-        let qs = queryString.stringify({
-            q: this.name,
-            group: docGroup  //get this from state
-        });
-        let json = fetch(`${BIO_INDEX_HOST}/api/portal/documentation?${qs}`)
-            .then(resp => {
-                if (resp.status === 422) {
-                    throw Error("missing parameters");
-                    // throw Error("In Documentation"+' '+resp.json().detail[0].type+' '+resp.json().detail[0].msg+' '+resp.json().detail[0].loc);
-                }
-                if (resp.status === 200) {
-                    return resp;
-                }
-            })
-            .then(resp => resp.json())
-            .then(json => {
-                if (json.data.length > 0) {
-
-                    const classMap = {
-                        h1: 'doc large-header',
-                        h2: 'doc medium-header',
-                        h3: 'doc small-header',
-                        h4: 'doc x-small-header',
-                        p:  'doc content',
-                        ul: 'doc list',
-                        li: 'doc item',
-                        em: 'doc italic',
-                        strong: 'doc bold',
-                        a: 'doc link',
-                    }
-
-                    const name_and_class_extensions = Object.keys(classMap)
-                        .map(key => ({
-                            type: 'output',
-                            regex: new RegExp(`<${key}(.*)>`, 'g'),
-                            replace: `<${key} id="${this.name}" class="${classMap[key]}" $1>`
-                        }));
-
-                    const valid_tags = this.findTemplateTagsFromContent(
-                        json.data[0].content
-                    );
-                    const fill_extensions = this.makeExtensions(
-                        this.content_fill,
-                        valid_tags
-                    );
-
-                    this.converter = new showdown.Converter({
-                        extensions: [...fill_extensions, ...name_and_class_extensions]
-                    });
-
-                    this.content = json.data[0].content;
-
-                } else {
-                    throw new Error("No content returned for given name " +
-                            this.name +
-                            " and group " +
-                    docGroup)
-                }
+        if (!!this.name) {
+            // fetch the documentation data and resolve it in data
+            let docGroup = this.group || this.$store.getters["bioPortal/diseaseGroup"].name || "md";
+            let qs = queryString.stringify({
+                q: this.name,
+                group: docGroup  //get this from state
             });
+            let json = fetch(`${BIO_INDEX_HOST}/api/portal/documentation?${qs}`)
+                .then(resp => {
+                    if (resp.status === 422) {
+                        throw Error("missing parameters");
+                        // throw Error("In Documentation"+' '+resp.json().detail[0].type+' '+resp.json().detail[0].msg+' '+resp.json().detail[0].loc);
+                    }
+                    if (resp.status === 200) {
+                        return resp;
+                    }
+                })
+                .then(resp => resp.json())
+                .then(json => {
+                    if (json.data.length > 0) {
+
+                        const classMap = {
+                            h1: 'doc large-header',
+                            h2: 'doc medium-header',
+                            h3: 'doc small-header',
+                            h4: 'doc x-small-header',
+                            p:  'doc content',
+                            ul: 'doc list',
+                            li: 'doc item',
+                            em: 'doc italic',
+                            strong: 'doc bold',
+                            a: 'doc link',
+                        }
+
+                        const name_and_class_extensions = Object.keys(classMap)
+                            .map(key => ({
+                                type: 'output',
+                                regex: new RegExp(`<${key}(.*)>`, 'g'),
+                                replace: `<${key} id="${this.name}" class="${classMap[key]}" $1>`
+                            }));
+
+                        const valid_tags = this.findTemplateTagsFromContent(
+                            json.data[0].content
+                        );
+                        const fill_extensions = this.makeExtensions(
+                            this.content_fill,
+                            valid_tags
+                        );
+
+                        this.converter = new showdown.Converter({
+                            extensions: [...fill_extensions, ...name_and_class_extensions]
+                        });
+
+                        this.content = json.data[0].content;
+
+                    } else {
+                        throw new Error("No content returned for given name " +
+                                this.name +
+                                " and group " +
+                        docGroup)
+                    }
+                });
+        }
     },
     computed: {
         documentationContent() {
