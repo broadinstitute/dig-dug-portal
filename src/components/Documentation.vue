481e9dd44df990835fec43d5a9dac9144d3e7d51
Preeti <singhpreeti236@gmail.com>
2020-05-20 16:39:01 -0400

    converter fix

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -45,41 +45,11 @@ export default Vue.component("documentation", {
                 .then(resp => resp.json())
                 .then(json => {
                     if (json.data.length > 0) {
-                        const classMap = {
-                            h1: "doc large-header",
-                            h2: "doc medium-header",
-                            h3: "doc small-header",
-                            h4: "doc x-small-header",
-                            p: "doc content",
-                            ul: "doc list",
-                            li: "doc item",
-                            em: "doc italic",
-                            strong: "doc bold",
-                            a: "doc link"
-                        };
-
-                        const name_and_class_extensions = Object.keys(
-                            classMap
-                        ).map(key => ({
-                            type: "output",
-                            regex: new RegExp(`<${key}(.*)>`, "g"),
-                            replace: `<${key} id="${this.name}" class="${classMap[key]}" $1>`
-                        }));
-
-                        const valid_tags = documentationParser.findTemplateTagsFromContent(
-                            json.data[0].content
-                        );
-                        const fill_extensions = documentationParser.makeExtensions(
+                        this.converter = documentationParser.makeConverter(
+                            json.data[0].content,
                             this.contentFill,
-                            valid_tags
+                            this.name
                         );
-
-                        this.converter = new showdown.Converter({
-                            extensions: [
-                                ...fill_extensions,
-                                ...name_and_class_extensions
-                            ]
-                        });
 
                         this.content = json.data[0].content;
                     } else {
@@ -98,6 +68,16 @@ export default Vue.component("documentation", {
             if (!!this.content) {
                 return this.converter.makeHtml(this.content);
             }
+        }
+    },
+    watch: {
+        contentFill: function(newContentFill) {
+            //create a new convertor that overides the one we are storing in data
+            this.converter = documentationParser.makeConverter(
+                this.content,
+                newContentFill,
+                this.name
+            );
         }
     },
 
