8aa368be2984a248ddcad59e2e7603ad3a6f645a
Preeti <singhpreeti236@gmail.com>
2020-06-03 13:47:02 -0400

    fix the banner class

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -22,8 +22,14 @@
                             style="padding: 5px; text-align: center;height: 50px;"
                         >
                             <img
-                                :src="'http://kp4cd.org/sites/default/files/vueportal/portals2mdkp_banner.svg'"
-                                :class="'portals-2-mdkp-logo'"
+                                v-if="frontContents.field_banner_logo"
+                                :src="'http://kp4cd.org/sites/default/files/vueportal/'+frontContents.field_banner_logo"
+                                :class="diseaseGroup.name+'kp-logo'"
+                            />
+                            <img
+                                v-else
+                                src="http://kp4cd.org/sites/default/files/vueportal/mdkp_header_logo.svg"
+                                class="mdkp-logo"
                             />
                         </div>
                     </a>
