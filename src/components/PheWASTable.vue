95201b5bfa1722a20145544f2a757a1d39a467e1
Quy <quy@broad.dev>
2020-06-05 11:25:05 -0400

    add filters to phewastable

--- src/components/PheWASTable.vue
+++ src/components/PheWASTable.vue
@@ -4,10 +4,11 @@
             hover
             small
             responsive="sm"
-            :items="pheWASAssociations"
+            :items="filterBeta"
             :fields="fields"
             :per-page="perPage"
             :current-page="currentPage"
+            filter-included-fields="pValue"
         >
             <template v-slot:cell(phenotype)="r">
                 <a
@@ -20,6 +21,55 @@
             <template
                 v-slot:cell(dichotomousEffect)="r"
             >{{!!r.item.phenotype.dichotomous ? floatFormatter(Math.exp(r.item.beta)) : null}}</template>
+            <template v-slot:thead-top="data">
+                <b-tr>
+                    <b-th>
+                        <vue-typeahead-bootstrap
+                            v-if="phenotypeMap"
+                            v-model="userText"
+                            ref="phenotypeSelect"
+                            placeholder="Select a phenotype ..."
+                            :data="phenotypeMap"
+                            :serializer="s => s.description"
+                            showOnFocus="true"
+                            showAllResults="true"
+                        >
+                            <template slot="suggestion" slot-scope="{ data, htmlText }">
+                                <span v-html="htmlText"></span>&nbsp;
+                                <small class="text-secondary">{{ data.group }}</small>
+                            </template>
+                        </vue-typeahead-bootstrap>
+                    </b-th>
+                    <b-th>
+                        <b-form-input
+                            id="filter-pValue"
+                            type="number"
+                            v-model="filter"
+                            placeholder="Filter pValue <="
+                        ></b-form-input>
+                    </b-th>
+                    <b-th>
+                        <b-form-group>
+                            <b-form-radio v-model="beta" name="all" value="a" size="sm">All</b-form-radio>
+                            <b-form-radio
+                                v-model="beta"
+                                name="positive"
+                                value="p"
+                                size="sm"
+                            >Positive</b-form-radio>
+                            <b-form-radio
+                                v-model="beta"
+                                name="negative"
+                                value="n"
+                                size="sm"
+                            >Negative</b-form-radio>
+                        </b-form-group>
+                    </b-th>
+                    <b-th></b-th>
+                    <b-th></b-th>
+                    <b-th></b-th>
+                </b-tr>
+            </template>
         </b-table>
         <b-pagination
             class="pagination-sm justify-content-center"
@@ -34,8 +84,10 @@
 import Vue from "vue";
 import $ from "jquery";
 
+import VueTypeaheadBootstrap from "vue-typeahead-bootstrap";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import Formatters from "@/utils/formatters";
+import { filterBeta } from "@/utils/filters";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -61,7 +113,8 @@ export default Vue.component("phewas-table", {
                     formatter: Formatters.floatFormatter,
                     tdClass(x) {
                         return !!x && x < 1e-5 ? "variant-table-cell high" : "";
-                    }
+                    },
+                    sortable: true
                 },
                 {
                     key: "continuousEffect",
@@ -81,9 +134,12 @@ export default Vue.component("phewas-table", {
                 {
                     key: "n",
                     label: "Sample Size",
-                    formatter: Formatters.intFormatter
+                    formatter: Formatters.intFormatter,
+                    sortable: true
                 }
-            ]
+            ],
+            beta: "a",
+            userText: ""
         };
     },
 
@@ -106,6 +162,9 @@ export default Vue.component("phewas-table", {
             return assocs
                 .filter(a => !!a.phenotype)
                 .sort((a, b) => a.pValue - b.pValue);
+        },
+        filterBeta() {
+            return filterBeta(this.pheWASAssociations, this.beta, "beta");
         }
     },
 
--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,8 +1,8 @@
 export function filterDropdown(data, filters) {
     //let filtered = [];
-    console.log("data ", data);
+    // console.log("data ", data);
     // console.log("col ", col);
-    console.log("filtersIN ", filters);
+    //console.log("filtersIN ", filters);
 
     // if (Array.isArray(term) && term.length > 0) {
     //     console.log("array");
@@ -13,12 +13,12 @@ export function filterDropdown(data, filters) {
             //console.log("key", key);
             //console.log("fKEY", filters[key]);
             //console.log("rKEY", row[key]);
-            console.log("compare", filters[key].includes(row[key]));
+            //console.log("compare", filters[key].includes(row[key]));
             if (filters[key] != "") return filters[key].includes(row[key]);
             else return true;
         });
     });
-    console.log("filters", filtered);
+    //console.log("filters", filtered);
 
     // else {
     //     console.log("not array");
@@ -36,7 +36,7 @@ export function filterDropdown(data, filters) {
     //     return term.includes(row[col]);
     // });
 
-    console.log("FF ", filtered);
+    //console.log("FF ", filtered);
     return filtered.length > 0 ? filtered : data;
     //return data;
 }
@@ -50,4 +50,29 @@ export function filterTissue(data, col, term) {
 
     //console.log("FF ", filtered);
     return filtered.length > 0 ? filtered : data;
+}
+
+export function filterBeta(data, value, key) {
+    if (value === "n")
+        //negative
+        return data.filter(row => {
+            return Number(row[key]) < 0;
+        });
+    else if (value === "p") {
+        //positive
+        return data.filter(row => {
+            return Number(row[key]) > 0;
+        });
+    } else {
+        //all
+        return data;
+    }
+}
+
+export function filterPValue(data, value) {
+    if (!!value)
+        return data.filter(row => {
+            return row[key] <= value;
+        });
+    else return data;
 }
