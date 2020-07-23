6c001d22c3c4a5b6abc4a77d7ed03af8d3cc807c
Kenneth Bruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-07-22 18:39:11 -0700

    refactoring cards a bit

---
+++ src/views/Results/cards/ResultCardTemplate.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h4>{{title}}</h4>
+        <b-row>
+            <b-col cols="3">
+                <slot name="sidebar">
+                    TODO: Sidebar for card type compatible with {{ title }}
+                </slot>
+            </b-col>
+            <b-col>
+                <slot name="content"></slot>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+import Vue from "vue"
+import { BootstrapVue } from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
+export default Vue.component('result-card', {
+    props: ["title"],
+})
+</script>
