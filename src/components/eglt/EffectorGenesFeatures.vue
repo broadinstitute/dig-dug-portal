3b9c11f0ef687e80f82e9695f76efbab8284edbd
Quy <quy@broad.dev>
2020-07-29 16:05:13 -0400

    add unique keys for row, separate v-if & v-for

--- src/components/eglt/EffectorGenesFeatures.vue
+++ src/components/eglt/EffectorGenesFeatures.vue
@@ -1,29 +1,38 @@
 <template>
     <div :class="'feature-content-wrapper hidden feature-content-wrapper-'+this.featureIndex">
         <b-container fluid>
-            <div v-for="(row, i) in features" :class="'feature-list '+i">
+            <div v-for="(row, i) in features" :class="'feature-list '+i" :key="i">
                 <b-row :class="'feature-name '+i" v-html="i"></b-row>
                 <template v-for="(col, j) in row">
-                    <b-row :class="'feature-headers '+i" v-if="j === 0" :title="i">
+                    <b-row
+                        :class="'feature-headers '+i"
+                        v-if="j === 0"
+                        :title="i"
+                        :key="`h_${i}_${j}`"
+                    >
                         <b-col
                             :class="($parent.config[$parent.dataset].featureRenderNot.includes(header))?'hidden feature-header-item '+i :'feature-header-item '+i "
                             v-for="header in Object.keys(col)"
+                            :key="header"
                             v-html="header"
                         ></b-col>
                     </b-row>
 
-                    <b-row :class="'feature-content '+i+ getColContent(col)">
-                        <b-col
-                            v-for="(item,name) in col"
-                            v-if="item == '' && item == null"
-                            :class="($parent.config[$parent.dataset].featureRenderNot.includes(name))? 'hidden feature-content-item '+i +' '+item : 'feature-content-item '+i +' '+item"
-                            v-html="'<span class=\'col-content-filler\'>filler</span>'"
-                        ></b-col>
-                        <b-col
-                            v-else
-                            :class="($parent.config[$parent.dataset].featureRenderNot.includes(name))? 'hidden feature-content-item '+i +' '+item : 'feature-content-item '+i +' '+item"
-                            v-html="$parent.formatContent([i,name],item,'feature')"
-                        ></b-col>
+                    <b-row :class="'feature-content '+i+ getColContent(col)" :key="`c_${i}_${j}`">
+                        <template v-for="(item,name) in col">
+                            <b-col
+                                :key="name"
+                                v-if="item == '' && item == null"
+                                :class="($parent.config[$parent.dataset].featureRenderNot.includes(name))? 'hidden feature-content-item '+i +' '+item : 'feature-content-item '+i +' '+item"
+                                v-html="'<span class=\'col-content-filler\'>filler</span>'"
+                            ></b-col>
+                            <b-col
+                                :key="name"
+                                v-else
+                                :class="($parent.config[$parent.dataset].featureRenderNot.includes(name))? 'hidden feature-content-item '+i +' '+item : 'feature-content-item '+i +' '+item"
+                                v-html="$parent.formatContent([i,name],item,'feature')"
+                            ></b-col>
+                        </template>
                     </b-row>
                 </template>
             </div>
