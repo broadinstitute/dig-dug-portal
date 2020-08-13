ab5cf3aa885939eebf70e7201431c75bac0116c4
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-08-13 16:02:45 -0700

    label

--- src/utils/lz/lzPanels.js
+++ src/utils/lz/lzPanels.js
@@ -50,6 +50,11 @@ export class LZAssociationsPanel {
             ...BASE_PANEL_OPTIONS,
             id: this.panel_id,
             y_index: 0,
+            axes: {
+                y1: {
+                    label: 'log10 log_pvalue'
+                }
+            },
         };
         this.handlers = {
             finishHandler,
@@ -119,7 +124,6 @@ export class LZAssociationsPanel {
 
 export class LZAnnotationIntervalsPanel {
     constructor(annotation, method, { finishHandler, resolveHandler, errHandler }, colorScheme=id=>'128,128,128', scoring) {
-        console.log(scoring)
 
         // panel_layout_type and datasource_type are not necessarily equal, and refer to different things
         // however they are also jointly necessary for LocusZoom –
