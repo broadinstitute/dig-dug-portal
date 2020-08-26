0c7d03d41cc0e0c0753de56d870c50ee65d77495
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-08-25 17:25:03 -0700

    rudimentary end-to-end calculation of credset if-then source

--- src/utils/lz/lzPanels.js
+++ src/utils/lz/lzPanels.js
@@ -4,6 +4,8 @@ import { query } from "@/utils/bioIndexUtils";
 import idCounter from "@/utils/idCounter"
 import { rgb } from "d3";
 
+import { marking, scoring } from 'gwas-credible-sets';
+
 import {
     postAlertNotice,
     postAlertError,
@@ -179,11 +181,15 @@ export class LZCredibleVariantsPanel {
             end: association.position,
             position: association.position,
             pvalue: association.pValue,
-            posteriorProbability: association.posteriorProbability,
+            // posteriorProbability => posterior_prob; it's refactored to the name compatible with the other credible set visualization supported by LocusZoom
+            posterior_prob: association.posteriorProbability,
             log_pvalue: ((-1) * Math.log10(association.pValue)).toPrecision(4),
             variant: association.varId,
             ref_allele: association.varId,
         }));
+
+        // the requirement for this field is required for how we're implementing the `bioIndexToLZReader` getter (below)
+        this.phenotype = phenotype;
 
         // LocusZoom Layout configuration options
         // See the LocusZoom docs for how this works
@@ -217,7 +223,7 @@ export class LZCredibleVariantsPanel {
                 "fields": [
                     `${this.datasource_namespace_symbol_for_panel}:id`,
                     `${this.datasource_namespace_symbol_for_panel}:position`,
-                    `${this.datasource_namespace_symbol_for_panel}:posteriorProbability`
+                    `${this.datasource_namespace_symbol_for_panel}:posterior_prob`
                 ],
                 "x_axis": {
                     "field": `${this.datasource_namespace_symbol_for_panel}:position`
@@ -227,7 +233,7 @@ export class LZCredibleVariantsPanel {
                 // we've removed axis y2 from the associations plot (as we're only defining y1)
                 "y_axis": {
                     "axis": 1,
-                    "field": `${this.datasource_namespace_symbol_for_panel}:posteriorProbability`,
+                    "field": `${this.datasource_namespace_symbol_for_panel}:posterior_prob`,
                     // normalizing the scale to probability space
                     "floor": 0,
                     "ceiling": 1
@@ -239,7 +245,8 @@ export class LZCredibleVariantsPanel {
     }
 
     get bioIndexToLZReader() {
-        return new _LZBioIndexSource({
+        return new _LZCredibleSetSource({
+            phenotype: this.phenotype,
             index: this.index,
             queryStringMaker: this.queryStringMaker,
             translator: this.translator,
@@ -338,7 +345,9 @@ export class LZPhewasPanel {
     }
 }
 
-
+// TODO: refactor to use BaseAdapter?
+// NOTE: Ideally we'd use an interface that both _LZBioIndexSource and _LZCredibleSetSource satisfy
+// (because otherwise we'd be using concrete inheritance if we were to minimize duplication here, or otherwise enforce structure across duplications)
 const _LZBioIndexSource = LocusZoom.Data.Source.extend(function (init) {
     this.parseInit(init);
 });
@@ -348,43 +357,226 @@ _LZBioIndexSource.prototype.parseInit = function (params) {
     this.queryStringMaker = queryStringMaker;
     this.index = index;
     this.translator = translator;
-    this.reader = readOnCoords(index, queryStringMaker, {
-        finishHandler,
-        resolveHandler,
-        errHandler,
-    });
 };
 _LZBioIndexSource.prototype.getRequest = function (state, chain, fields) {
     const self = this;
     return new Promise((resolve, reject) => {
         const alertID = postAlertNotice(`Loading ${self.index}; please wait ...`);
-        self.reader.fetch(state.chr, state.start, state.end, (data, err) => {
-            if (err) {
-                closeAlert(alertID);
-                postAlertError(err.detail);
-                reject(new Error(err));
-            }
-            closeAlert(alertID);
-            resolve(self.translator(data));
-        });
+        query(self.index, self.queryStringMaker(state.chr, state.start, state.end), {
+            finishHandler: self.finishHandler,
+            resolveHandler: self.resolveHandler,
+            errHandler: self.errHandler,
+        })
+        .then(async resultData => {
+            resolve(self.translator(resultData));
+        })
+        .catch(async error => {
+            postAlertError(error.detail);
+            reject(new Error(error));
+        })
+        .finally(closeAlert(alertID));
     });
 };
 
-// TODO: Can we eliminate this function completely in favor of just using bioIndexUtils.query?
-function readOnCoords(index, queryStringMaker, {
-    resolveHandler,
-    errHandler,
-    finishHandler,
-}) {
-    return {
-        async fetch(chr, start, end, callback) {
-            let q = queryStringMaker(chr, start, end);
-            let responseData = await query(index, q, {
-                finishHandler,
-                resolveHandler,
-                errHandler,
-            })
-            return callback(responseData);
+const _LZCredibleSetSource = LocusZoom.Data.Source.extend(function (init) {
+    this.parseInit(init);
+});
+_LZCredibleSetSource.prototype.parseInit = function (params) {
+    const { phenotype, index, queryStringMaker, translator, finishHandler, resolveHandler, errHandler, initialData } = params;
+    this.params = params;
+    this.queryStringMaker = queryStringMaker;
+    this.index = index;
+    this.translator = translator;
+    this.initialData = initialData;
+    this.phenotype = phenotype;
+};
+_LZCredibleSetSource.prototype.getRequest = function (state, chain, fields) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+
+        const alertID = postAlertNotice(`Loading ${self.index}; please wait ...`);
+        if (!!self.initialData) {
+            closeAlert(alertID);
+            resolve(self.translator(self.initialData));
+            self.initialData = null;
+        } else {
+            // decide whether or not to use a precomputed credset
+            // TODO: conditional based on the code below
+            if (true) {
+                const phenoRegionQuery = `${this.phenotype},${state.chr}:${state.start}-${state.end}`;
+                query('credible-sets', phenoRegionQuery).then(async results => {
+                    closeAlert(alertID);
+                    console.log('credible sets bioindex results', results);
+                    if (
+                        false
+                        // results.length > 0
+                    ) {
+
+                        console.log('there are credible sets - merge and return them');
+                        resolve([]);
+
+                    } else {
+
+                        console.log('calculate the credible sets')
+                        console.log('get the necessary associations')
+                        query('associations', phenoRegionQuery).then(async results => {
+                            // TODO: what is this?
+                            // const nlogpvals = chain.body.map(function (item) {
+                            //     return item[self.params.fields.log_pvalue];  // this implies that this data is column-indexed?
+                            // });
+                            const translatedResults = self.translator(results);
+                            const nlogpvals = results.map(association => -Math.log10(association.pValue));
+                            const credset_data = [];
+                            try {
+                                const scores = scoring.bayesFactors(nlogpvals);
+                                const posteriorProbabilities = scoring.normalizeProbabilities(scores);
+
+                                // Use scores to mark the credible set in various ways (depending on your visualization preferences,
+                                //   some of these may not be needed)
+                                const credibleSet = marking.findCredibleSet(scores, 0.95);
+                                const credSetScaled = marking.rescaleCredibleSet(credibleSet);
+                                const credSetBool = marking.markBoolean(credibleSet);
+                                console.log('trying to make credset info', scores, posteriorProbabilities, credibleSet, credSetScaled, credSetBool);
+
+                                // Annotate each response record based on credible set membership
+                                for (let i = 0; i < translatedResults.length; i++) {
+                                    credset_data.push({
+                                        ...translatedResults[i],
+                                        posterior_prob: posteriorProbabilities[i],
+                                        contrib_fraction: credSetScaled[i],
+                                        is_member: credSetBool[i],
+                                    });
+                                }
+                            } catch (e) {
+                                // If the calculation cannot be completed, return the data without annotation fields
+                                console.error(e);
+                            }
+                            console.log(credset_data);
+                            resolve(credset_data);
+
+                        });
+                    }
+                });
+
+            } else {
+                query(self.index, self.queryStringMaker(state.chr, state.start, state.end), {
+                    finishHandler: self.finishHandler,
+                    resolveHandler: self.resolveHandler,
+                    errHandler: self.errHandler,
+                })
+                .then(async resultData => {
+                    resolve(self.translator(resultData));
+                })
+                .catch(async error => {
+                    postAlertError(error.detail);
+                    reject(new Error(error));
+                })
+                .finally(closeAlert(alertID));
+            }
         }
-    }
-}
+
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+// Adapted Credible Set Data Source
+// Behavior: if there are no credible sets satisfactory on the given region, calculate credible set membership for the points and return that instead
+// From LocusZoom Code
+// https://github.com/statgen/locuszoom/blob/84d979c1fe54b2e3396857d3cef3fc7e581881ef/esm/ext/lz-credible-sets.js
+
+// const BaseAdapter = LocusZoom.Adapters.get('BaseAdapter');
+/**
+ * Custom data source that calculates the 95% credible set based on provided data.
+ * This source must be requested as the second step in a chain, after a previous step that returns fields required
+ *  for the calculation.
+ *
+ * @param {Object} init.params
+ * @param {Object} init.params.fields
+ * @param {String} init.params.fields.log_pvalue The name of the field containing pvalue information
+ * @param {Number} [init.params.threshold=0.95] The credible set threshold (eg 95%)
+ *
+ */
+// class CredibleSetLZ extends BaseAdapter {
+//     constructor(config) {
+//         super(...arguments);
+//         this.dependentSource = true; // Don't do calcs for a region with no assoc data
+//     }
+
+//     parseInit(config) {
+//         super.parseInit(...arguments);
+//         if (!(this.params.fields && this.params.fields.log_pvalue)) {
+//             throw new Error(`Source config for ${this.constructor.SOURCE_NAME} must specify how to find 'fields.log_pvalue'`);
+//         }
+//         if (!this.params.threshold) {
+//             this.params.threshold = 0.95;
+//         }
+//     }
+
+//     getCacheKey (state, chain, fields) {
+//         const threshold = state.credible_set_threshold || this.params.threshold;
+//         return [threshold, state.chr, state.start, state.end].join('_');
+//     }
+
+//     // TODO: This is the method to refactor!
+//     fetchRequest(state, chain) {
+//         const self = this;
+
+//         // TODO: !!! can work as a filter function replacement?! - K
+//         // The threshold can be overridden dynamically via `plot.state`, or set when the source is created
+//         const threshold = state.credible_set_threshold || this.params.threshold;
+
+//         // Calculate raw bayes factors and posterior probabilities based on information returned from the API
+//         if (typeof chain.body[0][self.params.fields.log_pvalue] === 'undefined') {
+//             throw new Error('Credible set source could not locate the required fields from a previous request.');
+//         }
+//         const nlogpvals = chain.body.map(function (item) {
+//             return item[self.params.fields.log_pvalue];
+//         });
+        // const credset_data = [];
+        // try {
+        //     const scores = scoring.bayesFactors(nlogpvals);
+        //     const posteriorProbabilities = scoring.normalizeProbabilities(scores);
+
+        //     // Use scores to mark the credible set in various ways (depending on your visualization preferences,
+        //     //   some of these may not be needed)
+        //     const credibleSet = marking.findCredibleSet(scores, threshold);
+        //     const credSetScaled = marking.rescaleCredibleSet(credibleSet);
+        //     const credSetBool = marking.markBoolean(credibleSet);
+
+        //     // Annotate each response record based on credible set membership
+        //     for (let i = 0; i < chain.body.length; i++) {
+        //         credset_data.push({
+        //             posterior_prob: posteriorProbabilities[i],
+        //             contrib_fraction: credSetScaled[i],
+        //             is_member: credSetBool[i],
+        //         });
+        //     }
+        // } catch (e) {
+        //     // If the calculation cannot be completed, return the data without annotation fields
+        //     console.error(e);
+        // }
+        // return Promise.resolve(credset_data);
+//     }
+
+//     combineChainBody(data, chain, fields, outnames, trans) {
+//         // At this point namespacing has been applied; add the calculated fields for this source to the chain
+//         for (let i = 0; i < data.length; i++) {
+//             const src = data[i];
+//             const dest = chain.body[i];
+//             Object.keys(src).forEach(function (attr) {
+//                 dest[attr] = src[attr];
+//             });
+//         }
+//         return chain.body;
+//     }
+// }
+
