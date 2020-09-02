4adcacce56b9bdaeb1f2e39def77003b5cbf13a3
DK Jang <dkjang@broadinstitute.org>
2020-09-02 12:48:20 -0400

    sticky header for EGL table improved

--- src/views/EglMethod/main.js
+++ src/views/EglMethod/main.js
@@ -63,10 +63,17 @@ new Vue({
             uiUtils.showElement(ELEMENT);
         },
         onScroll(e) {
-            let windowTop = window.top.scrollY /* or: e.target.documentElement.scrollTop */
+            let windowTop = window.top.scrollY;
+
+            let eglTable = document.getElementsByClassName("EGLT-table")[0];
+            let rect = eglTable.getBoundingClientRect();
+            let scrollTop = document.documentElement.scrollTop ?
+                document.documentElement.scrollTop : document.body.scrollTop;
+
+            let tableTop = rect.top + scrollTop;
 
             let element = document.getElementsByClassName("top-level-header")[0];
-            if (windowTop > 500) {
+            if (windowTop > tableTop) {
                 if (!element.classList.contains('fixed-header')) {
                     element.classList.add('fixed-header');
                 }
