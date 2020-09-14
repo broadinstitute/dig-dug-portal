943264bda406697d2ed6631e913e9277745d80af
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-09-14 13:50:52 -0700

    initial commit

--- src/views/Debug/Template.vue
+++ src/views/Debug/Template.vue
@@ -1,34 +1,5 @@
 <template>
     <div>
-        <div class="filtering-ui-wrapper">
-            <div class="row filtering-ui-content">
-                <div class="col filter-col-lg">
-                    <div class="label">Annotation Method Track</div>
-                    <annotation-method-selectpicker
-                        :annotations="$parent.globalEnrichmentAnnotations"
-                        @annotation="$parent.addAnnotationIntervalsPanel($event)"
-                    />
-                </div>
-
-                <div class="col divider">&nbsp;</div>
-                <div class="col filter-col-lg">
-                    <div class="label">Credible Sets Track</div>
-                    <credible-sets-selectpicker
-                        :credibleSets="$parent.credibleSets"
-                        @credibleset="$parent.addCredibleVariantsPanel($event)"
-                    />
-                </div>
-            </div>
-        </div>
-
-        <locuszoom
-            ref="locuszoom"
-            :chr="$store.state.chr"
-            :start="$store.state.start"
-            :end="$store.state.end"
-            :colorScheme="$parent.tissueColorScheme"
-            @panelremoved="$parent.tap($event)"
-            @regionchanged="$parent.requestCredibleSets($event.data)"
-        ></locuszoom>
+        {{$parent.hello}}
     </div>
 </template>
