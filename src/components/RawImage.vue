bae634f14b821fb4250afa6cfeb7b4ff2dc38f8a
Quy <quy@broad.dev>
2020-09-21 19:34:50 -0400

    add mixin to RI

--- src/components/RawImage.vue
+++ src/components/RawImage.vue
@@ -4,42 +4,42 @@
         <p class="card-text" v-if="documentation && loaded">
             <documentation :name="documentation" :content-fill="contentFill"></documentation>
         </p>
-        <div v-else-if="unauthorized" class="unauthorized">
-            Please
-            <a href="/login">log in</a> with an authorized Google account to see this content.
-        </div>
-        <div v-else-if="status" class="failed">Failed to load</div>
+        <b-alert v-else-if="unauthorized && !user" class="unauthorized" show variant="warning">
+            <b-icon icon="exclamation-triangle"></b-icon>Please
+            <a href="/login" @click="saveCurrentPage">log in</a> with an authorized Google account to see this content.
+        </b-alert>
+        <b-alert v-else-if="unauthorized && !!user" class="unauthorized" show variant="warning">
+            <b-icon icon="exclamation-triangle"></b-icon>You don't have permission to view this content.
+            <br />Please contact us if you believe you should've given access.
+        </b-alert>
+        <b-alert v-else-if="status" class="failed" show variant="danger">
+            <b-icon icon="exclamation-triangle"></b-icon>Failed to load
+        </b-alert>
     </div>
 </template>
 
 <script>
 import Vue from "vue";
 import $ from "jquery";
-
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
 import Documentation from "@/components/Documentation.vue";
 import { request } from "@/utils/bioIndexUtils.js";
-
+import { userMixin } from "@/mixins/userMixin";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 export default Vue.component("raw-img", {
     props: ["src", "documentation", "contentFill"],
-
+    mixins: [userMixin],
     components: {
         Documentation,
     },
-
     data() {
         return {
             status: null,
         };
     },
-
     mounted() {
         let that = this;
         let im = this.$refs.im;
@@ -58,7 +58,6 @@ export default Vue.component("raw-img", {
                 }
             });
     },
-
     computed: {
         loaded() {
             return this.status === 200;
