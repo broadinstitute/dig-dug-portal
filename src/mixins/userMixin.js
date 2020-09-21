7dc76c6dc2c84b7f6deede14ce53d29c21dcd500
Quy <quy@broad.dev>
2020-09-21 17:50:23 -0400

    move user functions to new mixin

---
+++ src/mixins/userMixin.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import VueCookies from "vue-cookies";
+import host from "@/utils/hostUtils";
+Vue.use(VueCookies);
+
+export const userMixin = {
+    computed: {
+        user() {
+            return this.$store.state.bioPortal.user;
+        }
+    },
+    methods: {
+        saveCurrentPage() {
+            Vue.$cookies.set("whereAmI", location.href, "", "", host.domain);
+        }
+    }
+};
