3021451b7d52be1b5d93a03f6a2e6c32722c84cc
Jeffrey Massung <massung@gmail.com>
2020-09-23 13:17:32 -0500

    Add space and fixed build-clean for Windows(again).

--- src/components/FilterWidget/FilterWidget.vue
+++ src/components/FilterWidget/FilterWidget.vue
@@ -1,37 +1,36 @@
 <template>
     <span>
         <!-- Controls and their labels -->
-        <slot name="header">
-        </slot>
-                <EventListener @change="filterControlChange">
-                    <!-- Filter Widget Control Slot -->
-                    <!-- It's unnamed because multiple filter controls will be placed inside here -->
-                    <slot></slot>
-                </EventListener>
+        <slot name="header"> </slot>
+        <EventListener @change="filterControlChange">
+            <!-- Filter Widget Control Slot -->
+            <!-- It's unnamed because multiple filter controls will be placed inside here -->
+            <slot></slot>
+        </EventListener>
 
         <!-- Pills for everything -->
 
-                <span v-if="filterList.length > 0">Selected Filters:&nbsp;&nbsp;
-                    <!-- Derive pills from current filter state?
+        <span v-if="filterList.length > 0"
+            >Selected Filters:&nbsp;&nbsp;
+            <!-- Derive pills from current filter state?
                         Might lose coloring - unless we use something like my planned colorUtils with real-time schema generation on a cycle
                         It would be deterministic upto the compile-time declaration of the FilterWidget controls which would lead to predicatable results at runtime
                     -->
-                    <!-- TODO: Color Scheme for Pills via Variant => use the colorUtils instead? -->
-                    <b-badge
-                        v-for="(filter, idx) in filterList"
-                        :key="filter.field+filter.predicate+filter.threshold+idx"
-                        pill
-                        :style="`background-color:${filter.pill.color};`"
-                        @click="unsetFilter(filter, idx)"
-                        class="btn">
-                            {{filter.pill.label(filter)}}
-                            <span class="remove">X</span>
-                    </b-badge>
-                </span>
-                <!-- Spacer to prevent flicker when new pills are added to the UI -->
-                <br v-else/>
-
-
+            <!-- TODO: Color Scheme for Pills via Variant => use the colorUtils instead? -->
+            <b-badge
+                v-for="(filter, idx) in filterList"
+                :key="filter.field + filter.predicate + filter.threshold + idx"
+                pill
+                :style="`background-color:${filter.pill.color}; margin-right: 4px`"
+                @click="unsetFilter(filter, idx)"
+                class="btn"
+            >
+                {{ filter.pill.label(filter) }}
+                <span class="remove">X</span>
+            </b-badge>
+        </span>
+        <!-- Spacer to prevent flicker when new pills are added to the UI -->
+        <br v-else />
     </span>
 </template>
 
@@ -45,7 +44,7 @@ Vue.use(IconsPlugin);
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import { filterFromPredicates, predicateFromSpec } from "@/utils/filterHelpers"
+import { filterFromPredicates, predicateFromSpec } from "@/utils/filterHelpers";
 
 const EventListener = {
     /*
@@ -63,7 +62,6 @@ const EventListener = {
      *
      */
     render(createElement) {
-
         // Using Vue's createElement API with render functions to eliminate the need to define const EventListener in a separate component file.
         // Without using Vue's runtime compiler (bloat), we can't define scoped slots directly in it, so instead we use an undocumented internal
         // feature – of course – to give the element a default slot.
@@ -71,9 +69,8 @@ const EventListener = {
         // https://stackoverflow.com/a/58859267
 
         return createElement("div", this.$scopedSlots.default());
-    }
-
-}
+    },
+};
 
 export default Vue.component("filter-widget", {
     props: ["value", "inclusive", "strictCase", "looseMatch"],
@@ -87,38 +84,44 @@ export default Vue.component("filter-widget", {
     },
 
     computed: {
-
         filterFunction() {
-            const predicates = this.filterList.map(obj => predicateFromSpec({...obj}, { strictCase: this.strictCase, notStrictMatch: this.looseMatch }));
+            const predicates = this.filterList.map((obj) =>
+                predicateFromSpec(
+                    { ...obj },
+                    {
+                        strictCase: this.strictCase,
+                        notStrictMatch: this.looseMatch,
+                    }
+                )
+            );
             return filterFromPredicates(predicates, !!this.inclusive);
-        }
-
+        },
     },
 
     methods: {
-
         filterControlChange(threshold, filterDefinition) {
-            if (threshold !== null && !this.filterList.map(filter => filter.field).includes(filterDefinition.field)) {
-
+            if (
+                threshold !== null &&
+                !this.filterList
+                    .map((filter) => filter.field)
+                    .includes(filterDefinition.field)
+            ) {
                 this.filterList.push({
                     ...filterDefinition,
                     threshold,
                 });
-
             } else {
                 // if the definition already exists, and it's a multiple, then just push, since we can allow for multiple instances of the same filter
                 if (filterDefinition.multiple) {
-
                     this.filterList.push({
                         ...filterDefinition,
                         threshold,
                     });
 
-                // if the definition already exists, and it's a not a multiple, then modify the existing definition in filterList that matches the field
-                // TODO: would be faster if we maintain a normalized version of filterList against the filter ID, refactor towards this for performance
+                    // if the definition already exists, and it's a not a multiple, then modify the existing definition in filterList that matches the field
+                    // TODO: would be faster if we maintain a normalized version of filterList against the filter ID, refactor towards this for performance
                 } else if (!filterDefinition.multiple) {
-
-                    this.filterList = this.filterList.map(filter => {
+                    this.filterList = this.filterList.map((filter) => {
                         if (filter.field === filterDefinition.field) {
                             const tempFilter = filter;
                             tempFilter.threshold = threshold;
@@ -128,7 +131,6 @@ export default Vue.component("filter-widget", {
                             return filter;
                         }
                     });
-
                 }
             }
             // NOTE: As a result of this.filterList being modified, the computed property for the filterFunction should reactively producing a new version of itself.
@@ -138,15 +140,15 @@ export default Vue.component("filter-widget", {
             // equiv to setFilter with no data => reduction by alias
             // this.filterData[obj] = "";
             // this.filterList = this.filterList.filter(filterSpec => filterSpec.field !== obj.field && filterSpec.threshold !== obj.threshold && filterSpec.threshold !== obj.threshold)
-            this.filterList = this.filterList.slice(0,idx).concat(this.filterList.slice(idx + 1, this.filterList.length))
+            this.filterList = this.filterList
+                .slice(0, idx)
+                .concat(this.filterList.slice(idx + 1, this.filterList.length));
         },
-
-
     },
     watch: {
         filterFunction(newFilterFunction) {
-            this.$emit('input', newFilterFunction);
-        }
-    }
+            this.$emit("input", newFilterFunction);
+        },
+    },
 });
 </script>
--- src/views/Region/Template.vue
+++ src/views/Region/Template.vue
@@ -1,7 +1,10 @@
 <template>
     <!-- Header -->
     <div>
-        <page-header :disease-group="$parent.diseaseGroup" :front-contents="$parent.frontContents"></page-header>
+        <page-header
+            :disease-group="$parent.diseaseGroup"
+            :front-contents="$parent.frontContents"
+        ></page-header>
 
         <!-- body -->
         <div class="container-fluid mdkp-body">
@@ -11,18 +14,34 @@
                         Chromosome: Start position - End position
                         <a
                             class="edit-btn"
-                            v-on:click="() => $parent.showHideElement('regionSearchHolder','region_gene_search')"
-                        >Edit position / Search gene</a>
+                            v-on:click="
+                                () =>
+                                    $parent.showHideElement(
+                                        'regionSearchHolder',
+                                        'region_gene_search'
+                                    )
+                            "
+                            >Edit position / Search gene</a
+                        >
                     </div>
                     <div class="col-md-4 gene-page-header-title">
                         Phenotype
                         <a
                             class="edit-btn"
-                            v-on:click="() => $parent.showHideElement('phenotypeSearchHolder')"
-                        >Select phenotype</a>
+                            v-on:click="
+                                () =>
+                                    $parent.showHideElement(
+                                        'phenotypeSearchHolder'
+                                    )
+                            "
+                            >Select phenotype</a
+                        >
                     </div>
                     <div class="col-md-8 gene-page-header-body regionInfo">
-                        <div id="regionSearchHolder" class="gene-page-header-search-holder hidden">
+                        <div
+                            id="regionSearchHolder"
+                            class="gene-page-header-search-holder hidden"
+                        >
                             <div class="region-search">
                                 <div class="col-md-1 input-wrapper">
                                     <input
@@ -50,7 +69,12 @@
                                 </div>
                                 <div class="col-md-3 input-wrapper">
                                     <gene-selectpicker
-                                        @onGeneChange="$store.dispatch('onGeneChange',$event)"
+                                        @onGeneChange="
+                                            $store.dispatch(
+                                                'onGeneChange',
+                                                $event
+                                            )
+                                        "
                                     ></gene-selectpicker>
                                 </div>
 
@@ -60,23 +84,29 @@
                                         class="btn btn-primary"
                                         type="button"
                                         @click="$store.dispatch('queryRegion')"
-                                    >GO</button>
+                                    >
+                                        GO
+                                    </button>
                                 </div>
                             </div>
                         </div>
-                        {{$parent.regionString}}
+                        {{ $parent.regionString }}
                         <button
                             class="btn btn-primary text-nowrap text-right explore-region-btn"
                             style="margin-left: 20px"
                             @click="$parent.exploreExpanded()"
-                        >Expand &plusmn; 50 kb</button>
+                        >
+                            Expand &plusmn; 50 kb
+                        </button>
                         <lunaris-link
                             :diseaseGroup="$parent.diseaseGroup"
                             :chr="$store.state.chr"
                             :begin="$store.state.start"
                             :end="$store.state.end"
                             :trait="$store.state.phenotype"
-                            :dataContent="this.$store.state.lunaris.dataFromLunaris"
+                            :dataContent="
+                                this.$store.state.lunaris.dataFromLunaris
+                            "
                         ></lunaris-link>
                     </div>
                     <div class="col-md-4 gene-page-header-body">
@@ -87,10 +117,14 @@
                             <phenotype-selectpicker
                                 v-if="$store.state.phenotype"
                                 :phenotypes="$store.state.bioPortal.phenotypes"
-                                :default-phenotype="$store.state.phenotype.description"
+                                :default-phenotype="
+                                    $store.state.phenotype.description
+                                "
                             ></phenotype-selectpicker>
                         </div>
-                        <span v-if="$store.state.phenotype">{{$store.state.phenotype.description}}</span>
+                        <span v-if="$store.state.phenotype">{{
+                            $store.state.phenotype.description
+                        }}</span>
                     </div>
                 </div>
             </div>
@@ -102,84 +136,102 @@
             <div class="card mdkp-card">
                 <div class="card-body">
                     <h4 class="card-title">Genes overlapping region</h4>
-                    <div v-for="row in $parent.genes" :class="'gene-with-signal '+row.type">
-                        <a :href="`/gene.html?gene=${row.name}`">{{row.name}}</a>
+                    <div
+                        v-for="row in $parent.genes"
+                        :class="'gene-with-signal ' + row.type"
+                    >
+                        <a :href="`/gene.html?gene=${row.name}`">{{
+                            row.name
+                        }}</a>
                     </div>
                 </div>
             </div>
             <div class="card mdkp-card">
                 <div class="card-body">
-                    <h4
-                        class="card-title"
-                    >Variant associations in the region: {{$parent.regionString}}</h4>
-                    <documentation name="region.phenos_w_signal.subheader"></documentation>
+                    <h4 class="card-title">
+                        Variant associations in the region:
+                        {{ $parent.regionString }}
+                    </h4>
+                    <documentation
+                        name="region.phenos_w_signal.subheader"
+                    ></documentation>
                     <div v-if="$parent.topAssociations.length > 0">
                         <div
-                            style="text-align: right; padding-bottom: 5px;"
+                            style="text-align: right; padding-bottom: 5px"
                             v-if="$parent.topAssociations[0].pValue <= 5e-8"
                         >
                             <div
                                 href="javascript:;"
-                                v-on:click="$parent.switchViews(['pws-merged-view','pws-bar-view']);"
+                                v-on:click="
+                                    $parent.switchViews([
+                                        'pws-merged-view',
+                                        'pws-bar-view',
+                                    ])
+                                "
                                 class="switch-view btn btn-secondary btn-sm"
-                            >View associations by phenotype group</div>
+                            >
+                                View associations by phenotype group
+                            </div>
                         </div>
-                        <phenotype-signal-mixed :phenotypes="$parent.topAssociations"></phenotype-signal-mixed>
+                        <phenotype-signal-mixed
+                            :phenotypes="$parent.topAssociations"
+                        ></phenotype-signal-mixed>
                     </div>
                 </div>
             </div>
 
             <div class="card mdkp-card">
                 <div class="card-body">
-
                     <div v-if="!!$store.state.phenotype">
-
                         <h4 class="card-title">
-                            Top Associations for {{$store.state.phenotype.description}}
+                            Top Associations for
+                            {{ $store.state.phenotype.description }}
                             <tooltip-documentation
                                 name="region.topassoc.tooltip"
                                 :isHover="true"
                                 :noIcon="false"
                             ></tooltip-documentation>
                         </h4>
-                        <documentation name="region.variantassociation.subheader"></documentation>
+                        <documentation
+                            name="region.variantassociation.subheader"
+                        ></documentation>
 
                         <b-container fluid class="filtering-ui-wrapper">
                             <b-row class="filtering-ui-content">
-                                <filter-widget v-model="$parent.associationsFilter" :looseMatch="true">
+                                <filter-widget
+                                    v-model="$parent.associationsFilter"
+                                    :looseMatch="true"
+                                >
                                     <filter-enumeration-control
                                         :field="'consequence'"
-                                        :options="$parent.associationConsequences">
-                                        <div class="label">
-                                        Consequence
-                                        </div>
+                                        :options="
+                                            $parent.associationConsequences
+                                        "
+                                    >
+                                        <div class="label">Consequence</div>
                                     </filter-enumeration-control>
 
                                     <filter-enumeration-control
                                         :field="'nearest'"
-                                        :options="$parent.associationNearestGenes">
-                                        <div class="label">
-                                        Closest Genes
-                                        </div>
+                                        :options="
+                                            $parent.associationNearestGenes
+                                        "
+                                    >
+                                        <div class="label">Closest Genes</div>
                                     </filter-enumeration-control>
 
-                                    <filter-pvalue-control
-                                        :field="'pValue'">
-                                        <div class="label">
-                                        P-Value (&le;)
-                                        </div>
+                                    <filter-pvalue-control :field="'pValue'">
+                                        <div class="label">P-Value (&le;)</div>
                                     </filter-pvalue-control>
 
                                     <filter-effect-direction-control
-                                        :field="'beta'">
-                                        <div class="label">
-                                        Effect (+/-)
-                                        </div>
+                                        :field="'beta'"
+                                    >
+                                        <div class="label">Effect (+/-)</div>
                                     </filter-effect-direction-control>
                                 </filter-widget>
                             </b-row>
                         </b-container>
-
 
                         <associations-table
                             v-if="$store.state.associations.data.length > 0"
@@ -188,11 +240,7 @@
                             :filter="$parent.associationsFilter"
                         ></associations-table>
 
-                        <br>
-
-                        <h4
-                            class="card-title"
-                        >Visualize associations for {{$store.state.phenotype.description}}</h4>
+                        <br />
                         <documentation
                             name="region.lz.subheader"
                             :content-fill="$parent.documentationMap"
@@ -206,33 +254,51 @@
                         <div class="filtering-ui-wrapper">
                             <div class="row filtering-ui-content">
                                 <div class="col filter-col-lg">
-                                    <div class="label" style="margin-bottom: 5px;">
+                                    <div
+                                        class="label"
+                                        style="margin-bottom: 5px"
+                                    >
                                         Add annotation method track
                                     </div>
                                     <annotation-method-selectpicker
-                                        :annotations="$parent.globalEnrichmentAnnotations"
+                                        :annotations="
+                                            $parent.globalEnrichmentAnnotations
+                                        "
                                         :clearOnSelected="true"
-                                        @annotation="$parent.addAnnotationIntervalsPanel($event)"/>
+                                        @annotation="
+                                            $parent.addAnnotationIntervalsPanel(
+                                                $event
+                                            )
+                                        "
+                                    />
                                 </div>
 
                                 <!-- <div class="col divider">&nbsp;</div> -->
                                 <div class="col filter-col-lg">
-                                    <div class="label" style="margin-bottom: 5px;">
+                                    <div
+                                        class="label"
+                                        style="margin-bottom: 5px"
+                                    >
                                         Add credible sets track
                                     </div>
                                     <credible-sets-selectpicker
                                         :credibleSets="$parent.credibleSets"
                                         :clearOnSelected="true"
-                                        @credibleset="$parent.addCredibleVariantsPanel($event)"
+                                        @credibleset="
+                                            $parent.addCredibleVariantsPanel(
+                                                $event
+                                            )
+                                        "
                                     />
                                 </div>
 
                                 <div class="col divider">&nbsp;</div>
 
-                                <div class="col" style="vertical-align: top;">
+                                <div class="col" style="vertical-align: top">
                                     <filter-widget
                                         v-model="$parent.annotationsFilter"
-                                        :looseMatch="true">
+                                        :looseMatch="true"
+                                    >
                                         <template #header>
                                             <div class="label">
                                                 Filter annotation tracks
@@ -240,7 +306,9 @@
                                         </template>
 
                                         <!-- 'span' leaves the label inline -->
-                                        <filter-pvalue-control :field="'pValue'">
+                                        <filter-pvalue-control
+                                            :field="'pValue'"
+                                        >
                                             <span class="label">
                                                 P-Value (&le;)
                                             </span>
@@ -251,7 +319,6 @@
                                                 Fold (&ge;)
                                             </span>
                                         </filter-greater-control>
-
                                     </filter-widget>
                                 </div>
 
@@ -264,7 +331,6 @@
                                         target="_blank"
                                     >{{`Trait: ${$store.state.phenotype.name}, Region: ${$parent.regionString}`}}</b-button>
                                 </div> -->
-
                             </div>
                         </div>
 
@@ -275,26 +341,27 @@
                             :end="$store.state.end"
                             :filterAssociations="$parent.associationsFilter"
                             :filterAnnotations="$parent.annotationsFilter"
-                            @regionchanged="$parent.requestCredibleSets($event.data)"
-                            :refSeq="true">
+                            @regionchanged="
+                                $parent.requestCredibleSets($event.data)
+                            "
+                            :refSeq="true"
+                        >
                             <lz-associations-panel
                                 :phenotype="$store.state.phenotype.name"
                                 :finishHandler="$parent.updateAssociationsTable"
                             ></lz-associations-panel>
                         </locuszoom>
-
                     </div>
                 </div>
-
-
-
             </div>
         </div>
         <!-- TODO: collapse tables -->
         <b-collapse id="collapse-1" class="mt-2">
             <b-card>
                 <p class="card-text">Collapse contents Here</p>
-                <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+                <b-button v-b-toggle.collapse-1-inner size="sm"
+                    >Toggle Inner Collapse</b-button
+                >
                 <b-collapse id="collapse-1-inner" class="mt-2">
                     <b-card>Hello!</b-card>
                 </b-collapse>
