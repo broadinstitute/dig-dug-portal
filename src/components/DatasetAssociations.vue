80f4a463eac6e9540dba943472290764508a3948
Quy <quy@broad.dev>
2020-09-25 16:31:19 -0400

    wip: component displays data

--- src/components/DatasetAssociations.vue
+++ src/components/DatasetAssociations.vue
@@ -1,11 +1,22 @@
 <template>
-    <div id="dataset-associations">hello</div>
+    <div id="dataset-associations" v-if="rows > 0">
+        <div v-for="(phenotype, index) in groupedAssociations" :key="phenotype">
+            <b-col>{{ phenotypeMap[index].description }}</b-col>
+        </div>
+        <b-pagination
+            class="pagination-sm justify-content-center"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+        ></b-pagination>
+    </div>
 </template>
 
 <script>
 import Vue from "vue";
 import { orderBy, groupBy } from "lodash";
 export default Vue.component("dataset-associations", {
+    props: ["associations", "phenotypeMap"],
     data() {
         return {
             perPage: 25,
@@ -13,12 +24,12 @@ export default Vue.component("dataset-associations", {
         };
     },
     computed: {
-        associations() {
-            return this.$store.state.datasetAssociations.data;
-        },
         groupedAssociations() {
-            let ordered = orderBy(this.associations, "pValue");
+            let ordered = orderBy(this.associations, ["pValue"], ["desc"]);
             return groupBy(ordered, "phenotype");
+        },
+        rows() {
+            return this.associations.length;
         },
     },
 });
