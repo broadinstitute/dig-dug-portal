068dd16e77a3ecc74ff3b575cf5c26706574778c
Quy <quy@broad.dev>
2020-09-25 13:11:22 -0400

    start component

---
+++ src/components/DatasetAssociations.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="dataset-associations">hello</div>
+</template>
+
+<script>
+import Vue from "vue";
+import { orderBy, groupBy } from "lodash";
+export default Vue.component("dataset-associations", {
+    data() {
+        return {
+            perPage: 25,
+            currentPage: 1,
+        };
+    },
+    computed: {
+        associations() {
+            return this.$store.state.datasetAssociations.data;
+        },
+        groupedAssociations() {
+            let ordered = orderBy(this.associations, "pValue");
+            return groupBy(ordered, "phenotype");
+        },
+    },
+});
+</script>
+
+<style>
+</style>
