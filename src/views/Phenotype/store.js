9e90dce06e87da9fde7e1bb1eb3c6011393e6437
Jeffrey Massung <massung@gmail.com>
2020-09-28 22:51:43 -0500

    Update index name

--- src/views/Phenotype/store.js
+++ src/views/Phenotype/store.js
@@ -12,7 +12,7 @@ export default new Vuex.Store({
     modules: {
         bioPortal,
         kp4cd,
-        associations: bioIndex("gwas-associations"),
+        associations: bioIndex("global-associations"),
         annotations: bioIndex("global-enrichment")
     },
     state: {
