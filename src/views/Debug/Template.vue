27e47ea16ddf67a1b8a2ad11588a3ae9132fbc66
Preeti <singhpreeti236@gmail.com>
2020-09-29 11:16:42 -0400

    stage modification

--- src/views/Debug/Template.vue
+++ src/views/Debug/Template.vue
@@ -39,13 +39,13 @@
             </div>-->
 
             <div class="slideInRight animated card mdkp-card">
-                <div class="card-body">
+                <div class="col card-body">
                     <!-- Stage 1 -->
 
-                    <div class="card mdkp-card">
-                        <div class="card-body">
+                    <div class="row card mdkp-card">
+                        <div class="col-md-6 card-body">
                             <div v-if="$parent.isSignificantAssociationCommonVariation">
-                                <h5>Stage 1</h5>
+                                <h5>Common Variation: Is {{$store.state.geneName}} is genome wide significant</h5>
                                 <documentation
                                     name="hugecal.stage1.subheader"
                                     :content-fill="$parent.documentationMap"
@@ -78,20 +78,47 @@
                                 </div>
                             </div>
                             <b-col class="col-md-auto" v-else>
-                                <h5>Stage 1</h5>
+                                <h5>Common Variation: Is {{$store.state.geneName}} is genome wide significant</h5>
                                 <documentation
                                     name="hugecal.stage1.subheader"
                                     :content-fill="$parent.documentationMap"
                                 ></documentation>
-                                <strong>No Evidence.The {{$store.state.geneName}} is not genome wide significant</strong>
+                                <strong>No Evidence available.The {{$store.state.geneName}} is not genome wide significant</strong>
                             </b-col>
+
+                            <div class="card mdkp-card">
+                                <div class="card-body">
+                                    <h4
+                                        v-if="$store.state.phenotype"
+                                        class="card-title"
+                                    >Associations for {{$store.state.phenotype.name}} in {{$parent.symbolName}}</h4>
+
+                                    <locuszoom
+                                        v-if="$parent.region"
+                                        ref="locuszoom"
+                                        :chr="$parent.region.chromosome"
+                                        :start="$parent.region.start"
+                                        :end="$parent.region.end"
+                                        :refSeq="true"
+                                    >
+                                        <lz-associations-panel
+                                            :phenotype="$store.state.phenotype.name"
+                                            :finishHandler="$parent.updateAssociationsTable"
+                                        ></lz-associations-panel>
+                                    </locuszoom>
+                                    <!-- <associations-table
+                                            :phenotypes="$parent.phenotypes"
+                                            :associations="$parent.associationsData"
+                                    ></associations-table>-->
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                     <!-- Stage 2 -->
 
-                    <div class="slideInRight animated2 card mdkp-card">
-                        <div class="card-body" v-if="$store.state.effectorGeneData.category">
+                    <b-row class="slideInRight animated2">
+                        <b-col md="6" v-if="$store.state.effectorGeneData.category">
                             <div v-if="$store.state.effectorGeneData.category != 'CAUSAL'">
                                 <h5>Stage 2</h5>
                                 <documentation
@@ -142,68 +169,37 @@
                                 ></documentation>
                                 <strong>{{$store.state.geneName}} is Causal</strong>
                             </div>
-                        </div>
-                    </div>
+                        </b-col>
+                    </b-row>
 
                     <!-- Stage 3 -->
 
                     <!-- <b-row class="mb-3 border-right">
-                                    <b-col md="10">
-                                        <h5 class="card-title">Stage 3</h5>
-                                        <documentation
-                                            name="hugecal.stage3.subheader"
-                                            :content-fill="$parent.documentationMap"
-                                        ></documentation>
-                                        <div v-if="$parent.geneAssociations52k">
-                                            <div
-                                                v-if="$parent.geneAssociationsLoftee.length >0"
-                                                class="border"
-                                            >
-                                                <forest-plot :data="$parent.geneAssociationsLoftee"></forest-plot>
-                                            </div>
-                                            <div v-else>
-                                                <strong>There are no loss-of-function variants detected in this gene</strong>
-                                            </div>
-                                        </div>
-                                    </b-col>
+                        <b-col md="6">
+                            <h5 class="card-title">Stage 3</h5>
+                            <documentation
+                                name="hugecal.stage3.subheader"
+                                :content-fill="$parent.documentationMap"
+                            ></documentation>
+                            <div v-if="$parent.geneAssociations52k">
+                                <div v-if="$parent.geneAssociationsLoftee.length >0" class="border">
+                                    <forest-plot :data="$parent.geneAssociationsLoftee"></forest-plot>
+                                </div>
+                                <div v-else>
+                                    <strong>There are no loss-of-function variants detected in this gene</strong>
+                                </div>
+                            </div>
+                        </b-col>
                     </b-row>-->
                 </div>
             </div>
-            <div class="huge-calculator-show-associations-wrapper">
+            <!-- <div class="huge-calculator-show-associations-wrapper">
                 <b-button
                     block
                     v-on:click="() => $parent.showAssociations == true ? $parent.showAssociations = false : $parent.showAssociations=true"
                     class="btn-sm huge-calculator-show-associations to-previous-page"
                 >Show Associations Data</b-button>
-            </div>
-            <div class="card mdkp-card" v-if="$parent.showAssociations">
-                <div class="card-body">
-                    <div id="gwasAssocHolder" class="mdkp-card">
-                        <h4
-                            v-if="$store.state.phenotype"
-                            class="card-title"
-                        >Associations for {{$store.state.phenotype.name}} in {{$parent.symbolName}}</h4>
-
-                        <locuszoom
-                            v-if="$parent.region"
-                            ref="locuszoom"
-                            :chr="$parent.region.chromosome"
-                            :start="$parent.region.start"
-                            :end="$parent.region.end"
-                            :refSeq="true"
-                        >
-                            <lz-associations-panel
-                                :phenotype="$store.state.phenotype.name"
-                                :finishHandler="$parent.updateAssociationsTable"
-                            ></lz-associations-panel>
-                        </locuszoom>
-                        <associations-table
-                            :phenotypes="$parent.phenotypes"
-                            :associations="$parent.associationsData"
-                        ></associations-table>
-                    </div>
-                </div>
-            </div>
+            </div>-->
         </div>
 
         <!-- Footer-->
@@ -237,10 +233,10 @@ export default Vue.component("test", {
     methods: {}
 });
 
-window.onload = function() {
-    document.getElementById("fademe3s").style.opacity = 1;
-    document.getElementById("fademe6s").style.opacity = 1;
-};
+// window.onload = function() {
+//     document.getElementById("fademe3s").style.opacity = 1;
+//     document.getElementById("fademe6s").style.opacity = 1;
+// };
 </script>
 
 
