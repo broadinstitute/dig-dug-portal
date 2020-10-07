ce033c6943351d1018342a39eddb0ed5d31e8d0b
Quy <quy@broad.dev>
2020-10-07 18:14:00 -0400

    wip: test link for redirect

--- src/views/404/main.js
+++ src/views/404/main.js
@@ -6,6 +6,7 @@ import PageHeader from "@/components/PageHeader.vue";
 import PageFooter from "@/components/PageFooter.vue";
 import Alert from "@/components/Alert";
 import { pageMixin } from "@/mixins/pageMixin";
+import parseUrl from "url-parse";
 
 Vue.config.productionTip = false;
 
@@ -18,10 +19,22 @@ new Vue({
         Alert
     },
     created() {
+        //call function to check db for new links
+        console.log("created1", window.location.href);
+        console.log("created2", window.location.pathname);
+
         // get the disease group and set of phenotypes available
         this.$store.dispatch("bioPortal/getDiseaseGroups");
     },
     render(createElement) {
         return createElement(Template);
+    },
+    methods: {
+        lookUpNewLink() {
+            let oldLink = window.location.href;
+            // check DB
+            let newLink = "";
+            window.location.href = newLink;
+        }
     }
 }).$mount("#app");
