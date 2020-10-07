5b860da45a74cfb37b07fdc77019a0ab3d62bc47
Quy <quy@broad.dev>
2020-10-06 22:08:07 -0400

    resetting up page again

--- src/views/404/Template.vue
+++ src/views/404/Template.vue
@@ -1,3 +1,26 @@
 <template>
-    <div>404</div>
+    <div>
+        <!-- Header -->
+        <page-header
+            :disease-group="$parent.diseaseGroup"
+            :front-contents="$parent.frontContents"
+        ></page-header>
+
+        <!-- Body -->
+        <div class="container-fluid mdkp-body">
+            <div class="card mdkp-card">
+                <div class="row card-body">
+                    <div class="col-md-12 notfound">
+                        <div class="page"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer-->
+        <page-footer :disease-group="$parent.diseaseGroup"></page-footer>
+    </div>
 </template>
+<style>
+@import url("/css/404.css");
+</style>
--- src/views/404/main.js
+++ src/views/404/main.js
@@ -1,14 +1,25 @@
 import Vue from "vue";
 import Template from "./Template.vue";
+import store from "./store.js";
+
 import PageHeader from "@/components/PageHeader.vue";
 import PageFooter from "@/components/PageFooter.vue";
+import Alert from "@/components/Alert";
+import { pageMixin } from "@/mixins/pageMixin";
 
 Vue.config.productionTip = false;
 
 new Vue({
+    store,
+    mixins: [pageMixin],
     components: {
         PageHeader,
-        PageFooter
+        PageFooter,
+        Alert
+    },
+    created() {
+        // get the disease group and set of phenotypes available
+        this.$store.dispatch("bioPortal/getDiseaseGroups");
     },
     render(createElement) {
         return createElement(Template);
