d73bc2347e816cf4a0974540901d8a0cbe8aa363
Quy <quy@broad.dev>
2020-10-07 18:13:13 -0400

    add icons

--- src/mixins/pageMixin.js
+++ src/mixins/pageMixin.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
-import { BootstrapVue } from "bootstrap-vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
-
+Vue.use(IconsPlugin);
 export const pageMixin = {
     computed: {
         frontContents() {
