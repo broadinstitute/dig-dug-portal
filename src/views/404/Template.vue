d09e8ba6dd7a64aa2fb22db87a721d5c941c9728
Quy <quy@broad.dev>
2020-10-06 23:44:31 -0400

    wip: style page

--- src/views/404/Template.vue
+++ src/views/404/Template.vue
@@ -7,11 +7,44 @@
         ></page-header>
 
         <!-- Body -->
-        <div class="container-fluid mdkp-body">
+        <div class="container-fluid mdkp-body notfound">
             <div class="card mdkp-card">
                 <div class="row card-body">
-                    <div class="col-md-12 notfound">
-                        <div class="page"></div>
+                    <div class="col-md-12">
+                        <div class="page">
+                            <span class="number">4</span>
+                            <div class="DNA">
+                                <div class="helix"></div>
+                                <div class="bases at"></div>
+                                <div class="bases ta"></div>
+                                <div class="bases at"></div>
+                                <div class="bases at"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases ta"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases at"></div>
+                                <div class="bases at"></div>
+                                <div class="bases ta"></div>
+                                <div class="bases at"></div>
+                                <div class="bases at"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases cg"></div>
+                                <div class="bases gc"></div>
+                                <div class="bases at"></div>
+                                <div class="bases ta"></div>
+                                <div class="bases ta"></div>
+                                <div class="bases cg"></div>
+                                <div class="bases cg"></div>
+                            </div>
+                            <span class="number">4</span>
+                        </div>
+                        <div class="notice">
+                            <h4>Page Not Found</h4>
+                            <p>We couldn't find the page you're looking for.</p>
+                        </div>
                     </div>
                 </div>
             </div>
