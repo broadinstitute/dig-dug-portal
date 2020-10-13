84d0b36314c77c264e4e044bf0ba9cb120288148
DK Jang <dkjang@broadinstitute.org>
2020-10-13 13:33:16 -0400

    EGL landing page reworked

--- src/views/EffectorGenes/Template.vue
+++ src/views/EffectorGenes/Template.vue
@@ -8,18 +8,23 @@
             <div class="card mdkp-card dataset-page-header">
                 <div class="row card-body">
                     <div class="col-md-12">
-                        <h2
-                            v-if="$parent.pageInfo && $parent.pageInfo[0]"
-                        >{{ $parent.pageInfo[0].title[0].value }}</h2>
+                        <h2>Effector gene predictions</h2>
                     </div>
                 </div>
             </div>
             <div class="card mdkp-card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-12">
-                            <static-page-info-section :pageInfo="$parent.pageInfo"></static-page-info-section>
+                        <div class="col-md-12" style="font-size: 20px">
+                            <documentation name="egl.summaries.info"></documentation>
                         </div>
+                    </div>
+                    <div
+                        class="row"
+                        v-if="$parent.eglSummaries && $parent.eglSummaries[0]"
+                        v-for="item in $parent.eglSummaries"
+                    >
+                        <div class="col-md-12" v-html="item.body"></div>
                     </div>
                 </div>
             </div>
