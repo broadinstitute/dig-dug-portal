2c276b6e9d6d976a8a15d380009e138fd51bb075
Quy <quy@broad.dev>
2020-10-15 12:22:06 -0400

    use mixin

--- src/views/GAIT/main.js
+++ src/views/GAIT/main.js
@@ -3,14 +3,6 @@ import BootstrapVue from "bootstrap-vue";
 import Template from "./Template.vue";
 import store from "./store.js";
 
-import PageHeader from "@/components/PageHeader.vue";
-import PageFooter from "@/components/PageFooter.vue";
-import Alert, {
-    postAlert,
-    postAlertNotice,
-    postAlertError,
-    closeAlert
-} from "@/components/Alert";
 import { pageMixin } from "@/mixins/pageMixin";
 
 Vue.use(BootstrapVue);
@@ -19,7 +11,7 @@ Vue.config.productionTip = false;
 new Vue({
     store,
     mixins: [pageMixin],
-    components: { PageHeader, PageFooter, Alert },
+    components: {},
     render(createElement, context) {
         return createElement(Template);
     },
