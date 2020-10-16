171d7a6f31502db69ccae063d84dc8793a47af5f
Jeffrey Massung <massung@gmail.com>
2020-10-16 10:19:06 -0500

    limitWhile in bioindex utils

--- src/utils/bioIndexUtils.js
+++ src/utils/bioIndexUtils.js
@@ -48,10 +48,10 @@ export async function request(path, query_params) {
 /* Perform a BioIndex query.
  */
 export async function query(index, q, opts = {}) {
-    let { limit, resolveHandler, errHandler, finishHandler } = opts;
+    let { limit, resolveHandler, errHandler, finishHandler, limitWhile } = opts;
     let req = request(`/api/bio/query/${index}`, { q, limit });
 
-    return await processRequest(req, resolveHandler, errHandler, finishHandler);
+    return await processRequest(req, resolveHandler, errHandler, finishHandler, limitWhile);
 }
 
 /* Perform a BioIndex match.
@@ -64,16 +64,38 @@ export async function match(index, q, opts = {}) {
     return await processRequest(req, resolveHandler, errHandler, finishHandler);
 }
 
+/* Alters the json to filter results and stop continuing.
+ */
+function limitRecordsWhile(json, limitWhile) {
+    let data = json.data;
+
+    if (!!limitWhile) {
+        data = json.data.filter(limitWhile);
+
+        // no continuations if less data
+        if (data.length < json.count) {
+            json.continuation = null;
+        }
+    }
+
+    return data;
+}
+
 /* Follow continuations and continue reading all data.
  */
-async function processRequest(req, resolveHandler, errHandler, finishHandler) {
+async function processRequest(req, resolveHandler, errHandler, finishHandler, limitWhile) {
     let resp = await req;
     let json = await resp.json();
     let data = [];
 
+    // The `limitWhile` parameter assumes that the results of the
+    // query are ordered in some way (e.g. by pValue), so that
+    // when the test fails, we know that no more records will meet
+    // the criteria either.
+
     // resolve or error
     if (resp.status === 200) {
-        data = json.data;
+        data = limitRecordsWhile(json, limitWhile);
 
         if (!!resolveHandler) {
             resolveHandler(json);
@@ -88,13 +110,15 @@ async function processRequest(req, resolveHandler, errHandler, finishHandler) {
             json = await resp.json();
 
             if (resp.status === 200) {
-                data = data.concat(json.data);
+                data = data.concat(limitRecordsWhile(json, limitWhile));
 
                 if (!!resolveHandler) {
                     resolveHandler(json);
                 }
             }
         }
+
+        // done
         if (!!finishHandler) {
             finishHandler(json);
         }
