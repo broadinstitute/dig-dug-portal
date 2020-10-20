f4b69d7d13606e0ce50c7f746b8d6ef5eb7b84ad
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-10-19 17:59:57 -0700

    pageview to analytics

---
+++ src/components/analytics/GAPageView.vue
@@ -0,0 +1,10 @@
+<template></template>
+<script>
+import Vue from "vue";
+import gaUtils from "@/utils/gaUtils";
+export default Vue.component("ga-pageview", {
+    async created() {
+        await gaUtils.logPageView();
+    }
+});
+</script>
