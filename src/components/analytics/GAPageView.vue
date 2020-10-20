ca01740075771373b78141ab464e7d60f67ab40c
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-10-20 16:10:25 -0700

    post uri to pageview endpoint

--- src/components/analytics/GAPageView.vue
+++ src/components/analytics/GAPageView.vue
@@ -2,9 +2,11 @@
 <script>
 import Vue from "vue";
 import gaUtils from "@/utils/gaUtils";
+import { url, domain, subDomain } from "@/utils/hostUtils";
+
 export default Vue.component("ga-pageview", {
     async created() {
-        await gaUtils.logPageView();
+        await gaUtils.logPageView(window.location.href);
     }
 });
 </script>
