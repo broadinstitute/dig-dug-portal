83a296ef6cab3b569d3983cb611c961798235d9c
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-10-22 14:45:34 -0700

    current and previous pages as referrers

--- src/utils/gaUtils.js
+++ src/utils/gaUtils.js
@@ -73,18 +73,19 @@ const logErrorEvent = async function (context, message, page) {
  * Issue an Pageview notification for Google Analytics (GA) reporting, to the server.
  * `URI` is passed in by whoever is logging the pageview. Should usually be equal to whatever `window.location.href` returns.
  *
- * @param {string} [uri]
+ * @param {string} [currentPage]
+ * @param {string} [previousPage]
  * @return null
  * @public
  */
-const logPageView = async function (uri) {
-    const qs = queryString.stringify({ uri }, { skipNull: true });
+const logPageView = async function (currentPage, previousPage) {
+    const qs = queryString.stringify({ currentPage, previousPage }, { skipNull: true });
     return await fetch(`/pageview`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ uri: uri }),
+            body: JSON.stringify({ currentPage, previousPage }),
         })
         .then(response => {
             if (response) {
