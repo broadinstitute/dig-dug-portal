015f540ec7d3dfc2b57dd0f848ade63d3496fd92
Quy <quy@broad.dev>
2020-10-26 14:58:25 -0400

    wip: module to get data from ld server

---
+++ src/modules/ldServer.js
@@ -0,0 +1,29 @@
+/*
+! LD Server specific data
+*/
+
+export default {
+    namespaced: true,
+
+    state() {
+        return {
+            phenotypes: []
+        };
+    },
+    mutations: {
+        setPhenotypes(state, data) {
+            state.phenotypes = data;
+        }
+    },
+    actions: {
+        // fetch all the phenotypes available
+        async getPhenotypes({ state, commit }) {
+            let json = await fetch(
+                "https://ld.hugeamp.org/aggregation/metadata"
+            ).then(resp => resp.json());
+
+            // phenotypes for dataset
+            commit("setPhenotypes", json.data.phenotypeDatasets.phenotypes);
+        }
+    }
+};
