6cd0c22f1ea9458b4cb67462477a17c676a05aca
Quy <quy@broad.dev>
2020-11-05 21:59:30 -0500

    test multiple binIDs

--- src/views/GAIT/store.js
+++ src/views/GAIT/store.js
@@ -6,6 +6,8 @@ import kp4cd from "@/modules/kp4cd";
 import bioIndex from "@/modules/bioIndex";
 import ldServer from "@/modules/ldServer";
 import keyParams from "@/utils/keyParams";
+import { query } from "@/utils/bioIndexUtils";
+import { uniqBy } from "lodash";
 
 Vue.use(Vuex);
 
@@ -15,20 +17,22 @@ export default new Vuex.Store({
         kp4cd,
         ldServer,
         gene: bioIndex("gene"),
-        genes: bioIndex("gene-finder"),
-        burden: bioIndex("burden")
+        genes: bioIndex("gene-finder")
+        //burden: bioIndex("burden")
     },
     state: {
         // phenotypes needs to be an array so colors don't change!
         searchGene: "slc30a8", //!static for test
-        binID: "bin1_7" //!can move to data prop later
+        //binID: "bin1_7" //!can move to data prop later
+        binID: ["bin1_7", "bin7_7"],
+        variants: []
     },
     mutations: {
         setPhenotype(state, phenotype) {
             state.phenotype = phenotype;
         },
-        setBurden(state, data) {
-            state.burden = data;
+        setVariants(state, data) {
+            state.variants = data;
         }
     },
     getters: {
@@ -62,13 +66,23 @@ export default new Vuex.Store({
             context.dispatch("genes/query", geneQuery);
         },
 
-        queryBurden(context) {
+        async queryBurden(context) {
             let gene = context.state.searchGene;
             let binID = context.state.binID;
-            let q = `${gene},${binID}`;
 
-            //TODO: set url params for bookmark
-            context.dispatch("burden/query", { q });
+            // let q = `${gene},${binID}`;
+
+            // //TODO: set url params for bookmark
+            // context.dispatch("burden/query", { q });
+            let queries = binID.map(bin => query("burden", `${gene},${bin}`));
+            let data = await Promise.all(queries).then(results =>
+                results.flatMap(data => data)
+            );
+
+            let unique = uniqBy(data, "varId");
+
+            context.commit("setVariants", unique);
+            //console.log("data", unique);
         }
     }
 });
