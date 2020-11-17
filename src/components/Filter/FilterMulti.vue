ffaacd3d50ad527ac24361e3fcba584f596c4454
Quy <quy@broad.dev>
2020-11-17 13:07:34 -0500

    add color prop to multi

--- src/components/Filter/FilterMulti.vue
+++ src/components/Filter/FilterMulti.vue
@@ -3,34 +3,38 @@
         class="filter-col-md"
         :field="field"
         :predicate="(string, selection) => string === selection"
-        :pillFormatter="filterDefinition => `${filterDefinition.field} == ${filterDefinition.threshold}`"
+        :pillFormatter="
+            (filterDefinition) =>
+                `${filterDefinition.field} = ${filterDefinition.threshold}`
+        "
         :options="options"
         :color="color"
         :multiple="true"
         :inclusive="inclusive"
-        :splitBy="splitBy">
-        <slot>
-        </slot>
+        :splitBy="splitBy"
+    >
+        <slot> </slot>
     </filter-control>
 </template>
 <script>
 import Vue from "vue";
-import FilterControl from "./FilterControl"
-export default Vue.component('filter-multi-control', {
+import FilterControl from "./FilterControl";
+export default Vue.component("filter-multi-control", {
     props: {
         field: String,
         options: {
             type: Array,
             required: false,
         },
+        color: String,
         splitBy: String,
         inclusive: {
             type: Boolean,
             default: true,
-        }
+        },
     },
     components: {
         FilterControl,
     },
-})
+});
 </script>
