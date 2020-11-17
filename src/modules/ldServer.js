44bb30d25a157f908ae2d904f1476ac1f3d02ba0
Quy <quy@broad.dev>
2020-11-17 11:35:02 -0500

    add dataset param

--- src/modules/ldServer.js
+++ src/modules/ldServer.js
@@ -2,6 +2,8 @@
 ! LD Server specific data
 */
 
+import { lowerCase } from "lodash";
+
 export default {
     namespaced: true,
 
@@ -41,7 +43,10 @@ export default {
                 json.data[0].phenotypeDatasets[0].phenotypes
             );
         },
-        async getCovariances(context, { variants, phenotype }) {
+        async getCovariances(
+            context,
+            { variants, phenotype, dataset = "52k" }
+        ) {
             //console.log("state", state);
             //let variants = state.variants;
             // let gene = await state.gene.data[0];
@@ -52,30 +57,58 @@ export default {
             //console.log("variants", variants);
             //console.log("gene", gene);
             //console.log("context", context);
+            let query = {};
             let region = context.rootGetters.region;
-            let query = {
-                chrom: region.chromosome,
-                start: region.start,
-                stop: region.end,
-                genotypeDataset: 1,
-                phenotypeDataset: 1,
-                phenotype: phenotype, //string only, no array
-                samples: "ALL",
-                genomeBuild: "GRCh37",
-                maskDefinitions: [
-                    {
-                        id: 1, //integer, required
-                        name: "Fetch Data", //required
-                        description: "Default", //required
-                        genome_build: "GRCh37",
-                        group_type: "GENE",
-                        identifier_type: "ENSEMBL",
-                        groups: {
-                            VARIANTS: variants
+
+            if (dataset == "52k") {
+                query = {
+                    chrom: region.chromosome,
+                    start: region.start,
+                    stop: region.end,
+                    genotypeDataset: 1,
+                    phenotypeDataset: 1,
+                    phenotype: phenotype, //string only, no array
+                    samples: "ALL",
+                    genomeBuild: "GRCh37",
+                    maskDefinitions: [
+                        {
+                            id: 1, //integer, required
+                            name: "Fetch Data", //required
+                            description: "Default", //required
+                            genome_build: "GRCh37",
+                            group_type: "GENE",
+                            identifier_type: "ENSEMBL",
+                            groups: {
+                                VARIANTS: variants
+                            }
                         }
-                    }
-                ]
-            };
+                    ]
+                };
+            } else {
+                //TopMed dataset
+                query = {
+                    chrom: region.chromosome,
+                    start: region.start,
+                    stop: region.end,
+                    summaryStatisticDataset: 1,
+                    samples: "ALL",
+                    genomeBuild: "GRCh37",
+                    maskDefinitions: [
+                        {
+                            id: 1, //integer, required
+                            name: "Fetch Data", //required
+                            description: "Default", //required
+                            genome_build: "GRCh37",
+                            group_type: "GENE",
+                            identifier_type: "ENSEMBL",
+                            groups: {
+                                VARIANTS: variants
+                            }
+                        }
+                    ]
+                };
+            }
+
             //console.log("query", query);
             let json = await fetch(
                 "https://ld.hugeamp.org/aggregation/covariance",
