a1461c03f7713edb024480f47e78aa802ac48aa9
Jeffrey Massung <massung@gmail.com>
2020-11-18 13:18:42 -0600

    Fixed title.

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -131,7 +131,12 @@ export default Vue.component("page-header", {
     watch: {
         diseaseGroup(group) {
             if (!!group && !!group.title) {
-                document.title = group.title;
+                let s = document.title.split(" - ");
+
+                // NB: If the <title> changes in index.html to use a different
+                //     separator other than ' - ', please update this code.
+                s[0] = group.title;
+                document.title = s.join(" - ");
 
                 // find the shortcut icon
                 let links = document.getElementsByTagName("link");
