4b6ca6b848e12e3fe74a54cc320e6768227bd976
Preeti <singhpreeti236@gmail.com>
2020-12-02 13:01:41 -0500

    fix the header

--- src/views/ComplicationsViewer/Template.vue
+++ src/views/ComplicationsViewer/Template.vue
@@ -4,7 +4,7 @@
         <div class="container-fluid mdkp-body">
             <div class="card mdkp-card">
                 <div class="card-body">
-                    <h1 class="card-title">Gene Finder</h1>
+                    <h1 class="card-title">Complications Viewer</h1>
 
                     <documentation style="margin-bottom: 30px" name="tools.genefinder.subheader"></documentation>
 
