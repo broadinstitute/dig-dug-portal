f2844d4063d31d6e79f70882bfa8417298905bba
Preeti <singhpreeti236@gmail.com>
2020-12-17 23:51:41 -0500

    the color bar component will take bayes factor as a prop and calculate it's category

--- src/components/ColorBarPlot.vue
+++ src/components/ColorBarPlot.vue
@@ -2,13 +2,31 @@
     <div :id="elementid">
         <div style="width: 700px">
             <br />
-            <div v-if="this.category == 'CAUSAL' " class="arrow-up causalclass"></div>
-            <div v-if="this.category == 'STRONG'" class="arrow-up strongclass"></div>
-            <div v-if="this.category == 'MODERATE'" class="arrow-up moderateclass"></div>
-            <div v-if="this.category == 'POSSIBLE'" class="arrow-up possibleclass"></div>
-            <div v-if="this.category == 'POTENTIAL'" class="arrow-up potentialclass"></div>
-            <div v-if="this.category == 'WEAK'" class="arrow-up weakclass"></div>
-            <div v-if="this.category == 'NO'" class="arrow-up noEvidenceclass"></div>
+            <div
+                v-if="determineCategory(this.bayesfactor) == 'CAUSAL' "
+                class="arrow-up causalclass"
+            ></div>
+            <div
+                v-if="determineCategory(this.bayesfactor) == 'STRONG'"
+                class="arrow-up strongclass"
+            ></div>
+            <div
+                v-if="determineCategory(this.bayesfactor) == 'MODERATE'"
+                class="arrow-up moderateclass"
+            ></div>
+            <div
+                v-if="determineCategory(this.bayesfactor) == 'POSSIBLE'"
+                class="arrow-up possibleclass"
+            ></div>
+            <div
+                v-if="determineCategory(this.bayesfactor) == 'POTENTIAL'"
+                class="arrow-up potentialclass"
+            ></div>
+            <div v-if="determineCategory(this.bayesfactor) == 'WEAK'" class="arrow-up weakclass"></div>
+            <div
+                v-if="determineCategory (this.bayesfactor)== 'NO'"
+                class="arrow-up noEvidenceclass"
+            ></div>
             <div>
                 <div class="container">
                     <div class="row">
@@ -27,11 +45,9 @@
                         <div class="col-sm variationPotential">
                             <strong>Potential</strong>
                         </div>
-
                         <div class="col-sm variationWeak">
                             <strong>Weak</strong>
                         </div>
-
                         <div class="col- variationNoEvidence">
                             <strong>No Evidence</strong>
                         </div>
@@ -47,16 +63,7 @@
 import Vue from "vue";
 
 export default Vue.component("color-bar-plot", {
-    props: {
-        category: {
-            type: String,
-            required: true,
-            default: "NO"
-        },
-        elementid: {
-            type: String
-        }
-    },
+    props: ["elementid", "bayesfactor"],
 
     data() {
         return {};
@@ -64,7 +71,27 @@ export default Vue.component("color-bar-plot", {
     components: {},
 
     mounted() {},
-    methods: {},
+    methods: {
+        determineCategory(bayesfactor) {
+            let category;
+            if (bayesfactor < 2.1) {
+                category = "NO";
+            } else if (bayesfactor >= 2.1 && bayesfactor < 7.26) {
+                category = "WEAK";
+            } else if (bayesfactor >= 7.26 && bayesfactor < 16.5) {
+                category = "POTENTIAL";
+            } else if (bayesfactor >= 16.5 && bayesfactor < 36.3) {
+                category = "POSSIBLE";
+            } else if (bayesfactor >= 36.3 && bayesfactor < 82.5) {
+                category = "MODERATE";
+            } else if (bayesfactor >= 82.5 && bayesfactor < 1650) {
+                category = "STRONG";
+            } else if (bayesfactor >= 1650) {
+                category = "CAUSAL";
+            }
+            return category;
+        }
+    },
 
     computed: {},
 
