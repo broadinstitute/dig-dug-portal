0bf8e332bb67bb481aadabec69ddc227c7b679c3
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-12-21 18:36:17 -0800

    play with resolution

--- src/views/Debug/Template.vue
+++ src/views/Debug/Template.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+        <input type="number" v-model="$parent.resolution"/>
         <div v-for="item in $parent.items"
             v-bind:key="item[0]">
              <svg width="10" height="10">
