cd1be38aed3fbf20225033afefbb01eb531702fe
kbruskiewicz <kenneth.bruskiewicz@gmail.com>
2020-12-21 18:41:29 -0800

    playing with resolution

--- src/views/Debug/Template.vue
+++ src/views/Debug/Template.vue
@@ -5,7 +5,7 @@
             v-bind:key="item[0]">
              <svg width="10" height="10">
 
-             <rect  width="10" height="10" :style="`fill:${item[1]};stroke-width:1;stroke:rgb(0,0,0)`" />
+             <rect  width="10" height="10" :style="`fill:${item[1]};`" />
            </svg>
 
         </div>
