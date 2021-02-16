90ecb46fcd55fef91272224c9581718f9196c633
Quy <quy@broad.dev>
2021-02-16 12:06:16 -0500

    wip: test data

--- src/components/VariantSearch.vue
+++ src/components/VariantSearch.vue
@@ -13,6 +13,16 @@
             >
                 <div class="label">Gene</div>
             </filter-enumeration-control>
+            <b-col class="divider"></b-col>
+            <filter-enumeration-control
+                ref="dataset"
+                :field="'dataset'"
+                placeholder="Select a dataset ..."
+                :options="datasets"
+                :multiple="true"
+            >
+                <div class="label">Dataset</div></filter-enumeration-control
+            >
         </criterion-list-group>
         <div class="function">
             <b-button variant="primary" @click="searchVariants"
@@ -20,7 +30,7 @@
             >
         </div>
         <b-table
-            :items="tableData"
+            :items="$options.testData"
             :per-page="perPage"
             :current-page="currentPage"
         ></b-table>
@@ -37,8 +47,10 @@ import Vue from "vue";
 import { match } from "@/utils/bioIndexUtils";
 import CriterionListGroup from "@/components/criterion/group/CriterionListGroup.vue";
 import FilterEnumeration from "@/components/criterion/FilterEnumeration.vue";
+import testData from "@/views/VariantSearch/data.json";
 
 export default Vue.component("variant-search", {
+    testData,
     components: {
         CriterionListGroup,
         FilterEnumeration,
@@ -49,6 +61,7 @@ export default Vue.component("variant-search", {
             matchingGenes: [],
             perPage: 10,
             currentPage: 1,
+            datasets: ["dataset1", "dataset2"],
         };
     },
     computed: {
@@ -59,16 +72,47 @@ export default Vue.component("variant-search", {
                 })
                 .map((v) => v.threshold);
         },
-        tableData() {
-            if (
-                this.$store.state.variants.data &&
-                this.$store.state.variants.data.length
-            ) {
-                return this.$store.state.variants.data;
-            } else {
-                return [];
-            }
+        selectedDataset() {
+            return this.searchCriteria
+                .filter((v) => {
+                    return v.field === "dataset";
+                })
+                .map((v) => v.threshold);
         },
+        baseFields() {
+            let fields = Object.keys(testData[0]);
+            return fields.filter((v) => v !== "datasets");
+        },
+        datasetFields() {
+            let data1Fields = testData[0].datasets[0].data;
+        },
+        tableFields() {
+            if (this.selectedDataset.length > 0) {
+                return testData.filter((v) => {
+                    console.log(v);
+
+                    return v.datasets.every((d) => {
+                        console.log("d", d.name);
+                        console.log("selected", this.selectedDataset);
+                        let ans = this.selectedDataset.includes(d.name);
+                        console.log("ans", ans);
+                        return ans;
+                    });
+                });
+                //return testData[0].datasets;
+            } else return [];
+        },
+        //This works to display all data fro BI
+        // tableData() {
+        //     if (
+        //         this.$store.state.variants.data &&
+        //         this.$store.state.variants.data.length
+        //     ) {
+        //         return this.$store.state.variants.data;
+        //     } else {
+        //         return [];
+        //     }
+        // },
         rows() {
             if (this.tableData) return this.tableData.length;
         },
