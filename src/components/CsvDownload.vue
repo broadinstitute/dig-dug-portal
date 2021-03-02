0173b3fc6760c545e417fb3b041076d863077b17
Quy <quy@broad.dev>
2021-03-02 10:40:52 -0500

    wip: new download csv component

---
+++ src/components/CsvDownload.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-btn @click="downloadCsv(data, filename)">Download CSV</b-btn>
+</template>
+<script>
+import Vue from "vue";
+import uiUtils from "@/utils/uiUtils";
+
+export default Vue.component("csv-download", {
+    props: ["data", "filename"],
+    modules: {
+        uiUtils,
+    },
+    methods: {
+        downloadCsv(DATA, FILENAME) {
+            //data is in json format
+            uiUtils.convertJson2Csv(DATA, FILENAME);
+        },
+    },
+});
+</script>
