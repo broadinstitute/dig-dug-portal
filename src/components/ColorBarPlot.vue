b9dcc5c9b5fa47f79dd98c8a0a038328b374a1e1
Preeti <singhpreeti236@gmail.com>
2021-03-26 11:51:56 -0400

    change the categories and add one more prop for displaying the score

--- src/components/ColorBarPlot.vue
+++ src/components/ColorBarPlot.vue
@@ -1,37 +1,40 @@
+
 <template>
     <div :id="elementid">
         <div class="color-bar-plot-wrapper">
+            <!-- <span>
+                 {{this.score}} HuGe score
+            </span>-->
+            <span
+                style="font-size:12px"
+            >*Compelling: HuGe Score >= 350 Extreme: >=100 Very Strong: >=30 Strong: >=10 Moderate: >=3 Anecdotal: >1 No Evidence: <=1</span>
             <div class="row color-bars-wrapper">
-                <div class="each-bar-section variationCausal">Causal</div>
-                <div class="each-bar-section variationStrong">Strong</div>
-                <div class="each-bar-section variationModerate">Moderate</div>
-                <div class="each-bar-section variationPossible">Possible</div>
-                <div class="each-bar-section variationPotential">Potential</div>
-                <div class="each-bar-section variationWeak">Weak</div>
-                <div class="each-bar-section variationEquivocal">Equivocal</div>
+                <div class="each-bar-section variationStrong">Compelling</div>
+                <div class="each-bar-section variationModerate">Extreme</div>
+                <div class="each-bar-section variationPossible">Very Strong</div>
+                <div class="each-bar-section variationPotential">Strong</div>
+                <div class="each-bar-section variationWeak">Moderate</div>
+                <div class="each-bar-section variationEquivocal">Anecdotal</div>
                 <div class="each-bar-section variationNoEvidence">No Evidence</div>
             </div>
             <div class="row">
                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Causal'" class="arrow-up causalclass"></div>
+                    <div v-if="this.category == 'Compelling'" class="arrow-up causalclass"></div>
                 </div>
                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Strong'" class="arrow-up strongclass"></div>
+                    <div v-if="this.category == 'Extreme'" class="arrow-up strongclass"></div>
                 </div>
                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Moderate'" class="arrow-up moderateclass"></div>
+                    <div v-if="this.category == 'Very Strong'" class="arrow-up moderateclass"></div>
                 </div>
                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Possible'" class="arrow-up possibleclass"></div>
+                    <div v-if="this.category == 'Strong'" class="arrow-up possibleclass"></div>
                 </div>
                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Potential'" class="arrow-up potentialclass"></div>
+                    <div v-if="this.category == 'Moderate'" class="arrow-up potentialclass"></div>
                 </div>
                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Weak'" class="arrow-up weakclass"></div>
-                </div>
-                 <div class="each-bar-section">
-                    <div v-if="this.category == 'Equivocal'" class="arrow-up equivocalclass"></div>
+                    <div v-if="this.category == 'Anecdotal'" class="arrow-up weakclass"></div>
                 </div>
                 <div class="each-bar-section">
                     <div v-if="this.category == 'No'" class="arrow-up noEvidenceclass"></div>
@@ -54,6 +57,10 @@ export default Vue.component("color-bar-plot", {
             type: String,
             required: true,
             default: "NO"
+        },
+        score: {
+            type: Number,
+            reuired: false
         }
     },
 
