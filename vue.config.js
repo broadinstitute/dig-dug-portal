95466476bee99ae24b3565a9204f91fed1cadc2d
DK Jang <dkjang@broadinstitute.org>
2021-05-21 15:07:35 -0400

    epigenomicdatasets.html added

--- vue.config.js
+++ vue.config.js
@@ -136,6 +136,13 @@ let pages = {
         title: "Epigenetic Datasets",
         chunks: ["chunk-vendors", "chunk-common", "epigeneticdatasets"]
     },
+    epigenomicdatasets: {
+        entry: "src/views/EpigenomicDatasets/main.js",
+        template: "public/index.html",
+        filename: "epigenomicdatasets.html",
+        title: "Epigenomic Datasets",
+        chunks: ["chunk-vendors", "chunk-common", "epigenomicdatasets"]
+    },
     effectorgenes: {
         entry: "src/views/EffectorGenes/main.js",
         template: "public/index.html",
