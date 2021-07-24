7485fefc763303b7ef4d2885c5ddca175d9337e3
Preeti <singhpreeti236@gmail.com>
2021-07-24 02:55:33 -0400

    initial code for customized table for combined evidence

---
+++ src/components/HugeCalTable.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <b-table small :fields="fields" :items="items" responsive="sm">
+            <!-- A virtual column -->
+            <template #cell(index)="data">{{ data.index + 1 }}</template>
+
+            <!-- A custom formatted column -->
+            <template #cell(name)="data">
+                <b class="text-info">{{ data.value.last.toUpperCase() }}</b>,
+                <b>{{ data.value.first }}</b>
+            </template>
+
+            <!-- A virtual composite column -->
+            <template
+                #cell(nameage)="data"
+            >{{ data.item.name.first }} is {{ data.item.age }} years old</template>
+
+            <!-- Optional default data cell scoped slot -->
+            <template #cell()="data">
+                <i>{{ data.value }}</i>
+            </template>
+        </b-table>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueTypeaheadBootstrap from "vue-typeahead-bootstrap";
+import Autocomplete from "@/components/Autocomplete.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { match } from "@/utils/bioIndexUtils";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.component("vue-typeahead-bootstrap", VueTypeaheadBootstrap);
+Vue.component("autocomplete", Autocomplete);
+
+export default Vue.component("hugecal-table", {
+    props: [],
+
+    data() {
+        return {
+            fields: [
+                // A virtual column that doesn't exist in items
+                "index",
+                // A column that needs custom formatting
+                { key: "name", label: "Full Name" },
+                // A regular column
+                "age",
+                // A regular column
+                "sex",
+                // A virtual column made up from two fields
+                { key: "nameage", label: "First name and age" }
+            ],
+            items: [
+                { name: { first: "John", last: "Doe" }, sex: "Male", age: 42 },
+                {
+                    name: { first: "Jane", last: "Doe" },
+                    sex: "Female",
+                    age: 36
+                },
+                {
+                    name: { first: "Rubin", last: "Kincade" },
+                    sex: "Male",
+                    age: 73
+                },
+                {
+                    name: { first: "Shirley", last: "Partridge" },
+                    sex: "Female",
+                    age: 62
+                }
+            ]
+        };
+    },
+    computed: {},
+    methods: {}
+});
+</script>
