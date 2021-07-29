5d4c421445458bbe53552ea475e9ed6526acf809
Quy <quy@broad.dev>
2021-07-29 10:35:38 -0400

    wip: sidebar

--- src/views/GEM/Template.vue
+++ src/views/GEM/Template.vue
@@ -13,6 +13,25 @@
                     <documentation name="region.igv.subheader"></documentation>
                 </div>
             </div>
+            <div class="card mdkp-card">
+                <div class="card-body">something</div>
+            </div>
         </div>
+
+        <b-button v-b-toggle.sidebar-1>Show Criteria</b-button>
+        <b-sidebar id="sidebar-1" title="Criteria" shadow>
+            <div class="px-3 py-2">
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo
+                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
+                    risus, porta ac consectetur ac, vestibulum at eros.
+                </p>
+                <b-img
+                    src="https://picsum.photos/500/500/?image=54"
+                    fluid
+                    thumbnail
+                ></b-img>
+            </div>
+        </b-sidebar>
     </div>
 </template>
