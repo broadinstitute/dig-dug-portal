05d71848bc726e0eed974a521fa266e006f698f6
hanzhang2021 <zhanghanri2019@gmail.com>
2021-09-30 09:10:30 -0400

    Create Template.vue

---
+++ src/views/BCHVariantSearch/Template.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <page-header
+            :disease-group="$parent.diseaseGroup"
+            :front-contents="$parent.frontContents"
+        ></page-header>
+        <div class="container-fluid mdkp-body">
+            <div class="card mdkp-card">
+                <div class="card-body temporary-card">
+                    <documentation
+                        name="variantsearch.header.info"
+                    ></documentation>
+                </div>
+            </div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <h1 class="card-title">Variant Search</h1>
+
+                    <transition name="fade"
+                        ><b-alert show v-if="$parent.selectedGene.length === 0"
+                            >Please select a gene.</b-alert
+                        >
+                        <!--
+                        <b-alert
+                            show
+                            v-else-if="$parent.selectedDataset.length === 0"
+                            >Please select a dataset.</b-alert
+                        > -->
+                    </transition>
+                    <criterion-list-group
+                        v-model="$parent.searchCriteria"
+                        :header="'Search Criteria'"
+                    >
+                        <filter-enumeration-control
+                            ref="gene"
+                            :field="'gene'"
+                            placeholder="Select a gene ..."
+                            :options="$parent.matchingGenes"
+                            @input-change="$parent.lookupGenes($event)"
+                        >
+                            <div class="label">Gene</div>
+                        </filter-enumeration-control>
+                        <!-- <b-col class="divider"></b-col>
+                        <filter-enumeration-control
+                            ref="dataset"
+                            :field="'dataset'"
+                            placeholder="Select a dataset ..."
+                            :options="$parent.datasets"
+                            :multiple="true"
+                        >
+                            <div class="label">
+                                Dataset
+                            </div></filter-enumeration-control
+                        > -->
+                    </criterion-list-group>
+
+                    <div class="card mdkp-card">
+                        <div class="card-body">
+                            <div v-if="$parent.selectedGene.length > 0">
+                                <h4 class="card-title">
+                                    Gene-Variants
+                                </h4>
+
+                                <bch-variant-search
+                                v-bind:variants="$store.state.variants.data"
+                                :gene="$parent.selectedGene"
+                                ></bch-variant-search>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <bch-variant-search
+                        :gene="$parent.selectedGene"
+                    ></bch-variant-search> -->
+                </div>
+            </div>
+        </div>
+        <page-footer :disease-group="$parent.diseaseGroup"></page-footer>
+    </div>
+</template>
+
+<style>
+</style>
