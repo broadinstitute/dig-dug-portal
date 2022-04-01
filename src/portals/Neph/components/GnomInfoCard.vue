33bd31147100a745d7009d401ba49147a8d8a984
hanzhang2021 <zhanghanri2019@gmail.com>
2022-04-01 12:27:09 -0400

    Update GnomInfoCard.vue

--- src/portals/Neph/components/GnomInfoCard.vue
+++ src/portals/Neph/components/GnomInfoCard.vue
@@ -1,33 +1,42 @@
 <template>
-    <div class="center">
-        <b-table-simple borderless fixed small>
-            <b-tbody>
-                <b-tr>
-                    <b-th>&nbsp;</b-th><b-th>Exome</b-th><b-th>Genome</b-th><b-th>Total</b-th>
-                </b-tr>
-                <b-tr>
-                    <b-td>Filters</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-                <b-tr>
-                    <b-td>Allele Count</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-                <b-tr>
-                    <b-td>Allele Number</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-                <b-tr>
-                    <b-td>Allele Frequency</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-                <b-tr>
-                    <b-td>Popmax Filtering AF More information<br/>(95% confidence)</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-                <b-tr>
-                    <b-td>Number of homozygotes</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-                <b-tr>
-                    <b-td>Mean depth of coverage</b-td><b-td></b-td><b-td></b-td><b-td></b-td>
-                </b-tr>
-            </b-tbody>
-        </b-table-simple>
+    <div>
+        <div v-if="rows > 0">
+            <b-row>
+                <b-col class="text-right mb-2">
+                    <csv-download
+                        v-if="tableData.length"
+                        :data="tableData"
+                        filename="variant-phenotypes"
+                    ></csv-download
+                ></b-col>
+            </b-row>
+            <div v-show="tableData.length">
+                <b-table
+                    hover
+                    small
+                    sort-icon-left
+                    responsive="sm"
+                    :items="tableData"
+                    :fields="fields"
+                    :per-page="perPage"
+                    :tbody-tr-class="rowPickClass"
+                    >
+                    
+                </b-table>
+                <b-pagination
+                    class="pagination-sm justify-content-center"
+                    v-model="currentPage"
+                    :total-rows="rows"
+                    :per-page="perPage"
+                ></b-pagination>
+            </div>
+        </div>
+        <div v-else>
+            <b-alert show variant="warning" class="text-center">
+                <b-icon icon="exclamation-triangle"></b-icon> No predicted
+                transcript consequences found.</b-alert
+            >
+        </div>
     </div>
 </template>
 
@@ -39,6 +48,21 @@ import { match, query } from "@/utils/bioIndexUtils";
 export default Vue.component("gnominfo-card", {
     props: ["variantId"],
     data() {
+        return {
+            
+            fields: [
+                {
+					key: "name",
+					label: "Field Name",
+				},
+				{
+					key: "value",
+					label: "Value",
+				},
+            ],
+            gnomAd_info: [],
+            
+        };
     },
     created() {
 		if (this.variantId) {
@@ -63,6 +87,27 @@ export default Vue.component("gnominfo-card", {
                 });
             }
             return dataRows;*/
+        },
+    },
+    methods: {
+        async searchVariants() {
+            //console.log("variant id:" + this.variantId);
+            let varinfo = this.variantId.split(":");
+            let searchquery = varinfo[0]+":"+varinfo[1];
+            this.variant = await query("variant-phenotype",searchquery,{},true);
+            let gnomdisplay = [];
+            let j = 0;
+
+            for (k in gnomAD_info) {
+                gnomdisplay[j]={};
+                gnomdisplay[j].name = k;
+                gnomdisplay[j].value = gnomAD_info[k];
+                j++
+            }
+            
+            this.gnomAD_info = gnomdisplay;
+
+            //console.log("results:"+JSON.stringify(this.variant[0].hprecords));
         },
     },
 })
