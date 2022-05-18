640e82476626d71c9a0aecfb10789fec97912d05
Quy <quy@broad.dev>
2022-05-18 12:21:47 -0400

    update hpoterms for nephkp

--- src/portals/Neph/components/VariantPhenotypeTable.vue
+++ src/portals/Neph/components/VariantPhenotypeTable.vue
@@ -20,7 +20,7 @@
                     :fields="fields"
                     :per-page="perPage"
                     :tbody-tr-class="rowPickClass"
-                    >
+                >
                     <template #cell(varId)="data">
                         <a :href="`/variant.html?variant=${data.item.varId}`">{{
                             data.item.varId
@@ -71,7 +71,7 @@
                     >
                     <template #cell(siftPrediction)="data">
                         {{ siftFormatter(data.item.siftPrediction) }}
-                    </template> 
+                    </template>
                 </b-table>
                 <b-pagination
                     class="pagination-sm justify-content-center"
@@ -100,57 +100,38 @@ export default Vue.component("variant-phenotype-table", {
     data() {
         return {
             HPOTerms: {
-				"HP-0000119": "Abnormality of the genitourinary system",
-				"HP-0000152": "Abnormality of head or neck",
-				"HP-0000478": "Abnormality of the eye",
-				"HP-0000598": "Abnormality of the ear",
-				"HP-0000707": "Abnormality of the nervous system",
-				"HP-0000769": "Abnormality of the breast",
-				"HP-0000818": "Abnormality of the endocrine system",
-				"HP-0001197": "Abnormality of prenatal development or birth",
-				"HP-0001507": "Growth abnormality",
-				"HP-0001574": "Abnormality of the integument",
-				"HP-0001608": "Abnormality of the voice",
-				"HP-0001626": "Abnormality of the cardiovascular system",
-				"HP-0001871": "Abnormality of blood and blood-forming tissues",
-				"HP-0001939": "Abnormality of metabolism/homeostasis",
-				"HP-0002086": "Abnormality of the respiratory system",
-				"HP-0002664": "Neoplasm",
-				"HP-0002715": "Abnormality of the immune system",
-				"HP-0025031": "Abnormality of the digestive system",
-				"HP-0025142": "Constitutional symptom",
-				"HP-0025354": "Abnormal cellular phenotype",
-				"HP-0033127": "Abnormality of the musculoskeletal system",
-				"HP-0040064": "Abnormality of limbs",
-				"HP-0045027": "Abnormality of the thoracic cavity",
-				"AllControl": "Controls",
-			},
+                Healthy: "Healthy",
+                NephSyndSteroidSensitive: "Steroid Sensitive NephSynd",
+                NephSyndUncategorized: "Uncategorized NephSynd",
+                NephSyndSteroidResistant: "Steroid Resistant NephSynd",
+                AllNyphroticSyndCases: "All Cases NephSynd",
+            },
             fields: [
                 {
-					key: "hpoterms",
-					label: "Phenotype",
-				},
-				{
-					key: "allelecount",
-					label: "Allele Count",
-					sortable: true,
-				},
-				{
-					key: "allelnumber",
-					label: "Allele Number",
-					sortable: true,
-				},
+                    key: "hpoterms",
+                    label: "Phenotype",
+                },
+                {
+                    key: "allelecount",
+                    label: "Allele Count",
+                    sortable: true,
+                },
+                {
+                    key: "allelnumber",
+                    label: "Allele Number",
+                    sortable: true,
+                },
 
-				{
-					key: "allelefrequency",
-					label: "Allele Frequency",
-					sortable: true,
-				},
-				{
-					key: "TWO_ALT_GENO_CTS",
-					label: "Homozygotes",
-					sortable: true,
-				},
+                {
+                    key: "allelefrequency",
+                    label: "Allele Frequency",
+                    sortable: true,
+                },
+                {
+                    key: "TWO_ALT_GENO_CTS",
+                    label: "Homozygotes",
+                    sortable: true,
+                },
             ],
             hprecords: [],
             perPage: 24,
@@ -158,21 +139,21 @@ export default Vue.component("variant-phenotype-table", {
         };
     },
     created() {
-		if (this.variantId) {
-			this.searchVariants();
-		}
-	},
-	computed: {
+        if (this.variantId) {
+            this.searchVariants();
+        }
+    },
+    computed: {
         rows() {
             return this.tableData.length;
         },
         tableData() {
             if (this.hprecords && this.hprecords.length) {
                 //console.log("here:"+this.hprecords);
-				return this.hprecords;
-			} else {
-				return [];
-			}
+                return this.hprecords;
+            } else {
+                return [];
+            }
             /*let dataRows = this.variant.hprecords;
             if (!!this.filter) {
                 dataRows = dataRows.filter((association) => {
@@ -190,36 +171,32 @@ export default Vue.component("variant-phenotype-table", {
         async searchVariants() {
             //console.log("variant id:" + this.variantId);
             let varinfo = this.variantId.split(":");
-            let searchquery = varinfo[0]+":"+varinfo[1];
-            this.variant = await query("variant-phenotype",searchquery,{},true);
+            let searchquery = varinfo[0] + ":" + varinfo[1];
+            this.variant = await query("variants", searchquery, {}, true);
             let hpdisplay = [];
             let j = 0;
 
-            for (
-                let k = 0;
-                k < this.variant[0].hprecords.length;
-                k++
-            ) {
+            for (let k = 0; k < this.variant[0].hprecords.length; k++) {
                 let hp = this.variant[0].hprecords[k];
                 //if (hp.HP != "AllControl") {
-                    hpdisplay[j] = {};
-                    //hpdisplay[j].hpoterms = this.HPOTerms[hp.HP];
-                    hpdisplay[j].hp = hp.HP;
-                    hpdisplay[j].hpoterms =
-                        Formatters.snakeFormatter(
-                            this.HPOTerms[hp.HP]
-                        );
-                    hpdisplay[j].allelecount =2 * hp.TWO_ALT_GENO_CTS +hp.HET_REF_ALT_CTS;
-                    hpdisplay[j].allelnumber =2 *(hp.HOM_REF_CT +hp.HET_REF_ALT_CTS +hp.TWO_ALT_GENO_CTS);
-                    hpdisplay[j].allelefrequency =
-                        this.formatAlleleFrequency(
-                            hpdisplay[j].allelecount,
-                            hpdisplay[j].allelnumber
-                        );
+                hpdisplay[j] = {};
+                //hpdisplay[j].hpoterms = this.HPOTerms[hp.HP];
+                hpdisplay[j].hp = hp.HP;
+                hpdisplay[j].hpoterms = Formatters.snakeFormatter(
+                    this.HPOTerms[hp.HP]
+                );
+                hpdisplay[j].allelecount =
+                    2 * hp.TWO_ALT_GENO_CTS + hp.HET_REF_ALT_CTS;
+                hpdisplay[j].allelnumber =
+                    2 *
+                    (hp.HOM_REF_CT + hp.HET_REF_ALT_CTS + hp.TWO_ALT_GENO_CTS);
+                hpdisplay[j].allelefrequency = this.formatAlleleFrequency(
+                    hpdisplay[j].allelecount,
+                    hpdisplay[j].allelnumber
+                );
 
-                    hpdisplay[j].TWO_ALT_GENO_CTS =
-                        hp.TWO_ALT_GENO_CTS;
-                    j++;
+                hpdisplay[j].TWO_ALT_GENO_CTS = hp.TWO_ALT_GENO_CTS;
+                j++;
                 //}
             }
             hpdisplay = hpdisplay.sort(function (a, b) {
@@ -236,9 +213,9 @@ export default Vue.component("variant-phenotype-table", {
             //console.log("results:"+JSON.stringify(this.variant[0].hprecords));
         },
         formatAlleleFrequency(count, number) {
-			if (count === 0 || number === 0) return 0;
-			else return Number.parseFloat(count / number).toExponential(2);
-		},
+            if (count === 0 || number === 0) return 0;
+            else return Number.parseFloat(count / number).toExponential(2);
+        },
         rowPickClass(item, type) {
             if (!item || type !== "row") return;
             if (item.pick === 1) return "row-pick";
