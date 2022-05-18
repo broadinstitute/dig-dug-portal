4ab4a4ad9a1789ec9225562f12cdb3ac7d2aa62b
hanzhang2021 <zhanghanri2019@gmail.com>
2022-05-18 12:57:45 -0400

    Update GeneAssociationsMasks.vue

--- src/portals/Neph/components/GeneAssociationsMasks.vue
+++ src/portals/Neph/components/GeneAssociationsMasks.vue
@@ -7,90 +7,92 @@
                     filename="rare_variant_gene_associations"
                 ></csv-download>
             </div>
-            <b-row class="top-level-header">
-                <b-col class="top-level-header-item" cols="4">Phenotype</b-col>
-                <b-col class="top-level-header-item" cols="2">pValue</b-col>
-                <b-col class="top-level-header-item" cols="2">Beta</b-col>
-                <b-col class="top-level-header-item" cols="2">Odds Ratio</b-col>
-                <b-col class="top-level-header-item" cols="2">View</b-col>
-            </b-row>
-            <template v-for="(row, i) in associations">
-                <b-row
-                    v-if="phenotypeMap[row.phenotype]"
-                    class="data top-level-value"
-                    :key="row.phenotype + i"
-                >
-                
-                <!-- <b-row
-                    class="data top-level-value"
-                > -->
-                    <b-col class="top-level-value-item" cols="4">
-                        <a
-                            :href="`/phenotype.html?phenotype=${row.phenotype}`"
-                            >{{ phenotypeMap[row.phenotype]}}</a
-                        >
-                    </b-col>
-                    <b-col class="top-level-value-item pValue" cols="2">{{
-                        pValueFormatter(row.pValue)
-                    }}</b-col>
-                    <b-col class="top-level-value-item beta" cols="2">
-                        <template
-                            v-if="!!phenotypeMap[row.phenotype]"
-                        >
-                        
-                            <span
-                                :class="
-                                    row.beta < 0
-                                        ? 'effect negative'
-                                        : 'effect positive'
-                                "
-                                >{{
-                                    row.beta < 0 ? "&#9660;" : "&#9650;"
-                                }}</span
-                            >
-                            <span>{{ effectFormatter(row.beta) }}</span>
-                        </template>
-                    </b-col>
-                    <b-col class="top-level-value-item beta" cols="2">
-                        <template
-                            v-if="!!phenotypeMap[row.phenotype]"
-                        >
-                            <span
-                                :class="
-                                    Math.exp(row.beta) < 1
-                                        ? 'effect negative'
-                                        : 'effect positive'
-                                "
-                                >{{
-                                    Math.exp(row.beta) < 1
-                                        ? "&#9660;"
-                                        : "&#9650;"
-                                }}</span
-                            >
-                            <span>{{
-                                effectFormatter(Math.exp(row.beta))
-                            }}</span>
-                            
-                        </template>
-                    </b-col>
-                    <b-col class="top-level-value-item" cols="2">
-                        <b-button
-                            @click="showFeatures(i)"
-                            class="view-features-btn"
-                            >Masks + Plot</b-button
-                        >
-                        
-                    </b-col>
+            
+                <b-row class="top-level-header">
+                    <b-col class="top-level-header-item" cols="4">Phenotype</b-col>
+                    <b-col class="top-level-header-item" cols="2">pValue</b-col>
+                    <b-col class="top-level-header-item" cols="2">Beta</b-col>
+                    <b-col class="top-level-header-item" cols="2">Odds Ratio</b-col>
+                    <b-col class="top-level-header-item" cols="2">View</b-col>
                 </b-row>
-                <mask-table
-                    v-if="!!phenotypeMap[row.phenotype]"
-                    :maskData="row.masks"
-                    :key="i"
-                    :index="i"
-                    :dichotomous="!!phenotypeMap[row.phenotype]"
-                    :isHidden="true"
-                ></mask-table>
-            </template>
+                <template v-for="(row, i) in tableData">
+                    <b-row
+                        v-if="phenotypeMap[row.phenotype]"
+                        class="data top-level-value"
+                        :key="row.phenotype + i"
+                    >
+                    
+                    <!-- <b-row
+                        class="data top-level-value"
+                    > -->
+                        <b-col class="top-level-value-item" cols="4">
+                            <a
+                                :href="`/phenotype.html?phenotype=${row.phenotype}`"
+                                >{{ phenotypeMap[row.phenotype]}}</a
+                            >
+                        </b-col>
+                        <b-col class="top-level-value-item pValue" cols="2">{{
+                            pValueFormatter(row.pValue)
+                        }}</b-col>
+                        <b-col class="top-level-value-item beta" cols="2">
+                            <template
+                                v-if="!!phenotypeMap[row.phenotype]"
+                            >
+                            
+                                <span
+                                    :class="
+                                        row.beta < 0
+                                            ? 'effect negative'
+                                            : 'effect positive'
+                                    "
+                                    >{{
+                                        row.beta < 0 ? "&#9660;" : "&#9650;"
+                                    }}</span
+                                >
+                                <span>{{ effectFormatter(row.beta) }}</span>
+                            </template>
+                        </b-col>
+                        <b-col class="top-level-value-item beta" cols="2">
+                            <template
+                                v-if="!!phenotypeMap[row.phenotype]"
+                            >
+                                <span
+                                    :class="
+                                        Math.exp(row.beta) < 1
+                                            ? 'effect negative'
+                                            : 'effect positive'
+                                    "
+                                    >{{
+                                        Math.exp(row.beta) < 1
+                                            ? "&#9660;"
+                                            : "&#9650;"
+                                    }}</span
+                                >
+                                <span>{{
+                                    effectFormatter(Math.exp(row.beta))
+                                }}</span>
+                                
+                            </template>
+                        </b-col>
+                        <b-col class="top-level-value-item" cols="2">
+                            <b-button
+                                @click="showFeatures(i)"
+                                class="view-features-btn"
+                                >Masks + Plot</b-button
+                            >
+                            
+                        </b-col>
+                    </b-row>
+                    <mask-table
+                        v-if="!!phenotypeMap[row.phenotype]"
+                        :maskData="row.masks"
+                        :key="i"
+                        :index="i"
+                        :dichotomous="!!phenotypeMap[row.phenotype]"
+                        :isHidden="true"
+                    ></mask-table>
+                </template>
+            
         </b-container>
     </div>
 </template>
@@ -110,9 +112,27 @@ export default Vue.component("gene-associations-masks", {
     component: { MaskTable, CsvDownload },
     data() {
         return {
-            visible: false
+            visible: false,
+            sortBy: 'pValue',
+            sortDesc: false
+
         };
     },
+    
+    /*mounted: function() {
+            if (this.associations) {
+                alert("start sort");
+                this.associations = this.associations.sort(function (a, b) {
+                    if (a.pValue > b.pValue) {
+                        return 1;
+                    } else if (a.pValue < b.pValue) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                //alert("here:"+this.associations.length);
+            }
+        },*/
     methods: {
         pValueFormatter: Formatters.pValueFormatter,
         effectFormatter: Formatters.effectFormatter,
@@ -121,7 +141,25 @@ export default Vue.component("gene-associations-masks", {
             uiUtils.showHideElement("feature-headers-" + index);
             uiUtils.showHideElement("feature-plot-" + index);
         },
+
     },
+    computed: {
+		//This works to display all data fro BI
+		tableData() {
+			if (this.associations && this.associations.length) {
+				return this.associations = this.associations.sort(function (a, b) {
+                    if (a.pValue > b.pValue) {
+                        return 1;
+                    } else if (a.pValue < b.pValue) {
+                        return -1;
+                    }
+                    return 0;
+                });
+			} else {
+				return [];
+			}
+		},
+    }
 });
 </script>
 
