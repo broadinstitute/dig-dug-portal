fba0c8a8d340f6c6b0c4f9d1927d6b127d52145c
Quy <quy@broad.dev>
2022-06-28 17:07:32 -0400

    add default values when store is not ready
    so they won't block page renderings

--- src/views/Gene/main.js
+++ src/views/Gene/main.js
@@ -218,8 +218,9 @@ new Vue({
             let r = this.region;
 
             if (!!r) {
-                window.location.href = `./region.html?chr=${r.chromosome
-                    }&start=${r.start - expanded}&end=${r.end + expanded}`;
+                window.location.href = `./region.html?chr=${
+                    r.chromosome
+                }&start=${r.start - expanded}&end=${r.end + expanded}`;
             }
         },
         isExomeWideSignificant(data, trait) {
@@ -382,7 +383,7 @@ new Vue({
 
         bayesFactorCommonVariation() {
             let commonBF = 1;
-            let data = []
+            let data = [];
             let selectedPhenotype = "";
             let selectGene = this.$store.state.geneName;
             if (this.selectedPhenotypes.length > 0) {
@@ -392,81 +393,95 @@ new Vue({
             if (data.length > 0) {
                 //let data = this.$store.state.associations.data;
                 let coding_variants = {
-                    transcript_ablation: "HIGH", splice_acceptor_variant: "HIGH", splice_donor_variant: "HIGH", stop_gained: "HIGH", frameshift_variant: "HIGH",
-                    stop_lost: "HIGH", start_lost: "HIGH", transcript_amplification: "HIGH", inframe_insertion: "MODERATE", inframe_deletion: "MODERATE", missense_variant: "MODERATE",
+                    transcript_ablation: "HIGH",
+                    splice_acceptor_variant: "HIGH",
+                    splice_donor_variant: "HIGH",
+                    stop_gained: "HIGH",
+                    frameshift_variant: "HIGH",
+                    stop_lost: "HIGH",
+                    start_lost: "HIGH",
+                    transcript_amplification: "HIGH",
+                    inframe_insertion: "MODERATE",
+                    inframe_deletion: "MODERATE",
+                    missense_variant: "MODERATE",
                     protein_altering_variant: "MODERATE"
-                }
-                data.sort(function (a, b) {
+                };
+                data.sort(function(a, b) {
                     return a.pValue - b.pValue;
                 });
                 let topVariant = data[0];
-                let topVariant_consequence = topVariant.consequence
+                let topVariant_consequence = topVariant.consequence;
                 let genesInARegion = this.$store.state.genes.data;
-                var filteredGenesInARegion = genesInARegion.filter(a => a.source == "symbol");
-                let distance = 0
+                var filteredGenesInARegion = genesInARegion.filter(
+                    a => a.source == "symbol"
+                );
+                let distance = 0;
                 //calculate the distance of topVariant to each gene and find the smallest distance
-                filteredGenesInARegion.forEach(function (geneinregion) {
-                    let distanceFromStart = topVariant.position - geneinregion.start
-                    let distanceFromEnd = topVariant.position - geneinregion.end
+                filteredGenesInARegion.forEach(function(geneinregion) {
+                    let distanceFromStart =
+                        topVariant.position - geneinregion.start;
+                    let distanceFromEnd =
+                        topVariant.position - geneinregion.end;
                     if (distanceFromStart * distanceFromEnd > 0) {
-                        distance = Math.min(Math.abs(distanceFromStart), Math.abs(distanceFromEnd))
-                        geneinregion["distance"] = distance
+                        distance = Math.min(
+                            Math.abs(distanceFromStart),
+                            Math.abs(distanceFromEnd)
+                        );
+                        geneinregion["distance"] = distance;
+                    } else {
+                        distance = 0;
+                        geneinregion["distance"] = distance;
                     }
-                    else {
-                        distance = 0
-                        geneinregion["distance"] = distance
-                    }
+                });
 
-                })
-
-                filteredGenesInARegion.sort(function (a, b) {
+                filteredGenesInARegion.sort(function(a, b) {
                     return a.distance - b.distance;
                 });
-                let lowestPvalueClosestGene = filteredGenesInARegion[0]
+                let lowestPvalueClosestGene = filteredGenesInARegion[0];
 
                 //find lowest p - value, is it closest gene - TO DO
 
-
-
-                data.forEach(function (eachSNP) {
+                data.forEach(function(eachSNP) {
                     if (coding_variants.hasOwnProperty(eachSNP.consequence)) {
                         if (eachSNP.pValue < 5e-8) {
-                            commonBF = 20
-                            return commonBF
-
+                            commonBF = 20;
+                            return commonBF;
                         }
                     }
                 });
                 //if NOT GWAS significant
-                if (!this.isGWASSignificantAssociation(data, selectedPhenotype)) {
-                    commonBF = 1
-                    console.log("gene is not GWAS significant")
-
-
+                if (
+                    !this.isGWASSignificantAssociation(data, selectedPhenotype)
+                ) {
+                    commonBF = 1;
+                    console.log("gene is not GWAS significant");
                 }
                 //if  GWAS significant
                 else {
                     //if top variant is coding and the impact of that coding variant is high or moderate
-                    if (coding_variants.hasOwnProperty(topVariant_consequence)) {
-                        if (coding_variants[topVariant_consequence] == "HIGH" || "MODERATE") {
+                    if (
+                        coding_variants.hasOwnProperty(topVariant_consequence)
+                    ) {
+                        if (
+                            coding_variants[topVariant_consequence] == "HIGH" ||
+                            "MODERATE"
+                        ) {
                             //you HAVE TP CHECK IF IT IS SAME GENE - TO DO
-                            commonBF = 360
+                            commonBF = 360;
                         }
+                    } else if (lowestPvalueClosestGene.name == selectGene) {
+                        commonBF = 45;
+                        console.log(
+                            lowestPvalueClosestGene,
+                            "lowestPvalueClosestGene"
+                        );
+                    } else {
+                        commonBF = 3;
                     }
-                    else if (lowestPvalueClosestGene.name == selectGene) {
-                        commonBF = 45
-                        console.log(lowestPvalueClosestGene, "lowestPvalueClosestGene")
-                    }
-                    else {
-                        commonBF = 3
-                    }
-
                 }
             }
 
-
             return commonBF;
-
         },
 
         queries() {
@@ -521,20 +536,19 @@ new Vue({
         },
 
         dbReference() {
-            return this.$store.getters["uniprot/dbReference"];
+            return this.$store.getters["uniprot/dbReference"] || [];
         },
 
         accession() {
-            return this.$store.getters["uniprot/accession"];
+            return this.$store.getters["uniprot/accession"] || [];
         },
 
         geneFunction() {
-            return this.$store.getters["uniprot/geneFunction"];
+            return this.$store.getters["uniprot/geneFunction"] || "";
         },
 
         geneNames() {
-            let x = this.$store.getters["uniprot/geneNames"];
-            return x;
+            return this.$store.getters["uniprot/geneNames"] || [];
         },
 
         gene() {
