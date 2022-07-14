7005099190f7f279f0e198e3a9d1ba271fe18bb5
Annie Moriondo <amoriond@broadinstitute.org>
2022-07-14 14:00:22 -0400

    cleaning up some histogram issues, vertical lines not showing yet

--- src/components/researchPortal/ResearchSummaryPlot.vue
+++ src/components/researchPortal/ResearchSummaryPlot.vue
@@ -1,10 +1,15 @@
 <template>
 <div>
-<p>Hello world</p>
-<p>Phenotype:</p>
-<input v-model="phenotype"/>
-<p>Gene or region:</p>
-<input v-model="region"/>
+<form>
+    <label>Phenotype<input v-model="phenotype"/></label>
+    <label>Gene or region<input v-model="region"/></label>
+    <p>Attributes to include:</p>
+    <label>Beta<input type="checkbox" v-model="includeBeta"/></label>
+    <label>P-value<input type="checkbox" v-model="includePvalue"/></label>
+    <label>Standard error<input type="checkbox" v-model="includeStdErr"/></label>
+    <label>Z-score<input type="checkbox" v-model="includeZscore"/></label>
+</form>
+
 <button @click="getAssociations(phenotype, region)">Get associations</button>
 <div class="all-charts">
     <div class="chart beta-chart"></div>
@@ -26,6 +31,11 @@ export default Vue.component("research-summary-plot", {
         return {
             phenotype: "t2d",
             region: "slc30a8",
+            includeBeta: false,
+            includePvalue: false,
+            includeStdErr: false,
+            includeZscore: false,
+            jsonData: null
         };
     },
     mounted: function () {},
@@ -37,23 +47,32 @@ export default Vue.component("research-summary-plot", {
             let assocServer = "https://bioindex.hugeamp.org/api/bio"; // will this ever change?
             let queryURL = `${assocServer}/query/associations?q=${phenotype},${region}`
             let assocJSON = await fetch(queryURL).then((response) => response.json());
+            this.jsonData = assocJSON.data;
             
             // Beta
-            let betaVals = assocJSON.data.map(item => item.beta);       
-            this.renderCharts(betaVals, 100, ".beta-chart");
+            if (this.includeBeta){
+                let betaVals = assocJSON.data.map(item => item.beta);
+                this.renderCharts(betaVals, 100, ".beta-chart");
+            }
 
             // P-value
-            let pVals = assocJSON.data.map(item => (-1 * Math.log10(item.pValue)));
-            this.renderCharts(pVals, 100, ".p-val-chart");
+            if (this.includePvalue){
+                let pVals = assocJSON.data.map(item => (-1 * Math.log10(item.pValue)));
+                this.renderCharts(pVals, 100, ".p-val-chart");
+            }
+            
 
             // Std error
-            let stdErrVals = assocJSON.data.map(item => Number(item.stdErr));
-            this.renderCharts(stdErrVals, 100, ".std-err-chart");
+            if (this.includeStdErr){
+                let stdErrVals = assocJSON.data.map(item => Number(item.stdErr));
+                this.renderCharts(stdErrVals, 100, ".std-err-chart");
+            }
 
             // Z-score
-            let zScoreVals = assocJSON.data.map(item => Number(item.zScore));
-            this.renderCharts(zScoreVals, 100, ".z-score-chart");
-
+            if (this.includeZscore){
+                let zScoreVals = assocJSON.data.map(item => Number(item.zScore));
+                this.renderCharts(zScoreVals, 100, ".z-score-chart");
+            }
         },
         
         renderCharts(dataset, nBuckets, chartWrapper){
@@ -81,6 +100,9 @@ export default Vue.component("research-summary-plot", {
             var yScale = d3.scaleLinear()
                     .domain([0, d3.max(bins, function(d){return d.length;})]) // input
                     .range([height, 0]); // output
+            
+            // Clear the div before drawing the graph.
+            document.getElementsByClassName(chartWrapper.slice(1))[0].innerHTML = "";
 
             let svg = d3.select(chartWrapper)
                     .append("svg")
@@ -92,7 +114,9 @@ export default Vue.component("research-summary-plot", {
             svg.append("g")
                     .attr("class", "x axis")
                     .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(xScale).ticks(3));
+                    .call(d3.axisBottom(xScale).ticks(3))
+                    .selectAll("text")
+                    .style("text-anchor", "end")
 
             svg.append("g")
                     .attr("class", "y axis")
@@ -106,15 +130,28 @@ export default Vue.component("research-summary-plot", {
                 .append("rect")
                 .attr("x", 1)
                 .attr("transform", function(d){return `translate(${xScale(d.x0)},${yScale(d.length)})`;})
-                .attr("width", function(d){return xScale(d.x1) - xScale(d.x0) - 1;})
+                .attr("width", function(d){return xScale(d.x1) - xScale(d.x0);})
                 .attr("height", function(d){return height - yScale(d.length);})
                 .style("fill", "orange");
             
             svg.append("text")
                 .attr("text-anchor", "start")
-                .attr("y", 1)
-                .attr("x", 0.5)
+                .attr("y", 1).attr("x", 0.5)
                 .text(chartWrapper.slice(1, -6).replace("-", " "));
+
+            // Select a random item
+            let randIndex = Math.floor(Math.random()* dataset.length);
+            let randItem = dataset[randIndex];
+            console.log(randItem);
+
+            // Draw a line for this item
+            svg.append("line")
+                .attr("x1", xScale(randIndex))
+                .attr("x2", xScale(randIndex))
+                .attr("y1", 0)
+                .attr("y2", height)
+                .attr("stroke", "#grey")
+                .attr("stroke-dasharray", "4");
         }
 
     },
@@ -128,5 +165,9 @@ export default Vue.component("research-summary-plot", {
 
 .all-charts{
     display: flex;
+}
+
+form label{
+    display: block;
 }
 </style>
