2e9e5d067d9fe4b03f8ea75549fc90604ca637b9
Annie Moriondo <amoriond@broadinstitute.org>
2022-07-28 17:09:18 -0400

    cleaned up a little, not really refactored

--- src/components/researchPortal/ResearchSummaryPlot.vue
+++ src/components/researchPortal/ResearchSummaryPlot.vue
@@ -105,97 +105,91 @@ export default Vue.component("research-summary-plot", {
                         .x1(d => xNum(d.length))
                         .y(d => yViolin(d.x0))
                     .curve(d3.curveCatmullRom));
-            }
-            var xScale = d3.scaleLinear()
+            } else {
+                var xScale = d3.scaleLinear()
                     .domain([minVal, maxVal]) // input
                     .range([0, width]);
+                // Data prep for histogram
+                var histogram = d3.histogram()
+                    .value(function (d) {return d })
+                    .domain(xScale.domain())
+                    .thresholds(xScale.ticks(nBuckets));          
+                var bins = histogram(dataset);
+                let histogramTopEnd = d3.max(bins, function(d){return d.length;});
 
-            // Data prep for histogram
-            var histogram = d3.histogram()
-                            .value(function (d) {return d })
-                            .domain(xScale.domain())
-                            .thresholds(xScale.ticks(nBuckets));          
-            var bins = histogram(dataset);
-            let histogramTopEnd = d3.max(bins, function(d){return d.length;});
+                // Data prep for line/scatterplot (yes, it does need to be done here)
+                let interval = (maxVal - minVal) / nBuckets;
 
-            // Data prep for line/scatterplot (yes, it does need to be done here)
-            let interval = (maxVal - minVal) / nBuckets;
+                let bucketsData = [];
+                for (let i = minVal; i < maxVal; i = i + interval){
+                    let bucketCount = dataset.filter(entry => entry >= i && entry < i + interval).length;
+                    bucketsData.push({"bucketStart": i, "bucketCount": bucketCount});
+                }
+                // Max vals get added to the top bucket
+                let maxCount = bucketsData.filter(entry => entry == maxVal).length;
+                bucketsData[bucketsData.length - 1].bucketCount += maxCount;
+                let linePlotTopEnd = bucketsData.map(item => item.bucketCount)
+                    .reduce((prev, next) => prev > next ? prev : next);
 
-            let bucketsData = [];
-            for (let i = minVal; i < maxVal; i = i + interval){
-                let bucketCount = dataset.filter(entry => entry >= i && entry < i + interval).length;
-                bucketsData.push({"bucketStart": i, "bucketCount": bucketCount});
-            }
-            // Max vals get added to the top bucket
-            let maxCount = bucketsData.filter(entry => entry == maxVal).length;
-            bucketsData[bucketsData.length - 1].bucketCount += maxCount;
-            let linePlotTopEnd = bucketsData.map(item => item.bucketCount)
-                .reduce((prev, next) => prev > next ? prev : next);
-
-            let yScaleTopEnd = configObject.type == "histogram" ? histogramTopEnd : linePlotTopEnd;
-            var yScale = d3.scaleLinear()
+                let yScaleTopEnd = configObject.type == "histogram" ? histogramTopEnd : linePlotTopEnd;
+                var yScale = d3.scaleLinear()
                     .domain([0, yScaleTopEnd]) // input
                     .range([height, 0]); // output
             
-            svg.append("g")
+                svg.append("g")
                     .attr("class", "x axis")
                     .attr("transform", "translate(0," + height + ")")
                     .call(d3.axisBottom(xScale).ticks(3))
                     .selectAll("text")
                     .style("text-anchor", "end")
 
-            svg.append("g")
+                svg.append("g")
                     .attr("class", "y axis")
                     .call(d3.axisLeft(yScale).ticks(3))
                     .selectAll("text")
                     .style("text-anchor", "end");
 
-            svg.append("text")
-                .attr("text-anchor", "start")
-                .attr("y", yScale(yScaleTopEnd * -0.2)).attr("x", 0.5)
-                .text(attributeLabel)
-                .style("font-weight", "bold");
+                svg.append("text")
+                    .attr("text-anchor", "start")
+                    .attr("y", yScale(yScaleTopEnd * -0.2)).attr("x", 0.5)
+                    .text(attributeLabel)
+                    .style("font-weight", "bold");
             
-            if (configObject.type == "histogram"){
-                svg.selectAll("rect")
-                .data(bins)
-                .enter()
-                .append("rect")
-                .attr("x", 1)
-                .attr("transform", function(d){return `translate(${xScale(d.x0)},${yScale(d.length)})`;})
-                .attr("width", function(d){return xScale(d.x1) - xScale(d.x0);})
-                .attr("height", function(d){return height - yScale(d.length);})
-                .style("fill", "orange");
-            } else if (configObject.type == "line"){
-                    var line = d3
-                    .line()
-                    .x(function (d) {
-                        return xScale(d.bucketStart);
-                    }) // set the x values for the line generator
-                    .y(function (d) {
-                        return yScale(d.bucketCount);
-                    }) // set the y values for the line generator
-                    .curve(d3.curveMonotoneX); // apply smoothing to the line
-              
-                    svg.append("path")
-                        .datum(bucketsData)
-                        .attr("class", "chart-line")
-                        .attr("d", line)
-                        .attr("fill", "none")
-                        .attr("stroke", "orange");
-
-            } else if (configObject.type == "scatterplot"){
-                svg.append("g")
-                    .selectAll("dot")
-                    .data(bucketsData.filter(i => i.bucketCount > 0))
+                if (configObject.type == "histogram"){
+                    svg.selectAll("rect")
+                    .data(bins)
                     .enter()
-                    .append("circle")
-                    .attr("cx", function(d){return xScale(d.bucketStart)})
-                    .attr("cy", function(d){return yScale(d.bucketCount)})
-                    .attr("r", 1.5)
+                    .append("rect")
+                    .attr("x", 1)
+                    .attr("transform", function(d){return `translate(${xScale(d.x0)},${yScale(d.length)})`;})
+                    .attr("width", function(d){return xScale(d.x1) - xScale(d.x0);})
+                    .attr("height", function(d){return height - yScale(d.length);})
                     .style("fill", "orange");
+                } else if (configObject.type == "line"){
+                        var line = d3
+                        .line()
+                        .x(d => xScale(d.bucketStart)) // set the x values for the line generator
+                        .y(d => yScale(d.bucketCount)) // set the y values for the line generator
+                        .curve(d3.curveMonotoneX); // apply smoothing to the line
+              
+                        svg.append("path")
+                            .datum(bucketsData)
+                            .attr("class", "chart-line")
+                            .attr("d", line)
+                            .attr("fill", "none")
+                            .attr("stroke", "orange");
+                } else if (configObject.type == "scatterplot"){
+                    svg.append("g")
+                        .selectAll("dot")
+                        .data(bucketsData.filter(i => i.bucketCount > 0))
+                        .enter()
+                        .append("circle")
+                        .attr("cx", function(d){return xScale(d.bucketStart)})
+                        .attr("cy", function(d){return yScale(d.bucketCount)})
+                        .attr("r", 1.5)
+                        .style("fill", "orange");
+                }
             }
-                
             if (this.$props.isPlotByRow){
                 // Select a random item
                 let randIndex = Math.floor(Math.random()* dataset.length);
