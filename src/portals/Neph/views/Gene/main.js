956d6eea1c5ced1a085b8ee0666eb4d38441cb6f
Quy <quy@broad.dev>
2022-08-21 16:37:24 -0400

    reorder

--- src/portals/Neph/views/Gene/main.js
+++ src/portals/Neph/views/Gene/main.js
@@ -38,7 +38,7 @@ import Alert, {
     postAlert,
     postAlertNotice,
     postAlertError,
-    closeAlert
+    closeAlert,
 } from "@/components/Alert";
 
 Vue.config.productionTip = false;
@@ -69,7 +69,7 @@ new Vue({
         ResultsDashboard,
         NCATSPredicateTable,
         VariantSearch,
-        ColorBarPlot
+        ColorBarPlot,
     },
 
     data() {
@@ -79,114 +79,30 @@ new Vue({
             externalResources: {
                 ensembl: {
                     title: "Ensembl",
-                    link:
-                        "https://useast.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g="
+                    link: "https://useast.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=",
                 },
                 hgnc: {
                     title: "HUGO Gene Nomenclature Committee",
-                    link:
-                        "https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/"
+                    link: "https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/",
                 },
                 mgd: {
                     title: "Mouse Genome Database",
-                    link: "http://www.informatics.jax.org/marker/"
+                    link: "http://www.informatics.jax.org/marker/",
                 },
                 rgd: {
                     title: "Rat Genome Database",
-                    link: "https://rgd.mcw.edu/rgdweb/report/gene/main.html?id="
+                    link: "https://rgd.mcw.edu/rgdweb/report/gene/main.html?id=",
                 },
                 ucsc: {
                     title: "USSC Genome Browser",
-                    link:
-                        "http://genome.ucsc.edu/cgi-bin/hgGene?db=hg19&hgg_gene="
+                    link: "http://genome.ucsc.edu/cgi-bin/hgGene?db=hg19&hgg_gene=",
                 },
                 uniprot: {
                     title: "Universal Protein Resource",
-                    link: "https://www.uniprot.org/uniprot/"
-                }
-            }
+                    link: "https://www.uniprot.org/uniprot/",
+                },
+            },
         };
-    },
-
-    created() {
-        this.$store.dispatch("queryGeneName", this.$store.state.geneName);
-        // this.$store.dispatch("queryAliasName", this.$store.state.aliasName)
-        //this.$store.dispatch("queryAssociations");
-        // get the disease group and set of phenotypes available
-        this.$store.dispatch("bioPortal/getDiseaseGroups");
-        this.$store.dispatch("bioPortal/getPhenotypes");
-
-        this.pushCriterionPhenotype("T2D");
-    },
-
-    render(createElement, context) {
-        return createElement(Template);
-    },
-
-    methods: {
-        ...uiUtils,
-        postAlert,
-        postAlertNotice,
-        postAlertError,
-        closeAlert,
-        pushCriterionPhenotype(phenotypeName) {
-            this.genePageSearchCriterion.push({
-                field: "phenotype",
-                threshold: phenotypeName
-            });
-        },
-        biolinkQueryGraph(subjectCurie, { subject, predicate, object }) {
-            const uuid = Counter.getUniqueId;
-            const sid = uuid("s");
-            const oid = uuid("o");
-            const eid = uuid("e");
-            return {
-                query_graph: {
-                    nodes: {
-                        [sid]: {
-                            id: subjectCurie,
-                            category: subject
-                        },
-                        [oid]: {
-                            category: object
-                        }
-                    },
-                    edges: {
-                        [eid]: {
-                            subject: sid,
-                            object: oid,
-                            predicate: predicate
-                        }
-                    }
-                }
-            };
-        },
-
-        // go to region page
-        exploreRegion(expanded = 0) {
-            let r = this.region;
-
-            if (!!r) {
-                window.location.href = `./region.html?chr=${
-                    r.chromosome
-                }&start=${r.start - expanded}&end=${r.end + expanded}`;
-            }
-        },
-        isExomeWideSignificant(data, trait) {
-            if (!!data.length) {
-                for (let i = 0; i < data.length; i++) {
-                    if (data[i].phenotype == trait) {
-                        if (data[i].pValue <= 2.5e-6) {
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            }
-        },
-        topPhenotype(topAssocData) {
-            return topAssocData[0];
-        }
     },
 
     computed: {
@@ -228,8 +144,8 @@ new Vue({
             }
 
             return this.genePageSearchCriterion
-                .filter(criterion => criterion.field === "phenotype")
-                .map(criterion => phenotypeMap[criterion.threshold]);
+                .filter((criterion) => criterion.field === "phenotype")
+                .map((criterion) => phenotypeMap[criterion.threshold]);
         },
 
         selectedPhenotype() {
@@ -240,7 +156,7 @@ new Vue({
         eglData() {
             let geneSymbol = this.$store.state.geneName;
             if (this.selectedPhenotype == "T2D") {
-                if (!!this.$store.state.kp4cd.eglData.data) {
+                if (this.$store.state.kp4cd.eglData.data) {
                     let effectordata = this.$store.state.kp4cd.eglData.data;
                     let effectorGeneData = {};
 
@@ -269,8 +185,8 @@ new Vue({
                 this.biolinkQueryGraph("NCBIGENE:1017", {
                     subject: "biolink:Gene",
                     predicate: "biolink:enables",
-                    object: "biolink:MolecularActivity"
-                })
+                    object: "biolink:MolecularActivity",
+                }),
             ];
         },
         frontContents() {
@@ -298,16 +214,16 @@ new Vue({
 
         aliasNames() {
             return this.$store.state.genes.data.filter(
-                g => g.source === "alias"
+                (g) => g.source === "alias"
             );
         },
 
         alternateNames() {
             let geneData = this.$store.state.gene.data;
             return this.$store.state.genes.data
-                .filter(g => g.start == geneData[0].start)
-                .filter(g => g.end == geneData[0].end)
-                .filter(g => g.source !== "symbol")
+                .filter((g) => g.start == geneData[0].start)
+                .filter((g) => g.end == geneData[0].end)
+                .filter((g) => g.source !== "symbol")
                 .sort((a, b) => {
                     if (a.source < b.source) return -1;
                     if (a.source > b.source) return 1;
@@ -342,7 +258,7 @@ new Vue({
         regionText() {
             let r = this.region;
 
-            if (!!r) {
+            if (r) {
                 return `${r.chromosome}:${Formatters.intFormatter(
                     r.start
                 )}-${Formatters.intFormatter(r.end)}`;
@@ -354,7 +270,7 @@ new Vue({
         regionTextExpanded() {
             let r = this.region;
 
-            if (!!r) {
+            if (r) {
                 return `${r.chromosome}:${Formatters.intFormatter(
                     r.start - 50000
                 )}-${Formatters.intFormatter(r.end + 50000)}`;
@@ -365,7 +281,7 @@ new Vue({
 
         associationPhenotypes() {
             return this.$store.state.geneassociations.data.map(
-                a => a.phenotype
+                (a) => a.phenotype
             );
         },
 
@@ -378,14 +294,14 @@ new Vue({
                     gene: symbol,
                     region: `${r.chromosome}:${Formatters.intFormatter(
                         r.start
-                    )}-${Formatters.intFormatter(r.end)}`
+                    )}-${Formatters.intFormatter(r.end)}`,
                 };
             }
         },
 
         phenotypeMap() {
             return this.$store.state.bioPortal.phenotypeMap;
-        }
+        },
     },
 
     watch: {
@@ -440,6 +356,87 @@ new Vue({
         symbolName(symbol) {
             this.$store.dispatch("queryUniprot", symbol);
             this.$store.dispatch("queryAssociations");
-        }
-    }
+        },
+    },
+
+    created() {
+        this.$store.dispatch("queryGeneName", this.$store.state.geneName);
+        // this.$store.dispatch("queryAliasName", this.$store.state.aliasName)
+        //this.$store.dispatch("queryAssociations");
+        // get the disease group and set of phenotypes available
+        this.$store.dispatch("bioPortal/getDiseaseGroups");
+        this.$store.dispatch("bioPortal/getPhenotypes");
+
+        this.pushCriterionPhenotype("T2D");
+    },
+
+    methods: {
+        ...uiUtils,
+        postAlert,
+        postAlertNotice,
+        postAlertError,
+        closeAlert,
+        pushCriterionPhenotype(phenotypeName) {
+            this.genePageSearchCriterion.push({
+                field: "phenotype",
+                threshold: phenotypeName,
+            });
+        },
+        biolinkQueryGraph(subjectCurie, { subject, predicate, object }) {
+            const uuid = Counter.getUniqueId;
+            const sid = uuid("s");
+            const oid = uuid("o");
+            const eid = uuid("e");
+            return {
+                query_graph: {
+                    nodes: {
+                        [sid]: {
+                            id: subjectCurie,
+                            category: subject,
+                        },
+                        [oid]: {
+                            category: object,
+                        },
+                    },
+                    edges: {
+                        [eid]: {
+                            subject: sid,
+                            object: oid,
+                            predicate: predicate,
+                        },
+                    },
+                },
+            };
+        },
+
+        // go to region page
+        exploreRegion(expanded = 0) {
+            let r = this.region;
+
+            if (r) {
+                window.location.href = `./region.html?chr=${
+                    r.chromosome
+                }&start=${r.start - expanded}&end=${r.end + expanded}`;
+            }
+        },
+        isExomeWideSignificant(data, trait) {
+            if (data.length) {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].phenotype == trait) {
+                        if (data[i].pValue <= 2.5e-6) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        },
+        topPhenotype(topAssocData) {
+            return topAssocData[0];
+        },
+    },
+
+    render(createElement, context) {
+        return createElement(Template);
+    },
 }).$mount("#app");
