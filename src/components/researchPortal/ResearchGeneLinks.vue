02d589b75b4ce22f8a07893f21961e11fb33b614
dkjang <dkjang@broadinstitute.org>
2022-11-01 11:22:36 -0400

    S2G activated

--- src/components/researchPortal/ResearchGeneLinks.vue
+++ src/components/researchPortal/ResearchGeneLinks.vue
@@ -41,11 +41,14 @@
 									</template>
 								</select>
 							</div>
-							<!-- this part is for SNP to Gene options
-							<div class="col divider" style="background: none">
+							<!-- this part is for SNP to Gene options -->
+							<!--<div class="col divider" style="background: none">
 								<span class="or-text">or</span>
-							</div>
-							<div class="col" style="padding: 2px">
+							</div>-->
+							<div
+								class="col"
+								style="padding: 2px; margin-left: 15px"
+							>
 								<div
 									class="label"
 									style="
@@ -69,7 +72,7 @@
 									></option>
 								</select>
 							</div>
-							-->
+							<!-- -->
 						</div>
 						<div
 							class=""
@@ -281,9 +284,6 @@ export default Vue.component("research-gene-links-plot", {
 				}
 
 				tempArray.sort((a, b) => (a.tissue > b.tissue ? 1 : -1));
-
-				/// to add S2G option.
-				tempArray.push({ tissue: "SNP to Gene", phenotypes: [] });
 
 				return tempArray;
 			}
@@ -1157,52 +1157,54 @@ export default Vue.component("research-gene-links-plot", {
 		},
 
 		async getS2GeneLinks(event) {
-			let geneLinksServer =
-				this.renderConfig["gene links server"] == "KP BioIndex"
-					? uiUtils.biDomain() + "/api/bio"
-					: this.renderConfig["gene links server"];
+			if (!!event.target.value && event.target.value != "") {
+				let geneLinksServer =
+					this.renderConfig["gene links server"] == "KP BioIndex"
+						? uiUtils.biDomain() + "/api/bio"
+						: this.renderConfig["gene links server"];
 
-			let region = this.searchingRegion;
+				let region = this.searchingRegion;
 
-			let GLURL =
-				geneLinksServer +
-				"/query/variant-links?q=" +
-				region.chr +
-				":" +
-				region.start +
-				"-" +
-				region.end;
+				let GLURL =
+					geneLinksServer +
+					"/query/variant-links?q=" +
+					region.chr +
+					":" +
+					region.start +
+					"-" +
+					region.end;
 
-			let S2GLJson = await fetch(GLURL).then((resp) => resp.json());
+				let S2GLJson = await fetch(GLURL).then((resp) => resp.json());
 
-			let S2GTissue = "SNP to gene";
+				let S2GTissue = "SNP to gene";
 
-			if (S2GLJson.error == null) {
-				if (S2GLJson.data.length == 0) {
-					alertUtils.popAlert(
-						"No SNP to gene data found in the region."
-					);
-				} else {
-					///adding this part since data model for variant-links is different from gene-links
-					S2GLJson.data.map((g) => {
-						g["targetGene"] = g.gene;
-					});
+				if (S2GLJson.error == null) {
+					if (S2GLJson.data.length == 0) {
+						alertUtils.popAlert(
+							"No SNP to gene data found in the region."
+						);
+					} else {
+						///adding this part since data model for variant-links is different from gene-links
+						S2GLJson.data.map((g) => {
+							g["targetGene"] = g.gene;
+						});
 
-					this.$store.dispatch("pkgDataSelected", {
-						type: "GLTissue",
-						id: S2GTissue,
-						action: "add",
-					});
-					if (!this.pkgData["GLData"]) {
-						this.pkgData["GLData"] = {};
-						this.GLData = {};
+						this.$store.dispatch("pkgDataSelected", {
+							type: "GLTissue",
+							id: S2GTissue,
+							action: "add",
+						});
+						if (!this.pkgData["GLData"]) {
+							this.pkgData["GLData"] = {};
+							this.GLData = {};
+						}
+						this.pkgData["GLData"][S2GTissue] = S2GLJson.data;
+						this.GLData[S2GTissue] = S2GLJson.data;
 					}
-					this.pkgData["GLData"][S2GTissue] = S2GLJson.data;
-					this.GLData[S2GTissue] = S2GLJson.data;
-				}
 
-				this.trigger++;
-				this.renderGLPlot();
+					this.trigger++;
+					this.renderGLPlot();
+				}
 			}
 		},
 		async getGlobalEnrichment(REGION_OBJ) {
