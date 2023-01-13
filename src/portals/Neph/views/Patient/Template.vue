7f313fa18c4be7f64cd505124612a32c91679742
hanzhang2021 <zhanghanri2019@gmail.com>
2023-01-13 11:07:05 -0500

    Update Template.vue

--- src/portals/Neph/views/Patient/Template.vue
+++ src/portals/Neph/views/Patient/Template.vue
@@ -14,7 +14,7 @@
                 <div class="col filter-col-md">
                     <div class="label">Patient</div>
                     <input
-                        v-model="$store.state.newPatientId"
+                        v-model="$store.state.patient"
                         type="text"
                         class="form-control"
                         placeholder="Search Patient"
@@ -29,7 +29,7 @@
                         @click="
                             $store.dispatch(
                                 'queryPatient',
-                                $store.state.newPatientId
+                                $store.state.patient
                             )
                         "
                     >
@@ -44,11 +44,117 @@
                     </div>
                 </div>
             </search-header-wrapper>
-
-            
-            
+            <div class="gene-page-header card mdkp-card">
+                <div class="row card-body">
+                    <div class="col-md-9 gene-page-header-title">
+                        Patient
+                        <tooltip-documentation
+                            name="variant.alleles.tooltip.hover"
+                            :isHover="true"
+                        ></tooltip-documentation>
+                    </div>
+                   
+                    <div class="col-md-9 gene-page-header-body">
+                        <span>
+                            {{ $store.state.patient }}
+                        </span>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <div
+                        v-if="$store.state.patient"
+                    >
+                        <h4 class="card-title">
+                            Patient Diagnosis
+                        </h4>
+                        <patient-diagnosis-card
+                            v-bind:patientId="
+                                $store.state.patient
+                            ">
+                        </patient-diagnosis-card>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <div
+                        v-if="$store.state.patient"
+                    >
+                        <h4 class="card-title">
+                            Patient Admin Medicine
+                        </h4>
+                        <patient-admin_medicine-card
+                            v-bind:patientId="
+                                $store.state.patient
+                            ">
+                        </patient-admin_medicine-card>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <div
+                        v-if="$store.state.patient"
+                    >
+                        <h4 class="card-title">
+                            Patient Home Medicine
+                        </h4>
+                        <patient-home_medicine-card
+                            v-bind:patientId="
+                                $store.state.patient
+                            ">
+                        </patient-home_medicine-card>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <div
+                        v-if="$store.state.patient"
+                    >
+                        <h4 class="card-title">
+                            Patient Laboratory
+                        </h4>
+                        <patient-laboratory-card
+                            v-bind:patientId="
+                                $store.state.patient
+                            ">
+                        </patient-laboratory-card>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <div
+                        v-if="$store.state.patient"
+                    >
+                        <h4 class="card-title">
+                            Patient Vital
+                        </h4>
+                        <patient-vital-card
+                            v-bind:patientId="
+                                $store.state.patient
+                            ">
+                        </patient-vital-card>
+                        
+                    </div>
+                    
+                </div>
+            </div>
         </div> 
-
+        
+        
         <!-- Footer-->
         <page-footer :disease-group="$parent.diseaseGroup"></page-footer>
     </div>
