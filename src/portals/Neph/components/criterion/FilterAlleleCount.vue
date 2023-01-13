25720bb31ac353b201e379406dc45b4f5f5b6e5b
hanzhang2021 <zhanghanri2019@gmail.com>
2023-01-13 11:05:13 -0500

    Create FilterAlleleCount.vue

---
+++ src/portals/Neph/components/criterion/FilterAlleleCount.vue
@@ -0,0 +1,48 @@
+<template>
+    <filter-control-template
+        class="filter-col-sm"
+        :field="field"
+        :type="'number'"
+        :predicate="predicate"
+        :pillFormatter="pillFormatter"
+        :computedField="computedField"
+        :multiple="false"
+        :color="color"
+        :placeholder="placeholder"
+    >
+        <slot></slot>
+    </filter-control-template>
+</template>
+<script>
+import Vue from "vue";
+import FilterControlTemplate from "@/components/criterion/template/FilterControlTemplate";
+import Formatter from "@/utils/formatters";
+export default Vue.component("filter-allelecount-control", {
+    props: {
+        field: String,
+        placeholder: String,
+        computedField: Function,
+        color: {
+            type: String,
+            default: "#dc3545",
+        },
+        pillFormatter: {
+            type: Function,
+            /*default: (filterDefinition) =>
+                `${filterDefinition.field} <= ${
+                    !!filterDefinition.labelFormatter
+                        ? Formatter.pValueFormatter(filterDefinition.threshold)
+                        : filterDefinition.threshold
+                }`,*/
+        },
+
+        predicate: {
+            type: Function,
+            default: (allelecount, aBound) => allelecount >= aBound,
+        },
+    },
+    components: {
+        FilterControlTemplate,
+    },
+});
+</script>
