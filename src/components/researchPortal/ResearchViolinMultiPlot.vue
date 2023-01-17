98c2a8bb7fe942562e93d6e4727d1e3a117fd334
Annie Moriondo <amoriond@broadinstitute.org>
2023-01-17 14:54:30 -0500

    messed up looking boxplots, but this is proof of concept

--- src/components/researchPortal/ResearchViolinMultiPlot.vue
+++ src/components/researchPortal/ResearchViolinMultiPlot.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="chart-wrapper">
-    <div class="v-multi-chart">
+    <div id="v-multi-chart">
         <h4>Pending!</h4>
     </div>
 </div>
@@ -32,82 +32,88 @@ export default Vue.component("research-violin-multi-plot", {
     methods: {
         ...uiUtils,
         displayResults(configObject){
-            let chart = document.getElementsByClassName("v-multi-chart")[0];
+            let rawData = this.$props.rawData;
+            let chart = document.getElementById("v-multi-chart");
             chart.innerHTML = "";
 
-            // We will start at 0 for now
-            let maxVal = this.$props.rawData.reduce((prev, next) => 
-                prev.max_TPM > next.max_TPM ? prev.max_TPM : next.max_TPM);
-            let minVal = 0;
-            let numEntries = this.$props.rawData.length;
-
-            var margin = { top: 25, right: 10, bottom: 35, left: 29 },
+            var margin = { top: 10, right: 30, bottom: 30, left: 40 },
                         width = configObject.width - margin.left - margin.right,
                         height = configObject.height - margin.top - margin.bottom;
             
-            let svg = d3.select("v-multi-chart")
+            let svg = d3.select("#v-multi-chart")
                     .append("svg")
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", height + margin.top + margin.bottom)
                     .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var sumstat = d3.nest()
+                .key(function(d){return d.dataset;})
+                .rollup(function(d){
+                    let q1 = d.map(g => g.FirstQu_TPM);
+                    let median = d.map(g => g.median_TPM);
+                    let q3 = d.map(g => g.ThirdQu_TPM);
+                    let interQuantileRange = q3-q1;
+                    let min = d.map(g => g.min_TPM);
+                    let max = d.map(g => g.max_TPM);
+                    return({
+                        q1: q1,
+                        median: median,
+                        q3: q3,
+                        interQuantileRange: interQuantileRange,
+                        min: min,
+                        max: max});
+                }).entries(rawData);
+
+            var x = d3.scaleBand()
+                .range([0, width])
+                .domain(rawData.map(entry => entry.dataset))
+                .paddingInner(1)
+                .paddingOuter(.5);
+            svg.append("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(d3.axisBottom(x));
             
-            var yViolin = d3.scaleLinear().domain([minVal, maxVal]).range([height, 0]);
-            svg.append("g").call(d3.axisLeft(yViolin));
-            var xViolin = d3.scaleBand().range([0, width]).domain(["dataset"]).padding(0.05);
-            svg.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(xViolin));
+            let maxVal = rawData.reduce((prev, next) => 
+                prev.max_TPM > next.max_TPM ? prev.max_TPM : next.max_TPM);
+            var y = d3.scaleLinear()
+                .domain([0,maxVal])
+                .range([height,0]);
+            svg.append("g").call(d3.axisLeft(y));
+
+            svg.selectAll("vertLines")
+                .data(sumstat)
+                .enter()
+                .append("line")
+                    .attr("x1", d => x(d.key))
+                    .attr("x2", d => x(d.key))
+                    .attr("y1", d => y(d.value.min))
+                    .attr("y2", d => y(d.value.max))
+                    .attr("stroke", "black")
+                    .style("width", 40);
             
-            let widthPer = width / numEntries;
-
-            for (let i = 0; i < numEntries; i++){
-                //let dataset = JSON.stringify(this.$props.rawData[index]);
-                let entry = this.$props.rawData[i];
-                let dataset = [
-                    entry["min_TPM"],
-                    entry["FirstQu_TPM"],
-                    entry["median_TPM"],
-                    entry["ThirdQu_TPM"],
-                    entry["max_TPM"]
-                ];
-
-                //let nBuckets = configObject.buckets < dataset.length ? configObject.buckets : dataset.length;
-                let nBuckets = dataset.length;
-
-                var histViolin = d3.histogram()
-                    .domain(yViolin.domain())
-                    .thresholds(yViolin.ticks(nBuckets))
-                    .value(d => d);
-                
-                var sumstat = d3.nest()
-                    .key(d => "dataset")
-                    .rollup(d => histViolin(d))
-                    .entries(dataset);
-                
-                var maxNum = 0;
-                for (let i in sumstat){
-                    let allBins = sumstat[i].value;
-                    let lengths = allBins.map(a => a.length);
-                    let longest = d3.max(lengths);
-                    if (longest > maxNum){ maxNum = longest; }
-                }
-
-                var xNum = d3.scaleLinear().range([0, xViolin.bandwidth()]).domain([-maxNum, maxNum]);
-
-                svg.selectAll("myViolin")
-                    .data(sumstat)
-                    .enter()
-                    .append("g")
-                    .attr("transform", d => `translate (${xViolin(d.key)}, 0)`)
-                    .append("path")
-                    .datum(d => d.value)
-                    .style("stroke", "none")
-                    .style("fill", "orange")
-                    .attr("d", d3.area()
-                        .x0(d => xNum(-d.length))
-                        .x1(d => xNum(d.length))
-                        .y(d => yViolin(d.x0))
-                    .curve(d3.curveCatmullRom));
-            }
+            var boxWidth = (width / rawData.length) * 0.75;
+            svg.selectAll("boxes")
+                .data(sumstat)
+                .enter()
+                .append("rect")
+                    .attr("x", d => x(d.key) - boxWidth/2)
+                    .attr("y", d => y(d.value.q3))
+                    .attr("height", d => y(d.value.q1) - y(d.value.q3))
+                    .attr("width", boxWidth)
+                    .attr("stroke", "black")
+                    .style("fill", "$69b3a2");
+            
+            svg.selectAll("medianLines")
+                .data(sumstat)
+                .enter()
+                .append("line")
+                    .attr("x1", d => x(d.key) - boxWidth/2)
+                    .attr("x2", d => x(d.key) + boxWidth/2)
+                    .attr("y1", d => y(d.value.median))
+                    .attr("y2", d => y(d.value.median))
+                    .attr("stroke", "black")
+                    .style("width", 80);
         }        
     },
 });
