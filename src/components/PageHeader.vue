ac1527bff9d11a8ac609eb3d284dbe716ca53c27
dkjang <dkjang@broadinstitute.org>
2023-01-24 11:24:45 -0500

    A2f and master code merged?

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -53,45 +53,31 @@
 							class="mdkp-logo"
 						/>
 					</a>
-					<span v-if="page == 'front'" class="disease-in-session">{{
-						$store.state.diseaseInSession
-					}}</span>
-					<disease-systems
-						v-if="
-							page != 'front' &&
-							$store.state.bioPortal.diseaseSystems.length > 0
-						"
-						:page="page"
-						:diseases="$store.state.bioPortal.diseaseSystems"
-						:diseaseGroups="$store.state.bioPortal.diseaseGroups"
-						:phenotypes="rawPhenotypes"
-						:diseaseInSession="$store.state.diseaseInSession"
-						:phenotypeCorrelation="
-							$store.state.phenotypeCorrelation
-						"
-					></disease-systems>
-					<!--
-
+					<template
+						v-if="!!diseaseGroup.name && diseaseGroup.name == 'a2f'"
+					>
+						<span
+							v-if="page == 'front'"
+							class="disease-in-session"
+							>{{ $store.state.diseaseInSession }}</span
+						>
 						<disease-systems
-													page="front"
-													:diseases="
-														$store.state.bioPortal
-															.diseaseSystems
-													"
-													:diseaseGroups="
-														$store.state.bioPortal
-															.diseaseGroups
-													"
-													:phenotypes="
-														$parent.phenotypes
-													"
-													:phenotypeCorrelation="
-														$store.state
-															.phenotypeCorrelation
-													"
-												></disease-systems>
-
-						-->
+							v-if="
+								page != 'front' &&
+								$store.state.bioPortal.diseaseSystems.length > 0
+							"
+							:page="page"
+							:diseases="$store.state.bioPortal.diseaseSystems"
+							:diseaseGroups="
+								$store.state.bioPortal.diseaseGroups
+							"
+							:phenotypes="rawPhenotypes"
+							:diseaseInSession="$store.state.diseaseInSession"
+							:phenotypeCorrelation="
+								$store.state.phenotypeCorrelation
+							"
+						></disease-systems>
+					</template>
 				</div>
 				<div :class="'kp-menu-wrapper col-md-8'">
 					<menu-item
