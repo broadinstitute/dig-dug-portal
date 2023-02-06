1a4fbe0c533ce9ad2a0168c9e81e72e56d522855
dkjang <dkjang@broadinstitute.org>
2023-02-06 10:21:09 -0500

    temp save

--- src/views/BYORConfigBuilder/Template.vue
+++ src/views/BYORConfigBuilder/Template.vue
@@ -75,10 +75,12 @@
 
 					<div class="row dr-data-section">
 						<div class="col-md-12">
-							<h4>Data <sup class="optional">Tutorial </sup></h4>
+							<h4>
+								Data point <sup class="optional">Tutorial </sup>
+							</h4>
 						</div>
 
-						<div class="col-md-9">
+						<div class="col-md-12">
 							<div class="row">
 								<div class="col-md-6 col filter-col-md">
 									<div class="label">Data type</div>
@@ -120,7 +122,7 @@ data_point1.csv,data_point2.csv,data_point3.csv,data_point4.csv</textarea
 									>
 								</div>
 							</div>
-							<div class="row dr-sub-category">
+							<!--<div class="row dr-sub-category">
 								<div
 									class="
 										offset-md-1
@@ -204,7 +206,7 @@ data_point1.csv,data_point2.csv,data_point3.csv,data_point4.csv</textarea
 										</div>
 									</div>
 								</div>
-							</div>
+							</div>-->
 
 							<div class="row">
 								<div class="col-md-4 col filter-col-md">
@@ -356,148 +358,6 @@ data_point1.csv,data_point2.csv,data_point3.csv,data_point4.csv</textarea
 								</div>
 							</div>
 						</div>
-
-						<div class="col-md-3 dr-meta-info">
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">Page Mode<sup>*</sup></div>
-								<select class="form-control">
-									<option value="">Unpublished</option>
-									<option value="">
-										Review or development
-									</option>
-									<option value="">Public</option>
-								</select>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">
-									Dev ID
-									<sup class="optional"
-										>If page mode in review or dev</sup
-									>
-								</div>
-								<input
-									type="text"
-									class="form-control input-default"
-									placeholder="Dev ID"
-								/>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">
-									Dev password
-									<sup class="optional"
-										>If page mode in review or dev</sup
-									>
-								</div>
-								<input
-									type="text"
-									class="form-control input-default"
-									placeholder="Dev password"
-								/>
-							</div>
-							<hr />
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">
-									Research method node ID
-									<sup class="optional"
-										><a href="javascript:;"
-											>Tutorial</a
-										></sup
-									>
-								</div>
-								<input
-									type="text"
-									class="form-control input-default"
-									placeholder="Dev ID"
-								/>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">
-									Page header menu node ID
-									<sup class="optional"
-										><a href="javascript:;"
-											>Tutorial</a
-										></sup
-									>
-								</div>
-								<input
-									type="text"
-									class="form-control input-default"
-									placeholder="Dev password"
-								/>
-							</div>
-							<hr />
-							<div class="col-md-12 col filter-col-md">
-								<div class="form-check">
-									<input
-										class="form-check-input"
-										type="checkbox"
-										value=""
-										id="flexCheckDefault"
-									/>
-									<label
-										class="form-check-label"
-										for="flexCheckDefault"
-									>
-										Show on BYOR front
-									</label>
-								</div>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">Project summary</div>
-								<textarea
-									rows="4"
-									class="form-control"
-								></textarea>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">URL to the page</div>
-								<input
-									type="text"
-									class="form-control input-default"
-									placeholder="URL"
-								/>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="label">
-									Image to show on BYOR front
-									<sup class="optional block"
-										>2MB limit. png, gif, jpg or jpeg
-										olny</sup
-									>
-									<div class="col-md-12 col filter-col-md">
-										<button
-											class="btn btn-primary btn-sm"
-											type="button"
-										>
-											Choose file
-										</button>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-12 col filter-col-md">
-								<div class="form-check">
-									<input
-										class="form-check-input"
-										type="checkbox"
-										value=""
-										id="flexCheckDefault"
-									/>
-									<label
-										class="form-check-label"
-										for="flexCheckDefault"
-									>
-										Is project landing page <br />
-										<sup class="optional block"
-											>The summary of Page Description
-											field will show on Projects page if
-											checked. Please set only 1 page per
-											project as a project landing
-											page.</sup
-										>
-									</label>
-								</div>
-							</div>
-						</div>
 					</div>
 
 					<div class="row dr-description-section">
@@ -525,7 +385,147 @@ data_point1.csv,data_point2.csv,data_point3.csv,data_point4.csv</textarea
 							</div>
 						</div>
 					</div>
+					<div class="row dr-meta-info">
+						<div class="col-md-4 col filter-col-md">
+							<div class="label">Page Mode<sup>*</sup></div>
+							<select class="form-control">
+								<option value="">Unpublished</option>
+								<option value="">Review or development</option>
+								<option value="">Public</option>
+							</select>
+						</div>
+						<div class="col-md-4 col filter-col-md">
+							<div class="label">
+								Dev ID
+								<sup class="optional"
+									>If page mode in review or dev</sup
+								>
+							</div>
+							<input
+								type="text"
+								class="form-control input-default"
+								placeholder="Dev ID"
+							/>
+						</div>
+						<div class="col-md-4 col filter-col-md">
+							<div class="label">
+								Dev password
+								<sup class="optional"
+									>If page mode in review or dev</sup
+								>
+							</div>
+							<input
+								type="text"
+								class="form-control input-default"
+								placeholder="Dev password"
+							/>
+						</div>
+					</div>
+					<div class="row dr-meta-info">
+						<div class="col-md-6 col filter-col-md">
+							<div class="label">
+								Research method node ID
+								<sup class="optional"
+									><a href="javascript:;">Tutorial</a></sup
+								>
+							</div>
+							<input
+								type="text"
+								class="form-control input-default"
+								placeholder="Dev ID"
+							/>
+						</div>
+						<div class="col-md-6 col filter-col-md">
+							<div class="label">
+								Page header menu node ID
+								<sup class="optional"
+									><a href="javascript:;">Tutorial</a></sup
+								>
+							</div>
+							<input
+								type="text"
+								class="form-control input-default"
+								placeholder="Dev password"
+							/>
+						</div>
+					</div>
+					<div class="row dr-meta-info">
+						<div class="col-md-4 col filter-col-md">
+							<div class="form-check">
+								<input
+									class="form-check-input"
+									type="checkbox"
+									value=""
+									id="flexCheckDefault"
+								/>
+								<label
+									class="form-check-label"
+									for="flexCheckDefault"
+								>
+									Show on BYOR front
+								</label>
+							</div>
+						</div>
+						<div class="col-md-8 col filter-col-md">
+							<div class="label">
+								URL to the page
+								<sup class="optional"
+									>ex.
+									/research.html?pageid=kp_variant_sifter&phenotype=T2D&region=17:40719090-40725257</sup
+								>
+							</div>
+							<input
+								type="text"
+								class="form-control input-default"
+								placeholder="URL"
+							/>
+						</div>
+						<div class="col-md-4 col filter-col-md">
+							<div class="label">
+								Image to show on BYOR front
+								<sup class="optional block"
+									>2MB limit. png, gif, jpg or jpeg olny</sup
+								>
+								<div class="col-md-12 col filter-col-md">
+									<button
+										class="btn btn-primary btn-sm"
+										type="button"
+									>
+										Choose file
+									</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8 col filter-col-md">
+							<div class="label">Project summary</div>
+							<textarea rows="4" class="form-control"></textarea>
+						</div>
+
+						<div class="col-md-12 col filter-col-md">
+							<div class="form-check">
+								<input
+									class="form-check-input"
+									type="checkbox"
+									value=""
+									id="flexCheckDefault"
+								/>
+								<label
+									class="form-check-label"
+									for="flexCheckDefault"
+								>
+									Is project landing page <br />
+									<sup class="optional block"
+										>The summary of Page Description field
+										will show on Projects page if checked.
+										Please set only 1 page per project as a
+										project landing page.</sup
+									>
+								</label>
+							</div>
+						</div>
+					</div>
 				</div>
+
 				<div class="card-body">
 					<div class="row dr-submit-section">
 						<div class="col-md-12 col filter-col-md">
@@ -547,6 +547,7 @@ data_point1.csv,data_point2.csv,data_point3.csv,data_point4.csv</textarea
 	border-radius: 5px;
 	background-color: #eeeeee;
 	padding: 15px 0 10px 0;
+	margin-bottom: 5px;
 }
 .dr-submit-section {
 	text-align: center;
