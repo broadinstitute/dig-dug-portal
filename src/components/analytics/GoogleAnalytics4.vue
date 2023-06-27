e729ffd557c45adfc6418b4b2c7fb48d0fa3326f
Quy <quy@broad.dev>
2023-06-27 09:30:22 -0400

    new ga4 component

---
+++ src/components/analytics/GoogleAnalytics4.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="analytics"></div>
+</template>
+<script>
+import Vue from "vue";
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+    config: {
+        id: process.env.GA4_ID || "G-D3G6XZYGBR",
+        params: { send_page_view: true },
+    },
+    appName: "HuGeAMP",
+    pageTrackerScreenviewEnabled: true,
+});
+
+export default {
+    name: "GoogleAnalytics",
+};
+</script>
