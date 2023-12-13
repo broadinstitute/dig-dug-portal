f6a73a7b9a673e71ce9317979ba75a37a497be41
hanzhang2021 <zhanghanri2019@gmail.com>
2023-12-13 14:41:03 -0500

    Create Template.vue

---
+++ src/portals/Neph/views/Index/Template.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="front-page-body">
+        <!-- Header -->
+        <page-header
+            :disease-group="$parent.diseaseGroup"
+            :front-contents="$parent.frontContents"
+        ></page-header>
+
+        <!-- Body -->
+        <div v-if="$parent.diseaseGroup">
+            <div class="fluid">
+                <div
+                    :class="
+                        'front-top-banner-' +
+                        $parent.diseaseGroup.name +
+                        'kp front-top-banner'
+                    "
+                >
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="front-logo-wrapper">
+                                    <img
+                                        class="front-logo-img"
+                                        v-if="
+                                            $parent.frontContents
+                                                .field_front_logo
+                                        "
+                                        :src="
+                                            'https://kp4cd.org/sites/default/files/vueportal/' +
+                                            $parent.frontContents
+                                                .field_front_logo
+                                        "
+                                    />
+                                    <span
+                                        :class="
+                                            'front-logo-tagline front-logo-tagline-' +
+                                            $parent.diseaseGroup.name +
+                                            'kp'
+                                        "
+                                         v-html="
+                                            $parent.frontContents.field_tagline
+                                        "
+                                        ></span
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="col-md-12 portal-front-tabs">
+                                <div class="front-gene-search-wrapper">
+                                    <div class="col-md-12 input-wrapper">
+                                        <autocomplete
+                                            :placeholder="'Search for a gene'"
+                                            :matches="$parent.matchingGenes"
+                                            :match-key="null"
+                                            @input-change="
+                                                $store.dispatch(
+                                                    'lookupGenes',
+                                                    $event
+                                                )
+                                            "
+                                            @keyup-enter="
+                                                $store.dispatch(
+                                                    'exploreRegionOrVariant',
+                                                    $event
+                                                )
+                                            "
+                                            @item-select="
+                                                $store.dispatch(
+                                                    'onGeneChange',
+                                                    $event
+                                                )
+                                            "
+                                        ></autocomplete>
+                                    </div>
+                                    <div class="region-search-examples">
+                                        <documentation
+                                            name="home.example"
+                                            :group="$parent.diseaseGroup"
+                                        ></documentation>
+                                    </div>
+
+                                    <div
+                                        class="text-danger"
+                                        v-show="
+                                            $store.state.invalidGeneOrRegion
+                                        "
+                                    >
+                                        Invalid gene name or region or variant
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container static-content-section">
+                <div class="row">
+                    <div class="col-md-7">
+                        <about-portal-section
+                            :front-contents="$parent.frontContents"
+                        ></about-portal-section>
+                        <datasets-section
+                            :disease-group="$parent.diseaseGroup"
+                            :disease-groups="
+                                $store.state.bioPortal.diseaseGroups
+                            "
+                            :datasets-info="$store.state.kp4cd.datasetsInfo"
+                        ></datasets-section>
+                        <under-datasets-section
+                            :front-contents="$parent.frontContents"
+                        ></under-datasets-section>
+                    </div>
+                    <div class="col-md-5">
+                        <news-feed-section
+                            :disease-group="$parent.diseaseGroup"
+                            :news-feed="$store.state.kp4cd.newsFeed"
+                        ></news-feed-section>
+                        <about-project-section
+                            :front-contents="$parent.frontContents"
+                        ></about-project-section>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        <!-- Footer-->
+        <page-footer :disease-group="$parent.diseaseGroup"></page-footer>
+    </div>
+</template>
