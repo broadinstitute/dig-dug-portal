09c69261a0c22a814f18d11848940720d2aaee68
hanzhang2021 <zhanghanri2019@gmail.com>
2023-12-13 14:39:54 -0500

    Create store.js

---
+++ src/portals/Neph/views/Documentation/store.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import bioPortal from "@/modules/bioPortal";
+import kp4cd from "@/modules/kp4cd";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {
+        bioPortal,
+        kp4cd,
+    },
+    state: {
+    },
+    mutations: {
+    },
+    getters: {
+    },
+    actions: {
+    }
+});
