ead616926f457223c97f0db1557cce715d3175be
hanzhang2021 <zhanghanri2019@gmail.com>
2023-12-13 14:38:37 -0500

    Create FilterWraper.vue

---
+++ src/portals/Neph/components/FilterWraper.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="FilterDiv" class="filtering-ui-wrapper container-fluid search-header">
+        <div
+            id="FilterContent"
+            class="row filtering-ui-content search-header-content hidden"
+        >
+            <slot></slot>
+        </div>
+        <a
+            v-on:click="() => this.showHideElement('FilterContent')"
+            class="reset-page-parameters"
+            href="javascript:;"
+        >
+            <!-- Set page level parameters -->
+            Search gene
+        </a>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+import uiUtils from "@/utils/uiUtils";
+
+export default Vue.component("filter-wrapper", {
+    props: ["datasetInfo"],
+
+    mounted() {
+        window.addEventListener("scroll", this.onScroll);
+    },
+    beforeDestroy() {
+        window.removeEventListener("scroll", this.onScroll);
+    },
+    computed: {
+        tableTop() {
+            let eglTable = document.getElementsById("FilterDiv");
+            let rect = eglTable.getBoundingClientRect();
+            let scrollTop = document.documentElement.scrollTop
+                ? document.documentElement.scrollTop
+                : document.body.scrollTop;
+
+            let tableTop = rect.top + scrollTop;
+
+            return tableTop;
+        },
+    },
+    methods: {
+        ...uiUtils,
+        onScroll(e) {
+            let windowTop = window.top.scrollY;
+
+            let element = document.getElementsById("FilterDiv");
+            if (windowTop > this.tableTop) {
+                if (!element.classList.contains("fixed-header")) {
+                    element.classList.add("fixed-header");
+                }
+            } else {
+                if (element.classList.contains("fixed-header")) {
+                    element.classList.remove("fixed-header");
+                }
+            }
+        },
+    },
+});
+</script>
+
+<style>
+</style>
