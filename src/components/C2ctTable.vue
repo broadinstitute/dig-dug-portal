3cf32af733438f3abc1ee01159e487519b51e575
Annie Moriondo <amoriond@broadinstitute.org>
2024-05-06 15:14:36 -0400

    further formatting for c2ct table

--- src/components/C2ctTable.vue
+++ src/components/C2ctTable.vue
@@ -16,6 +16,15 @@
                 :per-page="perPage"
                 :current-page="currentPage"
             >
+                <template #cell(chromosome)="r">
+                    <a
+                        :href="`/region.html?chr=${
+                            r.item.chromosome}&start=${
+                            r.item.clumpStart}&end=${
+                            r.item.clumpEnd}`">
+                        {{ `${r.item.chromosome}:${r.item.clumpStart}-${r.item.clumpEnd}` }}
+                    </a>
+                </template>
             </b-table>
             <b-pagination
                 v-model="currentPage"
@@ -47,19 +56,68 @@ export default Vue.component("c2ct-table", {
             currentPage: 1,
             sortByCorrelation: false,
             fields: [
-            "annotation",
-            "tissue",
-            "biosample",
-            "chromosome",
-            "clumpStart",
-            "clumpEnd",
-            "overlapLeadSNP",
-            "posteriorProbability",
-            "adjustedPP",
-            "totalEntropy",
-            "adjustedEntropy",
-            "Q",
-            "Q_adj",
+                {
+                    key: "annotation",
+                    label: "Annotation",
+                    formatter: Formatters.annotationFormatter,
+                    sortable: true
+                },
+                {
+                    key: "tissue",
+                    label: "Tissue",
+                    formatter: Formatters.tissueFormatter,
+                    sortable: true
+                },
+                {
+                    key: "biosample",
+                    label: "Biosample",
+                    formatter: Formatters.tissueFormatter,
+                    sortable: true
+                },
+                {
+                    key: "chromosome",
+                    label: "Clump start - end",
+                },
+                {
+                    key: "overlapLeadSNP",
+                    label: "Overlap Lead SNP"
+                },
+                {
+                    key: "posteriorProbability",
+                    label: "Posterior probability",
+                    formatter: Formatters.tpmFormatter,
+                    sortable: true
+                },
+                {
+                    key: "adjustedPP",
+                    label: "Adjusted PP",
+                    formatter: Formatters.tpmFormatter,
+                    sortable: true
+                },
+                {
+                    key: "totalEntropy",
+                    label: "Total entropy",
+                    formatter: Formatters.tpmFormatter,
+                    sortable: true
+                },
+                {
+                    key: "adjustedEntropy",
+                    label: "Adjusted entropy",
+                    formatter: Formatters.tpmFormatter,
+                    sortable: true
+                },
+                {
+                    key: "Q",
+                    label: "Q",
+                    formatter: Formatters.tpmFormatter,
+                    sortable: true
+                },
+                { 
+                    key: "Q_adj",
+                    label: "Adjusted Q",
+                    formatter: Formatters.tpmFormatter,
+                    sortable: true,
+                }
         ]
         };
     },
@@ -80,6 +138,9 @@ export default Vue.component("c2ct-table", {
         pValueFormatter: Formatters.pValueFormatter,
         effectFormatter: Formatters.effectFormatter,
         intFormatter: Formatters.intFormatter,
+        annotationFormatter: Formatters.annotationFormatter,
+        tissueFormatter: Formatters.tissueFormatter,
+        tpmFormatter: Formatters.tpmFormatter
     },
 });
 </script>
