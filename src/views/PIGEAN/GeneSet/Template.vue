86755c3d349a576041b31df5aa41d71c8ff1143e
Quy <quy@broad.dev>
2024-05-08 09:34:41 -0400

    geneset boilplate

---
+++ src/views/PIGEAN/GeneSet/Template.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <!-- Header -->
+        <page-header
+            :disease-group="$parent.diseaseGroup"
+            :front-contents="$parent.frontContents"
+            :raw-phenotypes="$parent.rawPhenotypes"
+        >
+        </page-header>
+
+        <!-- Body -->
+        <div class="container-fluid mdkp-body">
+            <search-header-wrapper>
+                <!-- Wrap page level searchs with "pageSearchParameters" div -->
+                <span class="gene-search-tip">
+                    <sup>*</sup>
+                    Alias names are converted to gene symbols
+                </span>
+                <div class="col filter-col-md">
+                    <div class="label">Gene</div>
+                    <gene-selectpicker></gene-selectpicker>
+                </div>
+                <div class="col filter-col-md">
+                    <div class="label">Search</div>
+                    <button
+                        id="regionSearchGo"
+                        class="btn btn-light btn-sm go"
+                        type="button"
+                        @click="$store.dispatch('queryGeneName')"
+                    >
+                        GO
+                    </button>
+                </div>
+            </search-header-wrapper>
+            <div class="gene-page-header card mdkp-card">
+                <div class="row card-body">
+                    <div class="col-md-8 gene-page-header-title">Gene</div>
+                    <div class="col-md-4 gene-page-header-title">Navigate</div>
+                    <div class="col-md-8 gene-page-header-body">
+                        <div>
+                            <span>{{
+                                $store.state.geneName.toUpperCase()
+                            }}</span>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gene-page-header-body">
+                        <div v-if="$parent.symbolName" class="input-group">
+                            <button
+                                class="btn btn-primary input-group-prepend explore-region-btn"
+                                style="margin-right: 20px"
+                                :title="$parent.regionText"
+                                @click="$parent.exploreRegion()"
+                            >
+                                Explore Region
+                            </button>
+                            <button
+                                class="btn btn-primary input-group-append explore-region-btn"
+                                :title="$parent.regionTextExpanded"
+                                @click="$parent.exploreRegion(50000)"
+                            >
+                                Explore &plusmn; 50 kb
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        Gene
+                        {{ $store.state.geneName }}
+                    </h4>
+                    [scatter plot]<br />
+                    [Table for gene]
+                    https://bioindex-dev.hugeamp.org/api/bio/query/pigean-gene-set?q=mp_abnormal_germ_layer_development<br />
+                    subtable:
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <page-footer :disease-group="$parent.diseaseGroup"></page-footer>
+    </div>
+</template>
+<style scoped></style>
---
+++ src/views/PIGEAN/GeneSet/main.js
@@ -0,0 +1,65 @@
+import Vue from "vue";
+import Template from "./Template.vue";
+import store from "./store.js";
+
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import PageHeader from "@/components/PageHeader.vue";
+import PageFooter from "@/components/PageFooter.vue";
+import SearchHeaderWrapper from "@/components/SearchHeaderWrapper.vue";
+import GeneSelectPicker from "@/components/GeneSelectPicker.vue";
+
+import keyParams from "@/utils/keyParams";
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+new Vue({
+    store,
+    modules: {},
+    components: {
+        PageHeader,
+        PageFooter,
+        SearchHeaderWrapper,
+        GeneSelectPicker,
+    },
+
+    data() {
+        return {};
+    },
+    computed: {
+        diseaseGroup() {
+            return this.$store.getters["bioPortal/diseaseGroup"];
+        },
+        frontContents() {
+            let contents = this.$store.state.kp4cd.frontContents;
+            if (contents.length === 0) {
+                return {};
+            }
+            return contents[0];
+        },
+    },
+    watch: {
+        diseaseGroup(group) {
+            this.$store.dispatch("kp4cd/getFrontContents", group.name);
+        },
+    },
+
+    created() {
+        if (keyParams.gene) {
+            console.log("gene", keyParams.gene);
+        }
+        this.$store.dispatch("queryGeneName", this.$store.state.geneName);
+        this.$store.dispatch("bioPortal/getDiseaseGroups");
+        this.$store.dispatch("bioPortal/getPhenotypes");
+    },
+
+    methods: {},
+
+    render(createElement, context) {
+        return createElement(Template);
+    },
+}).$mount("#app");
