7ff42c6c3b55403dc1086ad5605391c7c3023d97
Annie Moriondo <104587901+moriondo2022@users.noreply.github.com>
2024-05-21 18:18:11 -0400

    Pigean labels (#659)
    * changed dropdown menu labeling
    
    * 0 large, 2 medium, 4 small

--- src/components/SigmaSelectPicker.vue
+++ src/components/SigmaSelectPicker.vue
@@ -1,8 +1,8 @@
 <template>
 	<select v-model="sigma">
-		<option value="sigma0">0</option>
-		<option value="sigma2">2</option>
-		<option value="sigma4">4</option>
+		<option value="sigma0">Large</option>
+		<option value="sigma2">Medium</option>
+		<option value="sigma4">Small</option>
 	</select>
 </template>
 
