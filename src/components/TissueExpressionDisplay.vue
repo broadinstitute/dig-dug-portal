5a5c98cfecf0e72ed6dc2e845f8c2af3d6c8198d
Annie Moriondo <amoriond@broadinstitute.org>
2024-05-24 16:18:07 -0400

    refactoring

--- src/components/TissueExpressionDisplay.vue
+++ src/components/TissueExpressionDisplay.vue
@@ -18,7 +18,7 @@
                     <research-expression-plot 
                         :plotData="genePlotData"
                         :highlightedDataset="datasetDetails"
-                        :plotName="`gene_expression_${tissue.replaceAll(' ', '_')}_p${currentPage}`">
+                        :plotName="`gene_expression_${tissue.replaceAll(' ', '_')}`">
                     </research-expression-plot>
                 </div>
                 <div v-else>Loading expression plot...</div>
@@ -36,7 +36,6 @@
 </template>
 <script>
 import Vue from "vue";
-import { query } from "@/utils/bioIndexUtils";
 import Formatters from "@/utils/formatters";
 import ResearchExpressionPlot from "@/components/researchPortal/ResearchExpressionPlot.vue";
 import TissueExpressionTable from "@/components/TissueExpressionTable.vue";
@@ -55,138 +54,18 @@ export default Vue.component("TissueExpressionDisplay", {
     },
     data() {
         return {
-            perPage: 10,
-            currentPage: 1,
-            currentTable: [],
-            genePlotData: [],
-            rawData: [],
-            evidence: {},
-            plotData: [],
-            tableData: [],
-            sortKey: "",
+            genePlotData: [],            
             datasetDetails: {},
-            newTableFields: [
-                { key: "gene",
-                    label: "Gene",
-                    sortable: true,
-                    tdClass: "gene-findable" },
-                { key: "nSamples",
-                    label: "Samples",
-                    sortable: true },
-                { key: "meanTpm",
-                    label: "Mean TPM",
-                    sortable: true,
-                    formatter: Formatters.tpmFormatter },
-                { key: "show_datasets"}
-            ],
-            datasetFields: [
-                {
-                    key: "biosample",
-                    formatter: Formatters.tissueFormatter,
-                    sortable: true,
-                },
-                {
-                    key: "collection",
-                    formatter: (value) => value.toString(", "),
-                },
-                { key: "dataset", sortable: true },
-                {
-                    key: "diseaseTermName",
-                    label: "Disease",
-                    sortable: true,
-                    formatter: (value) =>
-                        !value
-                            ? "Not available"
-                            : Formatters.tissueFormatter(value),
-                },
-                {
-                    key: "Min TPM",
-                    formatter: Formatters.tpmFormatter,
-                    sortable: true,
-                },
-                {
-                    key: "Q1 TPM",
-                    formatter: Formatters.tpmFormatter,
-                    sortable: true,
-                },
-                {
-                    key: "Median TPM",
-                    formatter: Formatters.tpmFormatter,
-                    sortable: true,
-                },
-                {
-                    key: "Q3 TPM",
-                    formatter: Formatters.tpmFormatter,
-                    sortable: true,
-                },
-                {
-                    key: "Max TPM",
-                    formatter: Formatters.tpmFormatter,
-                    sortable: true,
-                },
-                { key: "nSamples", label: "Samples", sortable: true },
-            ],
         };
-    },
-    mounted() {
-        this.populateEvidence(this.currentGenes);
-    },
-    computed: {
-        currentGenes(){
-            return this.currentTable.map(d => d.gene);
-        }
     },
     methods: {
         tissueFormatter: Formatters.tissueFormatter,
-        async showEvidence(gene) {
-            if (gene) {
-                //check if evidence object already has key equal gene
-                if (!this.evidence[gene]) {
-                    let data = await query("gene-expression", gene);
-                    data = data.filter(
-                        (d) => d.tissue === this.tissue.replace(" ", "_")
-                    );
-                    Vue.set(this.evidence, gene, this.parseData(data));
-                }
-            }
-        },
-        async populateEvidence(genes) {
-            await Promise.all(genes.map((gene) => this.showEvidence(gene)));
-            this.genePlotData = genes.flatMap(gene => this.evidence[gene]);
-        },
-        parseData(data){
-            data.forEach((entry) => {
-                if(typeof entry.tpmForAllSamples === 'string'){
-                    let tpms = entry.tpmForAllSamples
-                        .split(",")
-                        .map((i) => !!Number.isNaN(parseFloat(i)) ? 0 : parseFloat(i));
-                    entry["tpmForAllSamples"] = tpms;
-                }
-                entry["keyField"] = entry.gene;
-				entry["tissue"] = Formatters.tissueFormatter(entry["tissue"]);
-				entry["Min TPM"] = parseFloat(entry.minTpm);
-				entry["Q1 TPM"] = parseFloat(entry.firstQuTpm);
-				entry["Median TPM"] = parseFloat(entry.medianTpm);
-				entry["Q3 TPM"] = parseFloat(entry.thirdQuTpm);
-				entry["Max TPM"] = parseFloat(entry.maxTpm);
-				entry["nSamples"] = parseInt(entry.nSamples);
-			});
-            return data;
-        },
-        getPlotData(plotData) {
-            this.plotData = plotData;
-        },
         highlight(details){
             this.datasetDetails = details;
         },
         getGeneData(data){
             this.genePlotData = data;
-        }
-    },
-    watch: {
-        async currentGenes(latestGenes){
-            await this.populateEvidence(latestGenes);
-        }
+        },
     },
 });
 </script>
