bb3ba5ea1070f642e4d6597f82f6c0a100c1867c
Annie Moriondo <amoriond@broadinstitute.org>
2024-08-20 12:05:18 -0400

    MenuItem component

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -4,12 +4,10 @@
 
 <script>
 import Vue from "vue";
-import { BIO_INDEX_HOST } from "@/utils/bioIndexUtils";
 import documentationParser from "@/utils/documentationUtils";
-import queryString from "query-string";
 
 export default Vue.component("menu-item", {
-    props: ["name", "group", "contentFill", "menu", "link"],
+    props: ["name", "contentFill", "contentMap"],
 
     data: (context) => {
         return {
@@ -18,62 +16,23 @@ export default Vue.component("menu-item", {
         };
     },
 
-    mounted() {
-        if (!!this.name) {
-            // fetch the documentation data and resolve it in data
-            let docGroup = !!this.group ? this.group : "md";
-
-            let qs = queryString.stringify({
-                q: this.name,
-                group: docGroup, //get this from state
-            });
-            let json = fetch(`${BIO_INDEX_HOST}/api/portal/documentation?${qs}`)
-                .then((resp) => {
-                    if (resp.status === 422) {
-                        throw Error("missing parameters");
-                        // throw Error("In Documentation"+' '+resp.json().detail[0].type+' '+resp.json().detail[0].msg+' '+resp.json().detail[0].loc);
-                    }
-                    if (resp.status === 200) {
-                        return resp;
-                    }
-                })
-                .then((resp) => resp.json())
-                .then((json) => {
-                    if (json.data.length > 0) {
-                        this.converter = documentationParser.makeConverter(
-                            json.data[0].content,
-                            this.contentFill,
-                            this.name
-                        );
-
-                        this.content = json.data[0].content;
-                    } else {
-                        throw new Error(
-                            "No content returned for given name " +
-                                this.name +
-                                " and group " +
-                                docGroup
-                        );
-                    }
-                });
-        }
-    },
     computed: {
         menuContent() {
-            if (!!this.content) {
-                return this.converter.makeHtml(this.content);
+            if (!!this.contentMap && !!this.contentMap[this.name]){
+                let content = this.contentMap[this.name].content;
+                let contentFill = this.contentFill || {};
+                let converter = documentationParser.makeConverter(
+                    content,
+                    contentFill,
+                    this.name
+                );
+                return converter.makeHtml(content);
             }
-        },
+            return "";
+        }
     },
     watch: {
-        contentFill: function (newContentFill) {
-            //create a new convertor that overides the one we are storing in data
-            this.converter = documentationParser.makeConverter(
-                this.content,
-                newContentFill,
-                this.name
-            );
-        },
+
     },
 
     methods: {},
