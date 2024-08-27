50c9c3f508fb5b30ae677e9209bf93722ef398fb
Alex Shilin <shilin@broadinstitute.org>
2024-08-27 11:39:39 -0400

    starting landing page depot viz

--- src/views/MATKP/Index/Template.vue
+++ src/views/MATKP/Index/Template.vue
@@ -19,19 +19,44 @@
                             <div class="f-row" style="gap:50px">
                                 <div class="anatomy">
                                     <div class="anatomy-item human-male">
-                                        <img src="https://hugeampkpncms.org/sites/default/files/users/user32/matkp/homo_sapiens.male_.svg">
+                                        <div class="anatomy-points" style="position:absolute; width:100%; height:100%; z-index: 1;">
+                                            <template v-for="depot in $parent.depots.human">
+                                                <div v-if="depot.pos.length>0 && depot.pos[0].length===2" v-for="point in depot.pos" class="depot-point" :data-depot="`${depot.ontology}`" :data-category="depot.category" style="position:absolute; width:15px; height:15px; transform:translate(-10%,-10%); border-radius: 50%; background:gold" :style="`left:${point[0]*$parent.anatomyScale}px; top:${point[1]*$parent.anatomyScale}px`"></div>
+                                            </template>
+                                        </div>
+                                        <div>
+                                            <div style="position: absolute;width: 50%;overflow: hidden;right: 0;transform: scaleX(-1);">
+                                                <img style="width:200px" src="https://hugeampkpncms.org/sites/default/files/users/user32/matkp/homo_sapiens.male_.svg">
+                                            </div>
+                                            <div style="position: absolute;width: 50%;overflow: hidden;border-right: 1px dashed black;">
+                                                <img style="width:200px" src="https://hugeampkpncms.org/sites/default/files/users/user32/matkp/homo_sapiens.female.svg">
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="f-row">
-                                    <div class="depot-items">
+                                    <div class="depot-items" style="height:300px">
                                         <div class="label underline">Adipose Tissue Depots</div>
-                                        <div class="f-col" style="gap:5px; padding:20px 0 0">
-                                            <div class="depot-item" v-for="value in $parent.filterOptions['depot']">{{ value.text }}</div>
+                                       
+                                        <div class="f-row" style="gap:15px;">
+                                            <template v-for="(depots, category) in $parent.depotsByCategory">
+                                                <div class="f-col" style="gap:5px;">
+                                                    <div class="depot-category" style="font-weight:bold; text-transform: capitalize; padding:5px 0" @mouseover="$parent.highlightDepotCategory(category)">{{ category }}</div>
+                                                    <div class="depot-item" v-for="depot in depots" @mouseover="$parent.highlightDepot(depot.ontology)">{{ depot.name }}</div>
+                                                </div>
+                                            </template>
                                         </div>
+                                         <!--
+                                        <div class="f-col" style="gap:5px; padding:20px 0 0; flex-wrap: wrap; height: -webkit-fill-available;">
+                                            <div class="depot-item" v-for="depot in $parent.depots.human" @mouseover="$parent.highlightDepot(depot.ontology)">{{ depot.name }}</div>
+                                        </div>
+                                        -->
                                     </div>
+                                    <!--
                                     <div class="depot-info">
                                         In humans, adipose tissue is located beneath the skin (subcutaneous fat), around internal organs (visceral fat), in bone marrow (yellow bone marrow) and in breast tissue. Adipose tissue is found in specific locations, which are referred to as adipose depots. Adipose tissue contains several cell types, with the highest percentage of cells being adipocytes, which contain fat droplets. Other cell types include fibroblasts, macrophages, and endothelial cells. Adipose tissue contains many small blood vessels.; Mice have eight major adipose depots, four of which are within the abdominal cavity. The paired gonadal depots are attached to the uterus and ovaries in females and the epididymis and testes in males; the paired retroperitoneal depots are found along the dorsal wall of the abdomen, surrounding the kidney, and, when massive, extend into the pelvis. The mesenteric depot forms a glue-like web that supports the intestines, and the omental depot, which originates near the stomach and spleen, and, when massive, extends into the ventral abdomen.
                                     </div>
+                                    -->
                                 </div>
                             </div>
                         </div>
@@ -56,10 +81,10 @@
 .content-wrap{
     padding: 20px 20px 100px 20px;
     line-height: normal;
-    opacity: 0.4;
 }
 .anatomy-item {
     width: 200px;
+    position:relative;
 }
 .anatomy-item img {
     height: 100%;
@@ -71,6 +96,7 @@
     border-radius: 5px 0 0 5px;
     cursor: pointer;
     font-size: 14px;
+    text-transform: capitalize;
 }
 .depot-item:hover {
     background: #dddddd;
@@ -86,5 +112,11 @@
     font-size: 14px;
     height: fit-content;
     border-radius: 10px;
+}
+.depot-point{
+    opacity: 0;
+}
+.depot-point.on{
+    opacity: 1;
 }
 </style>
--- src/views/MATKP/Index/main.js
+++ src/views/MATKP/Index/main.js
@@ -47,6 +47,288 @@ new Vue({
                 //{key:"Download", label:"", sortable: false},
                 {key:"datasetId", label:"", sortable: false},
             ],
+
+            depotsByCategory: null,
+            anatomyScale: 1.85, //svg display size / svg actual size
+            depots: {
+                human:[
+                    {
+                        "name": "craniofacial",
+                        "ontology": "h1",
+                        "category": "cranio-cervical",
+                        "pos": [[55.671,15.214],[51.291,0.461]]
+                    },{
+                        "name": "retroorbital",
+                        "ontology": "h2",
+                        "category": "cranio-cervical",
+                        "pos": [[47.873,11.424]]
+                    },{
+                        "name": "submental",
+                        "ontology": "h3",
+                        "category": "subcutaneous",
+                        "pos": [[51.140,27.393]]
+                    },{
+                        "name": "perithyroidal",
+                        "ontology": "h4",
+                        "category": "cranio-cervical",
+                        "pos": [[49.803,29.329]]
+                    },{
+                        "name": "cervical",
+                        "ontology": "h5",
+                        "category": "subcutaneous",
+                        "pos": [[55.858,25.095]]
+                    },{
+                        "name": "superclavicular",
+                        "ontology": "h6",
+                        "category": "subcutaneous",
+                        "pos": [[58.641,31.386]]
+                    },{
+                        "name": "periaortic",
+                        "ontology": "h7",
+                        "category": "visceral",
+                        "pos": [[53.075,39.370]]
+                    },{
+                        "name": "axillary",
+                        "ontology": "h8",
+                        "category": "subcutaneous",
+                        "pos": [[34.807,39.975]]
+                    },{
+                        "name": "intercostal",
+                        "ontology": "h9",
+                        "category": "thoracic",
+                        "pos": [[43.397,42.152]]
+                    },{
+                        "name": "mediastinal",
+                        "ontology": "h10",
+                        "category": "thoracic",
+                        "pos": [[49.567,46.508]]
+                    },{
+                        "name": "paravertebral",
+                        "ontology": "h11",
+                        "category": "thoracic",
+                        "pos": [[49.083,52.436],[53.559,52.436]]
+                    },{
+                        "name": "epicardial",
+                        "ontology": "h12",
+                        "category": "thoracic",
+                        "pos": [[55.495,44.452]]
+                    },{
+                        "name": "pericardial",
+                        "ontology": "h13",
+                        "category": "thoracic",
+                        "pos": [[55.311,47.092]]
+                    },{
+                        "name": "retoperitoneal",
+                        "ontology": "h14",
+                        "category": "visceral",
+                        "pos": [[52.547,56.243]]
+                    },{
+                        "name": "epineural",
+                        "ontology": "h15",
+                        "category": "specialized",
+                        "pos": [[51.319,55.813]]
+                    },{
+                        "name": "perineural",
+                        "ontology": "h16",
+                        "category": "specialized",
+                        "pos": [[51.319,55.813]]
+                    },{
+                        "name": "suprarenal",
+                        "ontology": "h17",
+                        "category": "visceral",
+                        "pos": [[58.504,56.734]]
+                    },{
+                        "name": "perirenal",
+                        "ontology": "h18",
+                        "category": "visceral",
+                        "pos": [[59.610,58.700]]
+                    },{
+                        "name": "omental",
+                        "ontology": "h19",
+                        "category": "visceral",
+                        "pos": [[46.099,59.744]]
+                    },{
+                        "name": "superficial abdominal",
+                        "ontology": "h20",
+                        "category": "subcutaneous",
+                        "pos": [[39.528,62.200]]
+                    },{
+                        "name": "deep abdominal",
+                        "ontology": "h21",
+                        "category": "subcutaneous",
+                        "pos": [[41.861,65.946]]
+                    },{
+                        "name": "mesenteric",
+                        "ontology": "h22",
+                        "category": "visceral",
+                        "pos": [[56.846,62.262]]
+                    },{
+                        "name": "epiploic",
+                        "ontology": "h23",
+                        "category": "visceral",
+                        "pos": [[56.355,66.745]]
+                    },{
+                        "name": "mammary",
+                        "ontology": "h24",
+                        "category": "subcutaneous",
+                        "pos": [[39.528,49.672]]
+                    },{
+                        "name": "gluteal",
+                        "ontology": "h25",
+                        "category": "subcutaneous",
+                        "pos": [[34.628,83.639]]
+                    },{
+                        "name": "periprostatic",
+                        "ontology": "h26",
+                        "category": "visceral",
+                        "pos": [[54.552,88.103]]
+                    },{
+                        "name": "femoral",
+                        "ontology": "h27",
+                        "category": "subcutaneous",
+                        "pos": [[67.507,108.026]]
+                    },{
+                        "name": "bone marrow",
+                        "ontology": "h28",
+                        "category": "specialized",
+                        "pos": [[61.628,114.994]]
+                    },{
+                        "name": "intraarticular",
+                        "ontology": "h29",
+                        "category": "specialized",
+                        "pos": [[62.172,137.421]]
+                    },{
+                        "name": "inter-muscular",
+                        "ontology": "h30",
+                        "category": "specialized",
+                        "pos": [[64.568,119.566]]
+                    },{
+                        "name": "intra-muscular",
+                        "ontology": "h31",
+                        "category": "specialized",
+                        "pos": [[64.568,119.566]]
+                    },{
+                        "name": "perivascular",
+                        "ontology": "h32",
+                        "category": "specialized",
+                        "pos": [[41.814,161.045]]
+                    },{
+                        "name": "dermal",
+                        "ontology": "h33",
+                        "category": "specialized",
+                        "pos": [[70.004,153.853]]
+                    },{
+                        "name": "subq",
+                        "ontology": "h34",
+                        "category": "subcutaneous",
+                        "pos": [[70.004,153.853]]
+                    },{
+                        "name": "specialized fat",
+                        "ontology": "h35",
+                        "category": "specialized",
+                        "pos": [[48.666,188.256],[58.355,182.704]]
+                    }
+                ],
+                "mouse":[
+                    {
+                        "name": "craniofacial",
+                        "ontology": "m1",
+                        "category": "specialized"
+                    },{
+                        "name": "dermal",
+                        "ontology": "m2",
+                        "category": "specialized"
+                    },{
+                        "name": "subq",
+                        "ontology": "m3",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "suprascapular",
+                        "ontology": "m4",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "intrascapular",
+                        "ontology": "m5",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "triceps",
+                        "ontology": "m6",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "cardiac",
+                        "ontology": "m7",
+                        "category": "perivascular"
+                    },{
+                        "name": "anterior axillary",
+                        "ontology": "m8",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "retroperitoneal",
+                        "ontology": "m9",
+                        "category": "perivascular"
+                    },{
+                        "name": "perirenal",
+                        "ontology": "m10",
+                        "category": "perivascular"
+                    },{
+                        "name": "mesenteric",
+                        "ontology": "m11",
+                        "category": "perivascular"
+                    },{
+                        "name": "inguinal",
+                        "ontology": "m12",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "perigonadal",
+                        "ontology": "m13",
+                        "category": "perivascular"
+                    },{
+                        "name": "epididymal",
+                        "ontology": "m14",
+                        "category": "perivascular"
+                    },{
+                        "name": "periovarian",
+                        "ontology": "m15",
+                        "category": "perivascular"
+                    },{
+                        "name": "superclavicular",
+                        "ontology": "m16",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "cervical",
+                        "ontology": "m17",
+                        "category": "subcutaneous"
+                    },{
+                        "name": "intrascapular",
+                        "ontology": "m18",
+                        "category": "visceral"
+                    },{
+                        "name": "axillary",
+                        "ontology": "m19",
+                        "category": "visceral"
+                    },{
+                        "name": "mediastinal",
+                        "ontology": "m20",
+                        "category": "visceral"
+                    },{
+                        "name": "perivascular",
+                        "ontology": "m21",
+                        "category": "specialized"
+                    },{
+                        "name": "perirenal",
+                        "ontology": "m22",
+                        "category": "visceral"
+                    },{
+                        "name": "popliteal",
+                        "ontology": "m23",
+                        "category": "specialized"
+                    },{
+                        "name": "bone marrow",
+                        "ontology": "m24",
+                        "category": "specialized"
+                    }
+                ]
+            }
         }
     },
 
@@ -87,6 +369,7 @@ new Vue({
 
     mounted() {
         this.getDatasets();
+        this.parseDepotsByCategory();
     },
 
     async created() {
@@ -106,6 +389,38 @@ new Vue({
             console.log(this.filterOptions);
             //this.filterOptions(jsonObjects);
         },
+
+        parseDepotsByCategory(){
+            const depotsByCategory = this.depots.human.reduce((acc, depot) => {
+                const category = depot.category;
+                if (!acc[category]) acc[category] = [];
+                acc[category].push(depot);
+                return acc;
+            }, {});
+            console.log('depotsByCategory',depotsByCategory);
+            this.depotsByCategory = depotsByCategory;
+        },
+
+        highlightDepot(depot){
+            const els = document.querySelectorAll('.depot-point');
+            els.forEach(el=>{
+                el.classList.remove('on');
+            })
+            const el = document.querySelectorAll(`.depot-point[data-depot="${depot}"]`);
+            el.forEach(ell=>{
+                ell.classList.add('on');
+            })
+        },
+        highlightDepotCategory(category){
+            const els = document.querySelectorAll('.depot-point');
+            els.forEach(el=>{
+                el.classList.remove('on');
+            })
+            const el = document.querySelectorAll(`.depot-point[data-category="${category}"]`);
+            el.forEach(ell=>{
+                ell.classList.add('on');
+            })
+        }
     },
 
     render(createElement, context) {
