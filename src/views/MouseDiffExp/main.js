425151391078cc470a7d09046419b9319b2628d6
Annie Moriondo <104587901+moriondo2022@users.noreply.github.com>
2024-09-09 14:24:00 -0400

    Mouse box-and-whisker plot (#715)
    * here we are getting set up
    
    * get tissue keys
    
    * get gene keys
    
    * before making new component
    
    * making select components
    
    * styling and function for the tissue selector
    
    * tissue and gene menus
    
    * fixed tissue select, now for gene
    
    * removed duplicate calls
    
    * gene selector pulling from index
    
    * automatically queries
    
    * table
    
    * trying to get the plot to load
    
    * plots are showing up
    
    * doesn't show blank plot anymore
    
    * sorted alphabetically
    
    * dot colors by founder and sex
    
    * Box and whisker plots are a go
    
    * narrower boxes
    
    * vertical separator lines
    
    * closer to what they want
    
    * so close and yet so far
    
    * founder scale looks good
    
    * cleanup of axes formats
    
    * there has got to be a better way to add a border
    
    * keeping dots off bottom
    
    * spacing and download chart button
    
    * why won't tooltip hide?
    
    * tooltip formatting
    
    * Y-axis
    
    * gray lines for dividers
    
    * gray line between 2 tiers of axis

---
+++ src/views/MouseDiffExp/main.js
@@ -0,0 +1,112 @@
+import Vue from "vue";
+import Template from "./Template.vue";
+import store from "./store.js";
+import PageHeader from "@/components/PageHeader.vue";
+import PageFooter from "@/components/PageFooter.vue";
+import Documentation from "@/components/Documentation.vue";
+import CriterionFunctionGroup from "@/components/criterion/group/CriterionFunctionGroup.vue";
+import FilterPValue from "@/components/criterion/FilterPValue.vue";
+import FilterEnumeration from "@/components/criterion/FilterEnumeration.vue";
+import FilterGreaterThan from "@/components/criterion/FilterGreaterThan.vue";
+import FilterLessThan from "@/components/criterion/FilterLessThan.vue";
+import SearchHeaderWrapper from "@/components/SearchHeaderWrapper.vue";
+import TissueSelectPicker from "@/components/TissueSelectPicker.vue";
+import Scatterplot from "@/components/Scatterplot.vue";
+import MouseSummaryTable from "@/components/MouseSummaryTable.vue";
+import MouseTissueSelect from "@/components/MouseTissueSelect.vue";
+import MouseGeneSelect from "@/components/MouseGeneSelect.vue";
+import MouseDiffExpTable from "@/components/MouseDiffExpTable.vue";
+import MouseWhiskerPlot from "@/components/MouseWhiskerPlot.vue";
+
+import uiUtils from "@/utils/uiUtils";
+import plotUtils from "@/utils/plotUtils";
+import sortUtils from "@/utils/sortUtils";
+import alertUtils from "@/utils/alertUtils";
+import Formatters from "@/utils/formatters";
+import dataConvert from "@/utils/dataConvert";
+import keyParams from "@/utils/keyParams";
+import regionUtils from "@/utils/regionUtils";
+
+import ResearchSingleSearch from "@/components/researchPortal/ResearchSingleSearch.vue";
+import { pageMixin } from "@/mixins/pageMixin";
+new Vue({
+    store,
+    components: {
+        PageHeader,
+        PageFooter,
+        Documentation,
+        CriterionFunctionGroup,
+        FilterPValue,
+        FilterEnumeration,
+        FilterGreaterThan,
+        FilterLessThan,
+        SearchHeaderWrapper,
+        TissueSelectPicker,
+        ResearchSingleSearch,
+        Scatterplot,
+        MouseSummaryTable,
+        MouseTissueSelect,
+        MouseGeneSelect,
+        MouseDiffExpTable,
+        MouseWhiskerPlot,
+    },
+    mixins: [pageMixin],
+    data() {
+        return {
+            currentPage: 1,
+            perPage: 10,
+        };
+    },
+    computed: {
+        utilsBox() {
+            let utils = {
+                Formatters: Formatters,
+                uiUtils: uiUtils,
+                alertUtils: alertUtils,
+                keyParams: keyParams,
+                dataConvert: dataConvert,
+                sortUtils: sortUtils,
+                plotUtils: plotUtils,
+                regionUtils: regionUtils,
+            };
+            return utils;
+        },
+
+        diseaseGroup() {
+            return this.$store.getters["bioPortal/diseaseGroup"];
+        },
+        diseaseSystem() {
+            return this.$store.getters["bioPortal/diseaseSystem"];
+        },
+        docDetails() {
+            return {
+                tissue: this.tissue
+                    ? this.tissue.toUpperCase().replaceAll("_", " ")
+                    : "",
+            };
+        },
+        diffExpData(){
+            let data = structuredClone(this.$store.state.diffExp.data); 
+            for (let i = 0; i < data.length; i++){
+                data[i].founder_sex = `${data[i].founder}_${data[i].sex}`;
+            }
+            return data;
+        }
+    },
+    created() {
+        // get the disease group and set of phenotypes available
+        this.$store.dispatch("bioPortal/getDiseaseGroups");
+        this.$store.dispatch("bioPortal/getPhenotypes");
+        this.$store.dispatch("bioPortal/getDatasets");
+        this.$store.dispatch("bioPortal/getDiseaseSystems");
+
+        this.$store.dispatch("queryDiffExp");
+    },
+    methods: {
+        tissueFormatter: Formatters.tissueFormatter,
+        searchDiffExp(){
+            this.$store.dispatch("queryDiffExp");
+        }
+    },
+    render: (h) => h(Template),
+}).$mount("#app");
---
+++ src/views/MouseDiffExp/store.js
@@ -0,0 +1,74 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import bioPortal from "@/modules/bioPortal";
+import bioIndex from "@/modules/bioIndex";
+import kp4cd from "@/modules/kp4cd";
+import keyParams from "@/utils/keyParams";
+import { BIO_INDEX_HOST } from "@/utils/bioIndexUtils";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {
+        bioPortal,
+        kp4cd,
+        diffExp: bioIndex("diff-exp"),
+        tissueSummary: bioIndex("diff-exp-summary-tissue"),
+        geneSummary: bioIndex("diff-exp-summary-gene")
+    },
+    state: {
+        tissue: keyParams.tissue || "",
+        gene: keyParams.gene || "",
+        tissueKeys: [],
+        tissueToQuery: "",
+        geneToQuery: "",
+    },
+
+    mutations: {
+        setTissueName(state, tissueName) {
+            state.tissue = tissueName || state.tissue;
+            keyParams.set({ tissue: state.tissue });
+        },
+        setGeneName(state, geneName) {
+            state.gene = geneName || state.gene;
+            keyParams.set({ gene: state.gene });
+        }
+    },
+    actions: {
+        async getTissueKeys(context) {
+			let tissues = await fetch(`${BIO_INDEX_HOST}/api/bio/keys/diff-exp/2?columns=tissue`)
+				.then(resp => resp.json())
+				.then(json => {
+					if (json.count == 0) {
+						return null;
+					}
+					return json.keys.map(key => key[0])
+				});
+            context.state.tissueKeys = tissues;
+		},
+        async selectGeneName(context, geneName){
+            console.log(geneName);
+            context.state.geneToQuery = geneName;
+        },
+        async queryDiffExp(context) {
+            let gene = context.state.geneToQuery || context.state.gene;
+            context.commit("setGeneName", gene);
+
+            let tissue = context.state.tissueToQuery || context.state.tissue;
+            context.commit("setTissueName", tissue);
+
+            if (!!tissue){
+                context.dispatch("tissueSummary/query", {q: tissue});
+            }
+            if (!!gene){
+                console.log("we have gene");
+                context.dispatch("geneSummary/query", {q: gene});
+            }
+            if (!!gene && !!tissue) {
+                context.dispatch("diffExp/query", { q: 
+                    `${gene},${tissue}` });
+            }
+        },
+    },
+});
