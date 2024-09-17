90e96983833cf5671ef47afd9a752b04b75cd997
Annie Moriondo <104587901+moriondo2022@users.noreply.github.com>
2024-09-17 17:18:33 -0400

    Common mouse (#720)
    * common and rare
    
    * added empty ancestry variable
    
    * doc details showing up
    
    * contextually hide and show filter items

--- src/views/MouseDiffExp/Template.vue
+++ src/views/MouseDiffExp/Template.vue
@@ -68,7 +68,7 @@
               </div>
               <div class="card mdkp-card">
                 <div class="card-body">
-                  <h4>HuGE Scores for Human Homolog {{ $store.state.gene }}</h4>
+                  <h4>Human Homolog {{ $store.state.gene }}</h4>
                   <div v-if="$parent.hugeScores.length > 0">
                     <criterion-function-group
                         @update:filter-list="
@@ -89,6 +89,7 @@
                           <div class="label">Phenotypes</div>
                       </filter-enumeration-control>
                       <filter-greater-control
+                          v-if="$parent.activeTab === 'hugeScorePheWASPlot'"
                           :field="'huge'"
                           placeholder="Set HuGE..."
                       >
@@ -96,36 +97,272 @@
                               <strong>HuGE Score (&ge;)</strong>
                           </div>
                       </filter-greater-control>
+                      <filter-pvalue-control
+                            v-if="$parent.activeTab !== 'hugeScorePheWASPlot'"
+                            :field="'pValue'"
+                            placeholder="Set P-Value ..."
+                        >
+                            <div class="label">P-Value (&le;)</div>
+                        </filter-pvalue-control>
                       <template slot="filtered" slot-scope="{ filter }">
-                        <research-phewas-plot  
-                          ref="hugeScorePheWASPlot"
-                          :canvas-id="`huge_scores_${$store.state.gene}`"
-                          :plotName="`huge_scores_${$store.state.gene}`"
-                          :phenotypes-data="$parent.hugeScores"
-                          :phenotype-map="
-                              $store.state.bioPortal.phenotypeMap
-                          "
-                          :colors="$parent.plotColors"
-                          :plot-margin="$parent.phewasPlotMargin"
-                          :render-config="
-                              $parent.hugeScoreRenderConfig
-                          "
-                          :pkg-data="null"
-                          :pkg-data-selected="null"
-                          :utils="$parent.utilsBox"
-                          :options="['open phenotype page']"
-                          :filter="filter"
-                        >
-                        </research-phewas-plot>
-                        <huge-scores-table
-                            :pageKey="$store.state.gene"
-                            leadTableField="phenotype"
-                            :hugeScores="$parent.hugeScores"
-                            :phenotypeMap="$store.state.bioPortal.phenotypeMap"
-                            :filter="filter"
-                        >
-                        </huge-scores-table>
-                      </template>
+                            <span class="filter-pill-collection center">
+                                <b-badge
+                                    v-if="
+                                        !!$store.state.selectedAncestry &&
+                                        $parent.activeTab ===
+                                            'commonVariantPheWASPlot'
+                                    "
+                                    pill
+                                    class="btn btn-secondary search-bubble 1 pseudoFilter"
+                                >
+                                    <strong>
+                                        Ancestry =
+                                        {{
+                                            $parent.ancestryFormatter(
+                                                $store.state.selectedAncestry
+                                            )
+                                        }}
+                                        <a
+                                            @click="
+                                                $parent.clearCriterion(
+                                                    'ancestry'
+                                                )
+                                            "
+                                            >X</a
+                                        >
+                                    </strong>
+                                </b-badge>
+                                <b-badge
+                                    v-if="
+                                        !!$store.state.selectedTranscript &&
+                                        $parent.diseaseGroup &&
+                                        !$parent.noTranscriptDataPortal.includes(
+                                            $parent.diseaseGroup.name
+                                        ) &&
+                                        $parent.activeTab ===
+                                            'rareVariantPheWASPlot'
+                                    "
+                                    pill
+                                    class="btn search-bubble 1 pseudoFilter"
+                                >
+                                    <strong>
+                                        Transcript =
+                                        {{ $store.state.selectedTranscript }}
+                                        <a
+                                            @click="
+                                                $parent.clearCriterion(
+                                                    'transcript'
+                                                )
+                                            "
+                                            >X</a
+                                        >
+                                    </strong>
+                                </b-badge>
+                            </span>
+                            <b-tabs>
+                                <b-tab
+                                    title="HuGE Scores"
+                                    @click="
+                                        $parent.renderPhewas(
+                                            'hugeScorePheWASPlot'
+                                        )
+                                    "
+                                >
+                                    <h4 class="card-title">HuGE Scores</h4>
+                                    <span>
+                                        <documentation
+                                            name="gene.hugecal.subheader"
+                                            :contentFill="$parent.docDetails"
+                                            :contentMap="
+                                                $store.state.bioPortal
+                                                    .documentations
+                                            "
+                                        >
+                                        </documentation>
+                                    </span>
+                                    <research-phewas-plot  
+                                      ref="hugeScorePheWASPlot"
+                                      :canvas-id="`huge_scores_${$store.state.gene}`"
+                                      :plotName="`huge_scores_${$store.state.gene}`"
+                                      :phenotypes-data="$parent.hugeScores"
+                                      :phenotype-map="
+                                          $store.state.bioPortal.phenotypeMap
+                                      "
+                                      :colors="$parent.plotColors"
+                                      :plot-margin="$parent.phewasPlotMargin"
+                                      :renderConfig="
+                                          $parent.hugeScoreRenderConfig
+                                      "
+                                      :pkg-data="null"
+                                      :pkg-data-selected="null"
+                                      :utils="$parent.utilsBox"
+                                      :options="['open phenotype page']"
+                                      :filter="filter"
+                                    >
+                                    </research-phewas-plot>
+                                    <unauthorized-message
+                                        :restricted="
+                                            $store.state.varassociations
+                                                .restricted
+                                        "
+                                    >
+                                    </unauthorized-message>
+                                    <huge-scores-table
+                                        :pageKey="$store.state.gene"
+                                        leadTableField="phenotype"
+                                        :hugeScores="$parent.hugeScores"
+                                        :phenotypeMap="$store.state.bioPortal.phenotypeMap"
+                                        :filter="filter"
+                                    >
+                                    </huge-scores-table>
+                                </b-tab>
+                                <b-tab
+                                    title="Common variant associations"
+                                    @click="
+                                        $parent.renderPhewas(
+                                            'commonVariantPheWASPlot'
+                                        )
+                                    "
+                                >
+                                    <h4 class="card-title">
+                                        Common variant gene-level associations
+                                        for
+                                        {{
+                                            $store.state.gene.toUpperCase()
+                                        }}
+                                        (Ancestry:
+                                        {{
+                                            $store.state.selectedAncestry == ""
+                                                ? "All"
+                                                : $parent.ancestryFormatter(
+                                                      $store.state
+                                                          .selectedAncestry
+                                                  )
+                                        }})
+                                        <tooltip-documentation
+                                            name="gene.associations.tooltip.hover"
+                                            :contentFill="$parent.docDetails"
+                                            :is-hover="true"
+                                            :no-icon="false"
+                                            :contentMap="
+                                                $store.state.bioPortal
+                                                    .documentations
+                                            "
+                                        >
+                                        </tooltip-documentation>
+                                    </h4>
+                                    <research-phewas-plot
+                                        v-if="
+                                            $parent.filteredAssociations
+                                                .length > 0
+                                        "
+                                        ref="commonVariantPheWASPlot"
+                                        canvas-id="commonVariantPlot"
+                                        :plotName="`common_variant_${$store.state.geneName}`"
+                                        :phenotypes-data="
+                                            $parent.filteredAssociations
+                                        "
+                                        :phenotype-map="
+                                            $store.state.bioPortal.phenotypeMap
+                                        "
+                                        :colors="$parent.plotColors"
+                                        :plot-margin="$parent.phewasPlotMargin"
+                                        :renderConfig="
+                                            $parent.commonVariantRenderConfig
+                                        "
+                                        :pkg-data="null"
+                                        :pkg-data-selected="null"
+                                        :filter="filter"
+                                        :utils="$parent.utilsBox"
+                                        :options="['open phenotype page']"
+                                    >
+                                    </research-phewas-plot>
+                                    <unauthorized-message
+                                        :restricted="
+                                            $store.state.varassociations
+                                                .restricted
+                                        "
+                                    >
+                                    </unauthorized-message>
+                                    <gene-associations-table
+                                        v-if="!!$parent.geneassociations.length > 0"
+                                        :gene="$store.state.gene"
+                                        :associations="$parent.geneassociations"
+                                        :phenotype-map="
+                                            $store.state.bioPortal.phenotypeMap
+                                        "
+                                        :filter="filter"
+                                    >
+                                    </gene-associations-table>
+                                </b-tab>
+                                <b-tab
+                                    title="Rare variant associations"
+                                    @click="
+                                        $parent.renderPhewas(
+                                            'rareVariantPheWASPlot'
+                                        )
+                                    "
+                                >
+                                    <h4 class="card-title">
+                                        Rare variant
+                                        {{
+                                            !$store.state.selectedTranscript
+                                                ? `gene-level associations for ${$store.state.gene}`
+                                                : `transcript-level associations for ${$store.state.selectedTranscript}`
+                                        }}
+                                        <tooltip-documentation
+                                            name="gene.52k.tooltip.hover"
+                                            :contentFill="$parent.docDetails"
+                                            :is-hover="true"
+                                            :no-icon="false"
+                                            :contentMap="
+                                                $store.state.bioPortal
+                                                    .documentations
+                                            "
+                                        ></tooltip-documentation>
+                                    </h4>
+                                    <research-phewas-plot
+                                        v-if="
+                                            $parent.transcriptOr52k.length >
+                                                0 &&
+                                            !$store.state.selectedTranscript
+                                        "
+                                        ref="rareVariantPheWASPlot"
+                                        canvas-id="rareVariantPlot"
+                                        :plotName="`rare_variant_${$store.state.gene}`"
+                                        :phenotypes-data="
+                                            $parent.transcriptOr52k
+                                        "
+                                        :phenotype-map="
+                                            $store.state.bioPortal.phenotypeMap
+                                        "
+                                        :colors="$parent.plotColors"
+                                        :plot-margin="$parent.phewasPlotMargin"
+                                        :render-config="
+                                            $parent.rareVariantRenderConfig
+                                        "
+                                        :pkg-data="null"
+                                        :pkg-data-selected="null"
+                                        :filter="filter"
+                                        :utils="$parent.utilsBox"
+                                        :options="['open phenotype page']"
+                                    >
+                                    </research-phewas-plot>
+                                    <unauthorized-message
+                                        :restricted="$store.state.restricted"
+                                    >
+                                    </unauthorized-message>
+                                    <gene-associations-masks
+                                        :associations="$parent.transcriptOr52k"
+                                        :phenotype-map="
+                                            $store.state.bioPortal.phenotypeMap
+                                        "
+                                        :filter="filter"
+                                    >
+                                    </gene-associations-masks>
+                                </b-tab>
+                            </b-tabs>
+                        </template>
                       
                     </criterion-function-group>
                     
