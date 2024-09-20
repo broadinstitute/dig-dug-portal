633a61859a2f69d1b43373cbdbb6bde1f3f8e07b
a-shilin <147729544+a-shilin@users.noreply.github.com>
2024-09-20 12:55:01 -0400

    Matkp UI, datasets metadata updates integration (#722)
    * start cellbrower template
    
    * cell browser (long form)
    
    * split gene expression so now you can search additional genes
    
    * css fixed to unbreak compiled template
    
    * legends + css tweaks
    
    * typo fix
    
    * gene removal feature
    
    * select gene for use in comparison tables
    
    * cell count table features, cody tidying
    
    * lock cells feature, code tidy
    
    * fixing labels
    
    * colors, lagends tweaks
    
    * updating diff expression table to select reference condition
    
    * restructuring to multi-page, starting landing page, startying interactive hero section
    
    * hero interactive element
    
    * minor tweaks
    
    * updates with dataset api's
    
    * starting datasets page
    
    * datasets page, and other updates
    
    * scoping styles
    
    * re-fixing matkp gloabl styles
    
    * more global style-fixes
    
    * fixing umap point cloud positioning
    
    * datasets page updates, cell browser speed improvement, beginning updated cell browser.
    
    * cellbrowser2 cell distribution tables and viz
    
    * removing hardcoded categories
    
    * added legend to cell count data, added table drawer
    
    * updates
    
    * cellbrowser2, plots
    
    * tables for single categories, minor tweaks
    
    * 3rd iteration
    
    * study characteristics, data sorting fixed
    
    * starting landing page depot viz
    
    * updated anatamogram with depots, linked to datasets page
    
    * 'fill-available' to 'stretch' style fix'
    git push
    
    git
    
    * adding config and updating to match datasets metadata tweaks

--- src/views/MATKP/Datasets/main.js
+++ src/views/MATKP/Datasets/main.js
@@ -12,9 +12,6 @@ Vue.config.productionTip = false;
 
 import matkpNav from "../components/matkp-nav.vue"
 import matkpFooter from "../components/matkp-footer.vue"
-import * as d3 from "d3";
-import Formatters from "@/utils/formatters";
-import uiUtils from "@/utils/uiUtils";
 
 //import { BIO_INDEX_HOST } from "@/utils/bioIndexUtils"; 
 const BIO_INDEX_HOST = 'https://bioindex-dev.hugeamp.org';
@@ -27,27 +24,15 @@ new Vue({
 
     data() {
         return{
-            fields: [
-                {key:"datasetName", label:"Name", tdClass: 'italic', sortable: true}, 
-                {key:"species", label:"Species", sortable: true}, 
-                {key:"depot", label:"Depot", sortable: false},
-                {key:"tissue", label:"Tissue", sortable: false},
-                {key:"sampleType", label:"Sample", sortable: false},
-                {key:"sex", label:"Sex", sortable: false},
-                {key:"ethnicity", label:"Ethnicity", sortable: false},
-                {key:"method", label:"Method", sortable: false}, 
-                {key:"platform", label:"Platform", sortable: false}, 
-                {key:"totalCells", label:"Cells", sortable: true}, 
-                //{key:"Download", label:"", sortable: false},
-                {key:"datasetId", label:"", sortable: false},
-            ],
+            config: null,
             datasets: null,
             filter: null,
             selectedFilters: {},
             searchedItems: [],
-            perPage: 5,
+            perPage: 10,
             currentPage: 1,
             pageOptions: [{ value: 5, text: "5" }, { value: 10, text: "10" }, { value: 15, text: "20" }, { value: 100, text: "All" }],
+            first: 2,
         }
     },
 
@@ -56,6 +41,7 @@ new Vue({
     },
 
     async created() {
+        await this.getConfig();
         this.getDatasets();
     },
 
@@ -67,39 +53,81 @@ new Vue({
                   this.$set(this.selectedFilters, key, []);
                 }
               });
+              this.updatePageFromQueryString();
             },
-            immediate: true
+        },
+        selectedFilters: {
+            handler(newVal) {
+                this.updateQueryStringFromPage(this.selectedFilters);
+            },
+            deep: true,
         }
     },
 
     computed: {
+        allFields(){
+            return this.config.columnLabels;
+        },
         filterOptions() {
             const filterOptions = {};
 
-            if (!this.datasets) return filterOptions;
-        
-            // Get field keys for easy checking
-            const fieldKeys = this.fields.map(field => field.key);
+            if (!this.datasets || !this.config) return filterOptions;
+
+            const fieldKeys = this.config.datasetsFilters;
         
             this.datasets.forEach(item => {
-                Object.keys(item).forEach(key => {
-                if (Array.isArray(item[key]) && fieldKeys.includes(key)) {
+                fieldKeys.forEach(key => {
+                //console.log('  ',key);
                     if (!filterOptions[key]) {
                         filterOptions[key] = new Set();
                     }
-                    item[key].forEach(value => {
+                    const itemKeyArr = typeof item[key] === 'string' ? [item[key]] : [...item[key]];
+                    itemKeyArr.forEach(value => {
                         filterOptions[key].add(value);
+                        //console.log('      ',value);
                     });
-                }
                 });
             });
-        
+            
             // Convert Sets to arrays and create options for select
             Object.keys(filterOptions).forEach(key => {
-                filterOptions[key] = Array.from(filterOptions[key]).map(value => ({ value, text: value }));
+                filterOptions[key] = Array.from(filterOptions[key]);
             });
-        
+
+            console.log('filterOptions', filterOptions);
+            
             return filterOptions;
+        },
+        mainFields(){
+            if (!this.datasets || !this.config) return null;
+            let fields = [];
+            this.config.datasetsColumns.forEach(column=>{
+                fields.push({
+                    key: column,
+                    label: this.config.columnLabels[column],
+                    sortable: true
+                })
+            });
+            fields = [...fields, ...[
+                {key:"show_details", label:"", sortable: false},
+                {key:"download", label:"", sortable: false},
+                {key:"datasetId", label:"", sortable: false},
+            ]]
+            console.log('mainFields', fields);
+            return fields;
+        },
+        subFields(){
+            if (!this.datasets || !this.config) return null;
+            const fields = [];
+            this.config.datasetsExtraColumns.forEach(column=>{
+                fields.push({
+                    key: column,
+                    label: this.config.columnLabels[column],
+                    sortable: true
+                })
+            });
+            console.log('subFields', fields);
+            return fields;
         },
         filteredItems() {
             if (!this.datasets) return [];
@@ -122,22 +150,65 @@ new Vue({
             if(this.filteredItems.length < this.datasets.length) return this.filteredItems.length;
             return this.datasets.length;
         }
-    },
+    }, 
 
     
 
     methods: {
+        async getConfig(){
+            const dataPoint = "https://hugeampkpncms.org/rest/data?pageid=matkp_config";
+            const result = await fetch(dataPoint).then((resp) => resp.json());
+            const json = JSON.parse(result[0]['field_data_points']);
+            this.config = json;
+            console.log('config', json);
+        },
         async getDatasets(){
             const fetchPath = '/api/raw/file/single_cell_metadata/dataset_metadata.json.gz';
             const response = await fetch(`${BIO_INDEX_HOST}${fetchPath}`);
             const dataText = await response.text();
-            console.log(dataText);
             const lines = dataText.split('\n').filter(line => line.trim() !== '');
             const jsonObjects = lines.map(line => JSON.parse(line));
+            jsonObjects.forEach(object => {
+                object._showDetails = false
+            })
             this.datasets = jsonObjects;
-           
+            console.log('datasets', this.datasets);
+        },
+        updateQueryStringFromPage(selectedFilters){
+            console.log('updateFromPage')
+            const queryParams = [];
+            for(const [key, value] of Object.entries(selectedFilters)){
+                if(value.length>0) {
+                    const encodedValues = value.map(v => encodeURIComponent(v.toLowerCase())).join(',');
+                    queryParams.push(`${encodeURIComponent(key)}=${encodedValues}`);
+                }
+            }
+            const queryString = queryParams.length ? `?${queryParams.join('&')}` : '';
+            const newUrl = `${window.location.origin}${window.location.pathname}${queryString}`;
+            window.history.pushState({ path: newUrl }, '', newUrl);
+        },
+        updatePageFromQueryString(){
+            console.log('updateFromURL')
+            const urlSearchParams = new URLSearchParams(window.location.search);
+            const params = Object.fromEntries(urlSearchParams.entries());
+            console.log('params', params)
+            for(const [key, value] of Object.entries(params)){
+                const values = value.split(",");
+                console.log(values);
+                for(const v of values){
+                    const options = this.filterOptions[key];
+                    console.log(key, 'options', options)
+                    const indices = options
+                        .map((str, index) => (str.toLowerCase().includes(v) ? index : -1))
+                        .filter(index => index !== -1);
+                    for(const i of indices){
+                        this.selectedFilters[key].push(this.filterOptions[key][i]);
+                    }
+                }
+            }
         },
         onFiltered(filteredItems) {
+            //unused
             this.searchedItems = filteredItems;
             this.filteredCount = filteredItems.length;
         },
@@ -174,6 +245,7 @@ new Vue({
         highlightTableItems(e){
             const key = e.target.dataset.inputKey;
             const option = e.target.dataset.inputOption;
+            console.log(option, key, this.selectedFilters);
             const index = this.selectedFilters[key].indexOf(option);
             if (index !== -1) {
                 document.querySelector(`.input-list > *[data-input-key="${key}"][data-input-option="${option}"]`).classList.add('highlight')
