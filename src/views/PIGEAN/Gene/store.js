bb0ec269694092bc8aae867a0c06e9b1f6bf15e7
Annie Moriondo <amoriond@broadinstitute.org>
2024-11-18 15:28:58 -0500

    getting ready to change completely over to default sigma

--- src/views/PIGEAN/Gene/store.js
+++ src/views/PIGEAN/Gene/store.js
@@ -17,22 +17,17 @@ export default new Vuex.Store({
     },
     state: {
         geneName: keyParams.gene,
-        sigma: keyParams.sigma || bioIndexUtils.DEFAULT_SIGMA,
         genesetSize: keyParams.genesetSize || bioIndexUtils.DEFAULT_GENESET_SIZE,
         geneToQuery: "",
-        sigmaToQuery: null,
         genesetSizeToQuery: null,
         aliasName: null,
+        sigmaInt: 2
     },
 
     mutations: {
         setGeneName(state, geneName) {
             state.geneName = geneName || state.geneName;
             keyParams.set({ gene: state.geneName });
-        },
-        setSigma(state, sigma){
-            state.sigma = sigma || state.sigma;
-            keyParams.set({ sigma: state.sigma });
         },
         setGenesetSize(state, genesetSize){
             state.genesetSize = genesetSize || state.genesetSize;
@@ -73,17 +68,13 @@ export default new Vuex.Store({
     actions: {
         async queryGeneName(context, symbol) {
             let name = context.state.geneToQuery || context.state.geneName;
-            let sigma = context.state.sigmaToQuery || context.state.sigma;
             let genesetSize = context.state.genesetSizeToQuery || context.state.genesetSize;
             context.commit("setGeneName", name);
-            context.commit("setSigma", sigma);
             context.commit("setGenesetSize", genesetSize);
-
-            let sigmaInt = parseInt(sigma.slice(-1));
             if (!!name) {
                 context.dispatch("gene/query", { q: name });
                 context.dispatch("pigeanGene/query", { q: 
-                    `${name},${sigmaInt},${genesetSize}` });
+                    `${name},${context.state.sigmaInt},${genesetSize}` });
             }
         },
     },
--- src/views/PIGEAN/GeneSet/store.js
+++ src/views/PIGEAN/GeneSet/store.js
@@ -17,22 +17,17 @@ export default new Vuex.Store({
     },
     state: {
         geneset: keyParams.geneset,
-        sigma: keyParams.sigma || bioIndexUtils.DEFAULT_SIGMA,
         genesetSize: keyParams.genesetSize || bioIndexUtils.DEFAULT_GENESET_SIZE,
         genesetToQuery: "",
-        sigmaToQuery: null,
         genesetSizeToQuery: null,
         aliasName: null,
+        sigmaInt: 2
     },
 
     mutations: {
         setGeneset(state, geneset) {
             state.geneset = geneset || state.geneset;
             keyParams.set({ geneset: state.geneset });
-        },
-        setSigma(state, sigma){
-            state.sigma = sigma || state.sigma
-            keyParams.set({ sigma: state.sigma });
         },
         setGenesetSize(state, genesetSize){
             state.genesetSize = genesetSize || state.genesetSize;
@@ -46,16 +41,13 @@ export default new Vuex.Store({
     actions: {
         async queryGeneset(context, symbol) {
             let name = context.state.genesetToQuery || context.state.geneset;
-            let sigma = context.state.sigmaToQuery || context.state.sigma;
             let genesetSize = context.state.genesetSizeToQuery || context.state.genesetSize;
             context.commit("setGeneset", name);
-            context.commit("setSigma", sigma);
             context.commit("setGenesetSize", genesetSize);
 
-            let sigmaInt = parseInt(sigma.slice(-1));
             if (!!name) {
                 context.dispatch("pigeanGeneset/query", { q: 
-                    `${name},${sigmaInt},${genesetSize}` });
+                    `${name},${context.state.sigmaInt},${genesetSize}` });
             }
         },
     },
