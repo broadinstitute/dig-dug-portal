b9b8c24ed59c661bb26d4848c4a207892413bad0
Quy <quy@broad.dev>
2024-12-13 22:16:03 -0500

    put into tabs

--- src/portals/PanKbase/views/DataBrowser/Template.vue
+++ src/portals/PanKbase/views/DataBrowser/Template.vue
@@ -5,66 +5,82 @@
         <!-- BODY -->
 
         <div class="pkb-body">
-            <b-table
-                :items="$parent.donors"
-                :fields="$parent.donorsFields"
-                striped
-                hover
-                responsive="sm"
-                head-variant="light"
-            ></b-table>
-            <b-pagination
-                v-model="$parent.currentPageDonors"
-                class="pagination justify-content-center"
-                :total-rows="$parent.donorsTotal"
-                :per-page="$parent.perPageDonors"
-            ></b-pagination>
-
-            <b-table
-                :items="$parent.assays"
-                :fields="$parent.assaysFields"
-                striped
-                hover
-                responsive="sm"
-                head-variant="light"
-            ></b-table>
-            <b-pagination
-                v-model="$parent.currentPageAssays"
-                class="pagination justify-content-center"
-                :total-rows="$parent.assaysTotal"
-                :per-page="$parent.perPageAssays"
-            ></b-pagination>
-
-            <b-table
-                :items="$parent.analyses"
-                :fields="$parent.analysesFields"
-                striped
-                hover
-                responsive="sm"
-                head-variant="light"
-            ></b-table>
-            <b-pagination
-                v-model="$parent.currentPageAnalyses"
-                class="pagination justify-content-center"
-                :total-rows="$parent.analysesTotal"
-                :per-page="$parent.perPageAnalyses"
-            ></b-pagination>
-
-            <b-table
-                :items="$parent.processedResults"
-                :fields="$parent.processedResultsFields"
-                striped
-                hover
-                responsive="sm"
-                head-variant="light"
-            ></b-table>
-            <b-pagination
-                v-model="$parent.currentPageProcessedResults"
-                class="pagination justify-content-center"
-                :total-rows="$parent.processedResultsTotal"
-                :per-page="$parent.perPageProcessedResults"
-            ></b-pagination>
-
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Donors" active>
+                        <b-card-text>
+                            <b-table
+                                :items="$parent.donors"
+                                :fields="$parent.donorsFields"
+                                striped
+                                hover
+                                responsive="sm"
+                                head-variant="light"
+                            ></b-table>
+                            <b-pagination
+                                v-model="$parent.currentPageDonors"
+                                class="pagination justify-content-center"
+                                :total-rows="$parent.donorsTotal"
+                                :per-page="$parent.perPageDonors"
+                            ></b-pagination
+                        ></b-card-text>
+                    </b-tab>
+                    <b-tab title="Assays">
+                        <b-card-text
+                            ><b-table
+                                :items="$parent.assays"
+                                :fields="$parent.assaysFields"
+                                striped
+                                hover
+                                responsive="sm"
+                                head-variant="light"
+                            ></b-table>
+                            <b-pagination
+                                v-model="$parent.currentPageAssays"
+                                class="pagination justify-content-center"
+                                :total-rows="$parent.assaysTotal"
+                                :per-page="$parent.perPageAssays"
+                            ></b-pagination
+                        ></b-card-text>
+                    </b-tab>
+                    <b-tab title="Analyses">
+                        <b-card-text
+                            ><b-table
+                                :items="$parent.analyses"
+                                :fields="$parent.analysesFields"
+                                striped
+                                hover
+                                responsive="sm"
+                                head-variant="light"
+                            ></b-table>
+                            <b-pagination
+                                v-model="$parent.currentPageAnalyses"
+                                class="pagination justify-content-center"
+                                :total-rows="$parent.analysesTotal"
+                                :per-page="$parent.perPageAnalyses"
+                            ></b-pagination
+                        ></b-card-text>
+                    </b-tab>
+                    <b-tab title="Processed Results">
+                        <b-card-text
+                            ><b-table
+                                :items="$parent.processedResults"
+                                :fields="$parent.processedResultsFields"
+                                striped
+                                hover
+                                responsive="sm"
+                                head-variant="light"
+                            ></b-table>
+                            <b-pagination
+                                v-model="$parent.currentPageProcessedResults"
+                                class="pagination justify-content-center"
+                                :total-rows="$parent.processedResultsTotal"
+                                :per-page="$parent.perPageProcessedResults"
+                            ></b-pagination
+                        ></b-card-text>
+                    </b-tab>
+                </b-tabs>
+            </b-card>
         </div>
         <!-- FOOTER -->
         <pkb-footer></pkb-footer>
