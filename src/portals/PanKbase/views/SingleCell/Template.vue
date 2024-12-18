20cb91aa26e7cdfb6442916341b457327f42fa1f
Annie Moriondo <amoriond@broadinstitute.org>
2024-12-18 12:31:22 -0500

    Single Cell descriptive content

--- src/portals/PanKbase/views/SingleCell/Template.vue
+++ src/portals/PanKbase/views/SingleCell/Template.vue
@@ -4,7 +4,7 @@
       <pkb-header></pkb-header>
       <!-- BODY -->
       <div class="pkb-body">
-          SingleCell page content goes here
+        <div v-html="$parent.singleCellInfo"></div>
       </div>
       <!-- FOOTER -->
       <pkb-footer></pkb-footer> 
