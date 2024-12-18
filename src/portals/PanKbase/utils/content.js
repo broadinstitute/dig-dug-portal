af5cd1b75d0d71a3c152bdf0721cc9a23c3d9ae4
Annie Moriondo <amoriond@broadinstitute.org>
2024-12-18 15:50:44 -0500

    getting ready to pull

--- src/portals/PanKbase/utils/content.js
+++ src/portals/PanKbase/utils/content.js
@@ -3,7 +3,7 @@ import dataConvert from "@/utils/dataConvert";
 
 const CONTENT_URL = "https://hugeampkpncms.org/rest/byor_content?id="
 
-export async function getPankbaseContent(contentId, getBody=false){
+export async function getPankbaseContent(contentId, getBody=false, getAll=false){
   let resourceUrl = `${CONTENT_URL}${contentId}`;
   let jsonContent = await fetch(resourceUrl).then(
     resp => resp.json());
@@ -12,6 +12,9 @@ export async function getPankbaseContent(contentId, getBody=false){
   }
   if (getBody){
     return jsonContent[0].body;
+  }
+  if (getAll){
+    return jsonContent[0];
   }
   let csvContent = jsonContent[0].field_data_points;
   return dataConvert.csv2Json(csvContent);
