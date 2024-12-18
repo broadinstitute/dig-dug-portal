a6ed5f27dfd1319beda2404ff04bea2c33980c4b
Alex Shilin <shilin@broadinstitute.org>
2024-12-18 12:10:47 -0500

    initial cell browser setup

--- src/portals/PanKbase/views/SingleCell/Template.vue
+++ src/portals/PanKbase/views/SingleCell/Template.vue
@@ -4,7 +4,13 @@
       <pkb-header></pkb-header>
       <!-- BODY -->
       <div class="pkb-body">
-          SingleCell page content goes here
+          <research-single-cell-browser 
+            v-if="true"
+            sectionId="scb"
+            :renderConfig="$parent.scbConfig"
+            :utils="$parent.utils"
+            :data="$parent.data"
+          ></research-single-cell-browser>
       </div>
       <!-- FOOTER -->
       <pkb-footer></pkb-footer> 
--- src/portals/PanKbase/views/SingleCell/main.js
+++ src/portals/PanKbase/views/SingleCell/main.js
@@ -4,13 +4,73 @@ import "../../assets/layout.css";
 import "../../assets/pkb-styles.css";
 import { pankbaseMixin } from "@/portals/PanKbase/mixins/pankbaseMixin.js";
 import { getResource } from "@/portals/PanKbase/utils/api";
+import dataConvert from "@/utils/dataConvert";
+import ResearchSingleCellBrowser from "@/components/researchPortal/ResearchSingleCellBrowser.vue"
 
 new Vue({
     components: {
+        ResearchSingleCellBrowser
     },
     mixins: [pankbaseMixin],
     data() {
-        return {};
+        return {
+            //update single cell component so it doesnt require data prop
+            //it should just load the 'metadata' data point from its config
+            data: [
+                {
+                    "Name": "An Integrated Map of Cell Type–Specific Gene Expression in Pancreatic Islets",
+                    "datasetId": "HPAP",
+                    "Species": "Human",
+                    "Tissue": "islets of langerhans",
+                    "Total Cells": 192203
+                }
+            ],
+            utils: {
+                dataConvert: dataConvert
+            },
+            scbConfig: {
+                "type": "cell browser",
+                "label": "Single Cell Browser",
+                "parameters":{
+                  "datasetId": "PKBdatasetId",
+                  "gene": "PKBgene"
+                },
+                "data points":[ 
+                  {
+                    "role": "metadata",
+                    "url": "https://skin.hugeampkpnbi.org/api/raw/file/single_cell_metadata/dataset_metadata.json.gz"
+                  },{
+                    "role": "fields",
+                    "url": "https://skin.hugeampkpnbi.org/api/raw/file/single_cell/$datasetId/fields.json.gz"
+                  },{
+                    "role": "coordinates",
+                    "url": "https://skin.hugeampkpnbi.org/api/raw/file/single_cell/$datasetId/coordinates.tsv.gz"
+                  },{
+                    "role": "expression",
+                    "url": "https://skin.hugeampkpnbi.org/api/bio/query/single-cell-lognorm?q=$datasetId,$gene"
+                  }
+                ],
+                "components": {
+                  "cell info": {
+                    "enabled": true
+                  },
+                  "cell proportion": {
+                    "enabled": true
+                  },
+                  "gene expression": {
+                    "enabled": true
+                  },
+                  "marker genes": {
+                    "enabled": true
+                  }
+                },
+                "presets": {
+                  "datasetId": "HPAP",
+                  "cell type label": "Cell Type",
+                  "genes": ["INS", "GCG", "SST", "PPY", "PRSS1", "CFTR", "PLVAP", "PDGFRB", "PTPRC"]
+                }
+            }
+        };
     },
     computed: {},
     watch: {},
