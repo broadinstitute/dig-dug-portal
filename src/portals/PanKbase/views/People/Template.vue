2453b8c198d686056f5bd640bfc7109f437d3423
Annie Moriondo <amoriond@broadinstitute.org>
2025-01-03 12:12:30 -0500

    few static pages done

--- src/portals/PanKbase/views/People/Template.vue
+++ src/portals/PanKbase/views/People/Template.vue
@@ -3,14 +3,18 @@
       <!-- NAV -->
       <pkb-header></pkb-header>
       <!-- BODY -->
-      <div class="pkb-body row">
-          <div v-for="person in $parent.peopleList" class="person col-md-6">
+      <div class="pkb-body">
+        <h3 class="page-title">People</h3>
+        <div class="row">
+            <div v-for="person in $parent.peopleList" class="person col-md-6">
             <img :src="person['Photo']" height="300px"/>
             <h4>{{ person["Name"] }}</h4>
             <h5>{{ person["Affiliation"] }}</h5>
             <p class="bio">{{ person["Role"] }}</p>
 
           </div>
+        </div>
+        
       </div>
       <!-- FOOTER -->
       <pkb-footer></pkb-footer> 
--- src/portals/PanKbase/views/Projects/Template.vue
+++ src/portals/PanKbase/views/Projects/Template.vue
@@ -4,6 +4,7 @@
       <pkb-header></pkb-header>
       <!-- BODY -->
       <div class="pkb-body">
+        <h3 class="page-title">About This Project</h3>
         <div v-html="$parent.projectInfo"></div>
       </div>
       <!-- FOOTER -->
