0cc05782799c395b8df2c7fc6658343efa00e856
Annie Moriondo <amoriond@broadinstitute.org>
2025-01-08 17:52:00 -0500

    iframe

---
+++ src/portals/PanKbase/views/DonorMetadata/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Template from "./Template.vue";
+import "../../assets/layout.css";
+import "../../assets/pkb-styles.css";
+import { pankbaseMixin } from "@/portals/PanKbase/mixins/pankbaseMixin.js";
+import { getPankbaseContent } from "@/portals/PanKbase/utils/content";
+
+new Vue({
+    components: {
+    },
+    mixins: [pankbaseMixin],
+    data() {
+        return {};
+    },
+    computed: {},
+    watch: {},
+    async created() {
+    },
+    render(createElement, context) {
+        return createElement(Template);
+    },
+}).$mount("#app");
