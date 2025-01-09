f9cbb727dc067888a261040a3319b2ce96b8ea76
Quy <quy@broad.dev>
2025-01-09 11:23:23 -0500

    update links

--- src/portals/PanKbase/assets/pkbMenu.js
+++ src/portals/PanKbase/assets/pkbMenu.js
@@ -1,41 +1,57 @@
 export const pkbMenu = {
     highlightItems: [
-        { label: 'PanKgraph',               path: 'https://dev.pankgraph.org/' },
-        { label: 'Integrated Cell Browser', path: '/single-cell.html' }
+        { label: "PanKgraph", path: "https://pankgraph.org/" },
+        { label: "Integrated Cell Browser", path: "/single-cell.html" },
     ],
     menuItems: [
-        { 
-            label: 'Data', path: '', 
+        {
+            label: "Data",
+            path: "",
             subMenuItems: [
-                { label: 'Donor Summary', path: 'http://tools.cmdga.org:3838/metadata_analysis_assays/' },
-                { label: 'Data Browser', path: 'https://data.pankbase.org' },
-                { label: 'APIs',         path: '/apis.html' }
-            ] 
-        },{ 
-            label: 'Resources', path: '', 
+                {
+                    label: "Donor Summary",
+                    path: "https://tools.cmdga.org/metadata_analysis_assays/",
+                },
+                { label: "Data Browser", path: "https://data.pankbase.org" },
+                { label: "APIs", path: "/apis.html" },
+            ],
+        },
+        {
+            label: "Resources",
+            path: "",
             subMenuItems: [
-                { label: 'Integrated Cell Browser', path: '/single-cell.html' },
-                { label: 'Analytical Library',      path: '/analytical-library.html' },
-                { label: 'Publications',            path: '/publications.html' }
-            ] 
-        },{ 
-            label: 'About', path: '', 
+                { label: "Integrated Cell Browser", path: "/single-cell.html" },
+                {
+                    label: "Analytical Library",
+                    path: "/analytical-library.html",
+                },
+                { label: "Publications", path: "/publications.html" },
+            ],
+        },
+        {
+            label: "About",
+            path: "",
             subMenuItems: [
-                { label: 'Project',     path: '/projects.html' },
-                { label: 'People',      path: '/people.html' },
-                { label: 'Policies',    path: '/policies.html' },
-                { label: 'Programs',    path: '/programs.html' },
-                { label: 'Collaborate', path: '/collaborate.html' }
-            ]
-        },{ 
-            label: 'Help', path: '', 
+                { label: "Project", path: "/projects.html" },
+                { label: "People", path: "/people.html" },
+                { label: "Policies", path: "/policies.html" },
+                { label: "Programs", path: "/programs.html" },
+                { label: "Collaborate", path: "/collaborate.html" },
+            ],
+        },
+        {
+            label: "Help",
+            path: "",
             subMenuItems: [
-                { label: 'Contact',                   path: '/contact.html' },
-                { label: 'Metadata | Data Standards', path: '/metadata-data-standards.html' },
-                { label: 'Tools | Pipelines',         path: '/tools-pipelines.html' },
-                { label: 'Tutorials',                 path: '/tutorials.html' },
-                { label: 'News',                      path: '/news.html' }
-            ]
-        }
+                { label: "Contact", path: "/contact.html" },
+                {
+                    label: "Metadata | Data Standards",
+                    path: "/metadata-data-standards.html",
+                },
+                { label: "Tools | Pipelines", path: "/tools-pipelines.html" },
+                { label: "Tutorials", path: "/tutorials.html" },
+                { label: "News", path: "/news.html" },
+            ],
+        },
     ],
-} 
+};
