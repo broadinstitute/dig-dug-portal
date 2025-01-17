ce7c9bb347c7a2be8459a8f91b005b4909b56ee4
Annie Moriondo <amoriond@broadinstitute.org>
2025-01-17 16:10:20 -0500

    homepage tissue search

--- src/portals/MousePortal/views/Index/store.js
+++ src/portals/MousePortal/views/Index/store.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import bioPortal from "@/modules/bioPortal";
-import bioIndex from "@/modules/bioIndex";
-import kp4cd from "@/modules/kp4cd";
-import keyParams from "@/utils/keyParams";
-import bioIndexUtils from "@/utils/bioIndexUtils";
+import bioPortal from "../../../../modules/bioPortal";
+import bioIndex from "../../../../modules/bioIndex";
+import kp4cd from "../../../../modules/kp4cd";
+import keyParams from "../../../../utils/keyParams";
+import bioIndexUtils from "../../../../utils/bioIndexUtils";
+import { BIO_INDEX_HOST } from "../../../../utils/bioIndexUtils";
 
 Vue.use(Vuex);
 
@@ -14,6 +15,7 @@ export default new Vuex.Store({
         kp4cd,
     },
     state: {
+        tissueKeys: []
     },
 
     mutations: {},
@@ -22,5 +24,16 @@ export default new Vuex.Store({
     },
 
     actions: {
+        async getTissueKeys(context) {
+            let tissues = await fetch(`${BIO_INDEX_HOST}/api/bio/keys/diff-exp/2?columns=tissue`)
+                .then(resp => resp.json())
+                .then(json => {
+                    if (json.count == 0) {
+                        return null;
+                    }
+                    return json.keys.map(key => key[0])
+                });
+            context.state.tissueKeys = tissues;
+        },
     },
 });
