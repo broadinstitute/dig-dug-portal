c1b18ad4686439cc7e1930ef91551c270e9bedaa
Annie Moriondo <amoriond@broadinstitute.org>
2025-02-20 17:29:51 -0500

    working on making the shape of the new page

---
+++ src/portals/MATKP/views/BulkBrowser/TemplateOld.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="matkp">
+        <div class="f-col fill-height">
+            <!-- NAV -->
+            <matkp-nav></matkp-nav>
+            <!-- BODY -->
+            <div class="mat-body">
+                <div class="row top-selectors">
+                    <div class="col-md-3"></div>
+                    <div class="f-col col-md-3">
+                        <div class="label">Select a dataset</div>
+                            <select v-model="$store.state.selectedDataset">
+                                <option value="">Select a dataset</option>
+                                <option v-for="dataset in $parent.datasets"
+                                    :value="dataset">
+                                    {{ dataset }}
+                                </option>
+                            </select>
+                        
+                    </div>
+                    <div class="f-col col-md-3">
+                        <div class="label">Select a comparison</div>
+                        <select v-model="$store.state.selectedComparison">
+                            <option value="">Select a comparison</option>
+                            <option v-for="comparison in $parent.comparisons"
+                                :value="comparison">
+                                {{ comparison }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+                <div class="f-col">
+                    
+                
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div v-if="$parent.zNormData.length > 0">
+                            <bulk-heatmap
+                                :zNormData="$parent.zNormData"
+                                :samplesColumns="$parent.samplesColumns"
+                                :margin="$parent.margin"
+                                :plotHeight="$parent.plotHeight"
+                            >
+                            </bulk-heatmap>
+                        </div>
+                        <div v-else-if="!$store.state.selectedDataset">
+                            Select a dataset and a comparison to view the heatmap.
+                        </div>
+                        <div v-else>Loading...</div>
+                    </div>
+                    <div class="col-md-6">
+                        <div v-if="$parent.bulkData19K.length> 0">
+                            <bulk-volcano-plot
+                                :renderData="$parent.bulkData19K"
+                                :renderConfig="$parent.volcanoConfig"
+                                :margin="$parent.margin"
+                                sectionId="_bulk">
+
+                            </bulk-volcano-plot>
+                        </div>
+                        <div v-else-if="!$store.state.selectedDataset">
+                            Select a dataset to view the volcano plot.
+                        </div>
+                        <div v-else>Loading...</div>
+                    </div>
+                </div>
+                <div>
+                    <bulk-table
+                        :bulkData="$parent.bulkData19K"
+                        :dataset="$store.state.selectedDataset"
+                        :config="$parent.tableConfig"
+                        :scatterConfig="$parent.scatterplotConfig">
+
+                    </bulk-table>
+                </div>
+            </div>
+            <!-- FOOTER -->
+            <matkp-footer></matkp-footer>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .top-selectors{
+        margin-bottom: 50px;
+    }
+</style>
--- src/portals/MATKP/views/BulkBrowser/main.js
+++ src/portals/MATKP/views/BulkBrowser/main.js
@@ -12,6 +12,8 @@ import BulkTable from "../../components/BulkTable.vue";
 import BulkViolinPlot from "../../components/BulkViolinPlot.vue";
 import Formatters from "@/utils/formatters";
 import uiUtils from "@/utils/uiUtils";
+import ResearchSingleCellBrowser from "@/components/researchPortal/singleCellBrowser/ResearchSingleCellBrowser.vue"
+import BulkBrowserComp from "../../components/BulkBrowserComp.vue";
 import * as d3 from 'd3';
 
 //import { BIO_INDEX_HOST } from "@/utils/bioIndexUtils";
@@ -25,6 +27,7 @@ new Vue({
         BulkVolcanoPlot,
         BulkTable,
         BulkViolinPlot,
+        ResearchSingleCellBrowser,
         uiUtils
     },
     mixins: [matkpMixin],
@@ -76,6 +79,52 @@ new Vue({
                     { key: "beta", label: "Effect (joint)", sortable: true },
                 ],
             },
+            scbConfig: {
+                "type": "cell browser",
+                "label": "Single Cell Browser",
+                "parameters":{
+                    "datasetId": "dataset",
+                    "gene": "gene"
+                },
+                "data points":[ 
+                    {
+                        "role": "metadata",
+                        "url": "https://bioindex-dev.hugeamp.org/api/raw/file/single_cell_metadata/dataset_metadata.json.gz"
+                    },{
+                        "role": "fields",
+                        "url": "https://bioindex-dev.hugeamp.org/api/raw/file/single_cell/$datasetId/fields.json.gz"
+                    },{
+                        "role": "coordinates",
+                        "url": "https://bioindex-dev.hugeamp.org/api/raw/file/single_cell/$datasetId/coordinates.tsv.gz"
+                    },{
+                        "role": "expression",
+                        "url": "https://bioindex-dev.hugeamp.org/api/bio/query/single-cell-lognorm?q=$datasetId,$gene"
+                    },{
+                        "role": "markers",
+                        "url": "https://bioindex-dev.hugeamp.org/api/raw/file/single_cell/$datasetId/marker_genes.json.gz"
+                    }
+                ],
+                "components": {
+                    "cell info": {
+                        "enabled": true
+                    },
+                    "cell proportion": {
+                        "enabled": true
+                    },
+                    "gene expression": {
+                        "enabled": true
+                    },
+                    "marker genes": {
+                        "enabled": true
+                    }
+                },
+                "presets": {
+                    "layout": 0,
+                    "datasetId": "SingleCell_Emont2022_Humans_SCP1376_SN_SAT",
+                    "cell type label": "cell_type__matkp",
+                    "genes": ["PPARG", "ADIPOQ"]
+                }
+            }
         };
     },
     computed: {
