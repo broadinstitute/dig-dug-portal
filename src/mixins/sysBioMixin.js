b5486de0b2b44bbbd8e2d8763ae65a73eebe1ca4
dkjang <dkjang@broadinstitute.org>
2025-02-25 13:33:20 -0500

    temp save

--- src/mixins/sysBioMixin.js
+++ src/mixins/sysBioMixin.js
@@ -2,53 +2,16 @@ import Vue from "vue";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import PageHeader from "@/components/PageHeader.vue";
-import PageFooter from "@/components/PageFooter.vue";
-import Documentation from "@/components/Documentation.vue";
-import TooltipDocumentation from "@/components/TooltipDocumentation.vue";
-import Alert, {
-    postAlert,
-    postAlertNotice,
-    postAlertError,
-    closeAlert,
-} from "@/components/Alert";
+import sysBioNav from "@/portals/SysBio/components/sysBioNav.vue";
+import sysBioFooter from "@/portals/SysBio/components/sysBioFooter.vue";
+
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
-Vue.config.productionTip = false;
+//Vue.config.productionTip = false;
 
-export const pageMixin = {
+export const sysBioMixin = {
     components: {
-        PageHeader,
-        PageFooter,
-        Alert,
-        Documentation,
-        TooltipDocumentation,
-    },
-    computed: {
-        deployment() {
-            return process.env.NODE_ENV || "development";
-        },
-        frontContents() {
-            let contents = this.$store.state.kp4cd.frontContents;
-            if (contents.length === 0) {
-                return {};
-            }
-            return contents[0];
-        },
-
-        diseaseGroup() {
-            return this.$store.getters["bioPortal/diseaseGroup"] || {};
-        },
-    },
-    watch: {
-        diseaseGroup(group) {
-            this.$store.dispatch("kp4cd/getFrontContents", group.name);
-        },
-    },
-    methods: {
-        postAlert,
-        postAlertNotice,
-        postAlertError,
-        closeAlert,
+        sysBioNav,
+        sysBioFooter,
     },
 };
