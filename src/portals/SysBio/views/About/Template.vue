69cac00881d98c34c1e1e3554c01d72fce7b441f
dkjang <dkjang@broadinstitute.org>
2025-02-25 12:10:07 -0500

    temp save

---
+++ src/portals/SysBio/views/About/Template.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="matkp">
+      <div class="f-col fill-height">
+          <!-- NAV -->
+          <matkp-nav></matkp-nav>
+          <!-- BODY -->
+          <div
+              class="mat-body f-col"
+              style="max-width: 1400px; margin: 0 auto; width: -webkit-fill-available"
+          >
+              <template>
+              <!-- CONTENT -->
+               <h2>{{ $parent.pageContent.title }}</h2>
+               <div v-html="$parent.pageContent.body"></div>
+              </template>
+          </div>
+          <!-- FOOTER -->
+          <matkp-footer></matkp-footer>
+      </div>
+  </div>
+</template>
