236c1de4b8b8074573392979aa3e38ca41380915
Alex Shilin <shilin@broadinstitute.org>
2025-02-27 15:51:58 -0500

    framing landing page

--- src/portals/SysBio/views/Index/Template.vue
+++ src/portals/SysBio/views/Index/Template.vue
@@ -3,8 +3,161 @@
         <!-- NAV -->
         <sysbio-header></sysbio-header>
         <!-- BODY -->
-        <div class="sysbio-body">
-            Welcome.
+        <div class="sysbio-body" style="gap:70px">
+            <div class="section hero f-row grow-children align-v-center" style="gap:50px;">
+                <div class="f-col" style="gap:20px;">
+                    <div class="f-col">
+                        <div class="hero-title">SysBio FAIRplex</div>
+                        <h4>
+                            a FAIR PLatform for EXploration<br/>
+                            of Systems Biology
+                        </h4>
+                    </div>
+                    <div>
+                        Integrated multi-tissue and multi-modal data from<br/>
+                        the Accelerating Medicines Partnership<sup>®</sup> (AMP<sup>®</sup>) ecosystem<br/>
+                        to generate novel insights.</div>
+                    <a class="bold">Learn More ❯</a>
+                </div>
+                <div class="f-col" style="gap:40px; align-self:flex-end">
+                    <input class="hero-search" type="text" placeholder="Search gene">
+                    <div class="f-row">
+                        <div class="stat f-row">
+                            <div class="stat-num">08</div>
+                            <div class="stat-txt">AMP® Programs</div>
+                        </div>
+                        <div class="stat f-row">
+                            <div class="stat-num">40</div>
+                            <div class="stat-txt">Datasets</div>
+                        </div>
+                        <div class="stat f-row">
+                            <div class="stat-num">14</div>
+                            <div class="stat-txt">Tissues</div>
+                        </div>
+                        <div class="stat f-row">
+                            <div class="stat-num">23</div>
+                            <div class="stat-txt">Disease States</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section">
+                <h2>Resources</h2>
+                <div>Section description</div>
+                <div class="section-body">
+                    <div class="f-row">
+                        <div class="tabs f-col">
+                            <div class="tab f-col active">
+                                <div class="tab-title">Data Composition</div>
+                                <div class="tab-body">Distribution of samples by assay, tissue, and disease status</div>
+                            </div>
+                            <div class="tab f-col">
+                                <div class="tab-title">Gene Expression</div>
+                                <div class="tab-body">Differential expression between disease and control samples</div>
+                            </div>
+                            <div class="tab f-col">
+                                <div class="tab-title">Sample Clustering</div>
+                                <div class="tab-body">PCA variation across tissues and disease status</div>
+                            </div>
+                        </div>
+                        <div class="tab-content f-col">
+                            <!--
+                            <research-bar-in-cell-plot
+                                v-if="plotConfig['type'] == 'bar in cell plot'"
+                                :plotData="plotData"
+                                :plotMargin="plotMargin"
+                                :plotConfig="plotConfig"
+                                :canvasId="sectionId"
+                                :utils="utils"
+                            />
+                            -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section">
+                <h2>Accelerating Medicines Partnership<sup>®</sup> (AMP<sup>®</sup>) Programs</h2>
+                <div>Learn more about the AMP<sup>®</sup> Programs</div>
+                <div class="section-body">
+                    <div class="section-items f-row">
+                    <div
+                        class="section-item"
+                        v-for="item in $parent.content.amps.rows"
+                        :class="[item.type, item.comingSoon ? 'soon' : '']"
+                    >
+                        <div class="item-copy">
+                            <div class="item-title">{{ item.title }}</div>
+                            <div class="item-body" v-html="item.body"></div>
+                            <a class="item-btn" :href="item.linkUrl" style="margin-top: auto;"
+                                >{{ item.linkLabel }} ❯</a
+                            >
+                        </div>
+                        <div class="item-bg contain">
+                            <img :src="item.bgImage" />
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="section">
+                <h2>SysBio FAIRplex Consortium</h2>
+                <div>Section description</div>
+                <div class="section-body">
+                    <div class="partners" style="background: #fbfbfb">
+                        <div
+                            class="partner-logo"
+                            v-for="item in $parent.content.partners.list"
+                        >
+                            <a :href="item.url || null" target="_blank">
+                                <img :src="item.logo" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="f-row grow-children" style="gap:40px">
+                <div class="section">
+                    <h2>News</h2>
+                    <div>Section description</div>
+                    <div class="section-body">
+                        <div class="news-items" v-if="$parent.newsFeed">
+                            <div
+                                class="news-item f-row"
+                                v-for="item in $parent.newsFeed"
+                            >
+                                <div
+                                    class="news-thumbnail contain"
+                                    v-html="item.field_thumbnail_image"
+                                ></div>
+                                <div class="f-col">
+                                    <a
+                                        :href="`${$parent.content.news.newsItemUrl}${item.nid}`"
+                                        ><div class="">{{ item.title }}</div></a
+                                    >
+                                    <div class="" v-html="item.body"></div>
+                                </div>
+                            </div>
+                            <a
+                                style="align-self: flex-end"
+                                :href="`${$parent.content.news.newsUrl}`"
+                                >See All News ❯</a
+                            >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section">
+                    <h2>Releases</h2>
+                    <div>Section description</div>
+                    <div class="section-body">
+
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- FOOTER -->
         <sysbio-footer></sysbio-footer>
@@ -12,5 +165,184 @@
 </template>
 
 <style scoped>
+.hero{
+    background: #eee;
+    height: 500px;
+    margin: -40px 0 0;
+    padding: 100px;
+}
+.hero-title{
+    font-size: 2.15rem;
+    font-weight: bold;
+}
+.hero-search {
+    padding: 7px 15px;
+    border-radius: 5px;
+    border: 0;
+    box-shadow: 2px 2px 2px 0 black;
+}
+.stat{
+    gap:5px;
+}
+.stat-num {
+    font-size: 50px;
+    line-height: 35px;
+    font-weight: bold;
+}
+.stat-txt {
+    width: 70px;
+    line-height: 14px;
+    display: flex;
+    align-items: flex-end;
+    font-weight: bold;
+}
 
+.section-body{
+    padding: 20px 0 0;
+}
+.section-items {
+    display: flex;
+    gap: 20px;
+    flex-wrap: wrap;
+}
+.section-item {
+    display: flex;
+    background: #fbfbfb;
+    border: 1px solid #eee;
+    flex: 1;
+    position: relative;
+    min-width: calc(25% - 20px);
+}
+.item-copy {
+    width: -webkit-fill-available;
+    max-width: 200px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+.item-title {
+    font-weight: bold;
+    font-size: 1.1em;
+}
+.item-bg {
+    width: 100%;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    mix-blend-mode: darken;
+    overflow: hidden;
+}
+::v-deep .contain img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    padding: 7%;
+}
+
+::v-deep .cover img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.section-item.medium {
+    flex-direction: column-reverse;
+    justify-content: flex-end;
+
+    .item-copy {
+        width: -webkit-fill-available;
+        max-width: unset;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+    }
+    .item-title {
+        font-weight: bold;
+        font-size: 1.2em;
+    }
+    .item-bg {
+        width: auto;
+        height: 150px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        mix-blend-mode: darken;
+        overflow: hidden;
+    }
+}
+
+
+.partners {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
+    padding: 20px;
+    flex-wrap: wrap;
+}
+.partners-title {
+    align-self: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: #ccc;
+}
+.partner-logo {
+    height: 50px;
+    mix-blend-mode: darken;
+}
+.partner-logo img {
+    height: 50px;
+}
+
+
+.tabs {
+    width: 300px;
+    gap: 1px;
+}
+.tab {
+    padding: 20px;
+    background: #eee;
+    margin: 0 0 0 5px;
+    border-radius: 5px 0 0 5px;
+}
+.tab.active {
+    background: white;
+    box-shadow: 2px 2px 2px 0 black;
+    z-index: 1;
+    margin: 0;
+}
+.tab-content {
+    flex: 1;
+    background: white;
+    z-index: 1;
+    min-height: 100%;
+    padding: 20px;
+    border-radius: 0 5px 5px 0;
+    box-shadow: 2px 2px 2px 0 black;
+}
+.tab-title{
+    font-weight: bold;
+    font-size: 1.1em;
+}
+
+
+.news-items {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+.news-item {
+    gap: 10px;
+}
+.news-thumbnail {
+    min-width: 200px;
+    height: 100px;
+    background: #fbfbfb;
+    border: 1px solid #eee;
+}
+.news-thumbnail img {
+    mix-blend-mode: darken;
+}
 </style>
--- src/portals/SysBio/views/Index/main.js
+++ src/portals/SysBio/views/Index/main.js
@@ -6,9 +6,145 @@ import "../../css/sysbio.css";
 
 import { sysbioMixin } from "../../mixins/sysbioMixin.js";
 
+import ResearchBarInCellPlot from "@/components/researchPortal/ResearchBarInCellPlot.vue";
+
 new Vue({
     components: {
+        ResearchBarInCellPlot
     },
     mixins: [sysbioMixin],
+    data:{
+        newsFeed: null,
+        content: {
+            news: {
+                feedUrl:
+                    "https://hugeampkpncms.org/rest/news_list?project=sysbio",
+                newsUrl: "/about.html?page=news",
+                newsItemUrl: "/about.html?page=news&id=",
+            },
+            amps: {
+                title: "Tools and Workflows",
+                subtitle: "Analyses you can do on PanKbase",
+                rows: [
+                    {
+                        title: "AMP® AD Alzheimer's Disease",
+                        body: "Leveraging precision medicine to validate existing targets and discover novel targets and biomarkers.",
+                        linkLabel: "Learn More",
+                        linkUrl: "",
+                        bgImage:"",
+                        type: "medium"
+                    },
+                    {
+                        title: "AMP® ALS Amyotrophic Lateral Sclerosis",
+                        body: "Identifying biomarkers, therapeutic targets, and clinical outcome assessments for ALS.",
+                        linkLabel: "Learn More",
+                        linkUrl: "",
+                        bgImage: "",
+                        type: "medium"
+                    },
+                    {
+                        title: "AMP® AIM Autoimmune and Immune-Mediated Diseases",
+                        body: "Understanding the cellular and molecular interactions that lead to AIM.",
+                        linkLabel: "Learn More",
+                        linkUrl: "/",
+                        bgImage: "",
+                        type: "medium"
+                    },
+                    {
+                        title: "AMP® CMD Common Metabolic Diseases",
+                        body: "Identifying targets for six common metabolic diseases: liver diseases, kidney diseases, obesity, cardiovascular diseases, type 2 diabetes/prediabetes, and type 1 diabetes.",
+                        linkLabel: "Learn More",
+                        linkUrl: "/",
+                        bgImage: "",
+                        type: "medium"
+                    },
+
+                    {
+                        title: "AMP® PD Parkinson’s Disease",
+                        body: "Identifying biomarkers and therapies for Parkinson’s Disease.",
+                        linkLabel: "Learn More",
+                        linkUrl: "/",
+                        bgImage: "",
+                        type: "medium"
+                    },
+                    {
+                        title: "AMP® RA/SLE Rheumatoid Arthritis and Systemic Lupus Erythematosus",
+                        body: "Performing single cell-level analyses of tissue and blood samples from people with rheumatoid arthritis and systemic lupus erythematosus to pinpoint genes, proteins, pathways, and signaling networks. ",
+                        linkLabel: "Learn More",
+                        linkUrl: "/",
+                        bgImage: "",
+                        type: "medium"
+                    },
+                    {
+                        title: "AMP® SCZ Schizophrenia",
+                        body: "Developing effective early-stage treatments for individuals at risk for schizophrenia, understanding disease pathways, and identifying targets for treatment.",
+                        linkLabel: "Learn More",
+                        linkUrl: "/",
+                        bgImage: "",
+                        type: "medium"
+                    },
+                    {
+                        title: "AMP® HF Heart Failure",
+                        body: "Investigating heart failure with preserved ejection fraction (HFpEF) through deep phenotyping and data analysis.",
+                        linkLabel: "Learn More",
+                        linkUrl: "/",
+                        bgImage: "",
+                        type: "medium"
+                    },
+                ],
+            },
+            partners: {
+                title: "Our Partners",
+                list: [
+                    {
+                        url:"",
+                        logo:"/"
+                    },{
+                        url:"",
+                        logo:"/"
+                    },{
+                        url:"",
+                        logo:"/"
+                    },{
+                        url:"",
+                        logo:"/"
+                    },{
+                        url:"",
+                        logo:"/"
+                    },{
+                        url:"",
+                        logo:"/"
+                    },{
+                        url:"",
+                        logo:"/"
+                    },
+                ],
+            },
+        },
+    },
+    async created() {
+        this.getNews();
+    },
+    methods:{
+        async getNews() {
+            const newsFeedUrl = this.content.news.feedUrl;
+            const newsFeed = await fetch(newsFeedUrl).then((resp) => {
+                return resp.json();
+            });
+            //trim feed to 5 items
+            if (newsFeed.length > 5) newsFeed.length = 5;
+            newsFeed.forEach((item) => {
+                //extract only the img element frforom thumbnail, wysiwyg html can be polluted sometimes
+                item.field_thumbnail_image =
+                    new DOMParser()
+                        .parseFromString(
+                            item.field_thumbnail_image,
+                            "text/html"
+                        )
+                        .querySelector("img")?.outerHTML || "";
+            });
+            this.newsFeed = newsFeed;
+        },
+    },
     render: (h) => h(Template),
 }).$mount("#app");
