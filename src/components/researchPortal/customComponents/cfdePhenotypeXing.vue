1f054f684a10f3c910042e18d73d130060ffc15e
dkjang <dkjang@broadinstitute.org>
2025-03-20 16:32:31 -0400

    phenotype crossing viz updated

--- src/components/researchPortal/customComponents/cfdePhenotypeXing.vue
+++ src/components/researchPortal/customComponents/cfdePhenotypeXing.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
         <div class="phenotype-xing-legend">
-            <strong>{{ renderConfig["threshold field"] }}: </strong>
+            <strong>{{ renderConfig["x axis field"] }}: </strong>
             Max: {{ utils.Formatters.pValueFormatter(dataMax) }}, 
             <span style="color: #ffaa00">Mean: {{ utils.Formatters.pValueFormatter(dataMean) }}</span>, 
-            Min: {{ utils.Formatters.pValueFormatter(dataMin) }}  |  <strong>{{renderConfig["suppliment field"]}} <span class="value-dot" style="width:10px; height:10px; top: 5px; position:relative; display: inline-block;">&nbsp;</span> : </strong>
+            Min: {{ utils.Formatters.pValueFormatter(dataMin) }}  |  <strong>{{renderConfig["suppliment field"]}} <span class="value-dot" style="background-color: #000000; width:10px; height:10px; top: 5px; position:relative; display: inline-block;">&nbsp;</span> : </strong>
             Max: {{ utils.Formatters.pValueFormatter(suppMax) }}, 
             Mean: {{ utils.Formatters.pValueFormatter(suppMean) }}, 
             Min: {{ utils.Formatters.pValueFormatter(suppMin) }}
@@ -12,7 +12,7 @@
         <table class="cfde-xing-table">
                 <thead>
                     <tr>
-                        <th>{{ renderConfig["render by"] }}</th><th>{{renderConfig["threshold field"]}}</th><th>{{renderConfig["suppliment field"]}}</th><th>Log10({{renderConfig["threshold field"]}}) : {{renderConfig["suppliment field"]}}</th>
+                        <th>{{ renderConfig["render by"] }}</th><th>{{renderConfig["x axis field"]}}</th><th>{{renderConfig["suppliment field"]}}</th><th>Background</th><th>Log10({{renderConfig["x axis field"]}}) : {{renderConfig["suppliment field"]}}</th>
                     </tr>
                     
                 </thead>
@@ -21,20 +21,27 @@
             :class="(iIndex % 2 == 0)? 'shady-tr':''">
                 
             <td class="phenotype-td">
-               {{ item[renderConfig["render by"]] }}
+                <template v-if="!!renderConfig['link to']">
+                    <a :href="renderConfig['link to']+item[renderConfig['link field']]">{{ item[renderConfig["render by"]] }}</a>
+                </template>
+                <template v-else>
+                    {{ item[renderConfig["render by"]] }}
+                </template>
+               
 
             </td>
             <td>
-               {{utils.Formatters.pValueFormatter(item[renderConfig["threshold field"]])}}
+               {{utils.Formatters.pValueFormatter(item[renderConfig["x axis field"]])}}
 
             </td>
             <td>
                {{utils.Formatters.pValueFormatter(item[renderConfig["suppliment field"]])}}
 
             </td>
+            <td>{{ item["Background"] }}</td>
             <td class="value-td">
                <span class="mean-pos" :style="'left:' + getMeanXpos(dataMean)" ></span>
-               <span class="value-dot" :style="'left:' + getXpos(item[renderConfig['threshold field']],item[renderConfig['suppliment field']]) + 'width:'+ getDotScale(item[renderConfig['suppliment field']]) + 'px;height:'+getDotScale(item[renderConfig['suppliment field']])+'px;'">
+               <span class="value-dot" :style="'background-color:'+getBGColor(item[renderConfig['color by']])+';left:' + getXpos(item[renderConfig['x axis field']],item[renderConfig['suppliment field']]) + 'width:'+ getDotScale(item[renderConfig['suppliment field']]) + 'px;height:'+getDotScale(item[renderConfig['suppliment field']])+'px;'">
                 &nbsp;
                </span>
 
@@ -42,7 +49,7 @@
 
         </tr>
         <tr>
-            <td></td><td></td><td></td><td>
+            <td></td><td></td><td></td><td></td><td>
                 <span style="float:left; margin-left: -10px;">{{utils.Formatters.pValueFormatter(dataMin)}}</span>
                 <span style="float:right; margin-right: -10px;">{{utils.Formatters.pValueFormatter(dataMax)}}</span>
             </td>
@@ -93,15 +100,16 @@ export default Vue.component("research-cfde-xing", {
 	computed: {
 		renderData() {
             let data = [...new Set(this.data)];
+            let sortData;
 
             switch(this.renderConfig["threshold type"]) {
                 case "highest n":
 
-                    data = this.utils.sortUtils.sortArrOfObjects(data, this.renderConfig["threshold field"], "number", 'desc');
+                    data = this.utils.sortUtils.sortArrOfObjects(data, this.renderConfig["x axis field"], "number", 'desc');
 
-                    this.dataMax = data[0][this.renderConfig["threshold field"]];
-                    this.dataMin = data[data.length - 1][this.renderConfig["threshold field"]];
-                    this.dataMean = data[Math.round(data.length / 2)][this.renderConfig["threshold field"]];
+                    this.dataMax = data[0][this.renderConfig["x axis field"]];
+                    this.dataMin = data[data.length - 1][this.renderConfig["x axis field"]];
+                    this.dataMean = data[Math.round(data.length / 2)][this.renderConfig["x axis field"]];
 
                     let suppData = [...new Set(this.data)];
                     suppData = this.utils.sortUtils.sortArrOfObjects(suppData, this.renderConfig["suppliment field"], "number", 'desc');
@@ -110,18 +118,22 @@ export default Vue.component("research-cfde-xing", {
                     this.suppMin = suppData[suppData.length - 1][this.renderConfig["suppliment field"]];
                     this.suppMean = suppData[Math.round(suppData.length / 2)][this.renderConfig["suppliment field"]];
 
+                    sortData = this.utils.sortUtils.sortArrOfObjects(data, this.renderConfig["threshold field"], "number", 'desc');
 
-                    data = data.filter((i,iIndex) => iIndex < this.renderConfig["threshold"])
+                    sortData = sortData.filter((i,iIndex) => iIndex < this.renderConfig["threshold"])
                     break;
             }
             
-            return data;
+            return sortData;
         }
 	},
 	watch: {
 		
 	},
 	methods: {
+        getBGColor(GROUP) {
+            return this.renderConfig['dot colors'][GROUP];
+        },
         getXpos(VALUE,SUPP) {
 
             let xPos = 100 * (Math.log10(VALUE) - Math.log10(this.dataMin))/(Math.log10(this.dataMax) - Math.log10(this.dataMin));
@@ -172,7 +184,6 @@ export default Vue.component("research-cfde-xing", {
 
 .value-dot{
     position:absolute;
-    background-color: #ff0000;
     border-radius: 20px;
 }
 
