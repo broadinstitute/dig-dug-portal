4da01460fe947258c36093d74b1e84ce80dd7db7
Annie Moriondo <amoriond@broadinstitute.org>
2025-04-01 17:31:31 -0400

    closer

--- src/components/PigeanLocusZoom.vue
+++ src/components/PigeanLocusZoom.vue
@@ -5,13 +5,13 @@
       :renderConfig="plotConfig"
       :searchParameters="searchParameters"
       :dataComparisonConfig="dataComparisonConfig"
-      :region="$store.state.searchingRegion"
-      :plotMargin="$parent.plotMargin"
-      :compareGroupColors="$parent.colors.moderate"
-      :regionZoom="$parent.regionZoom"
-      :regionViewArea="$parent.regionViewArea"
-      :pkgData="$store.state.pkgData"
-      :pkgDataSelected="$store.state.pkgDataSelected"
+      :region="searchingRegion"
+      :plotMargin="plotMargin"
+      :compareGroupColors="colors"
+      :regionZoom="0"
+      :regionViewArea="0"
+      :pkgData="[]"
+      :pkgDataSelected="[]"
       :utils="$parent.utilsBox"
       sectionId=""
     ></research-region-plot>
@@ -20,9 +20,21 @@
 <script>
 import Vue from "vue";
 import { query } from "@/utils/bioIndexUtils";
+import uiUtils from "@/utils/uiUtils";
+import plotUtils from "@/utils/plotUtils";
+import sortUtils from "@/utils/sortUtils";
+import alertUtils from "@/utils/alertUtils";
+import Formatters from "@/utils/formatters";
+import dataConvert from "@/utils/dataConvert";
+import keyParams from "@/utils/keyParams";
+import sessionUtils from "@/utils/sessionUtils";
+import filterUtils from "@/utils/filterUtils";
+import regionUtils from "@/utils/regionUtils";
+import userUtils from "@/utils/userUtils.js";
 import ResearchRegionPlot from "@/components/researchPortal/ResearchRegionPlot.vue";
 export default Vue.component("pigean-locus-zoom", {
   components: {
+    ResearchRegionPlot
   },
   props: ["phenotype", "gene"],
   data() {
@@ -31,6 +43,15 @@ export default Vue.component("pigean-locus-zoom", {
           "type":"gem package",
           "viewers": ["region plot","genes plot"],
           "pkg id":"testPkg",
+          "ld server":{
+              "pos":"Position",
+              "ref":"ref",
+              "alt":"alt",
+              "ref variant field":"Variant ID",
+              "populations field":"P-Value",
+              "populations type":"fixed",
+              "fixed population":"ALL",
+              "populations":{"ALL":"ALL"}},
           "region viewer":{
             "x axis field":"Position",
             "y axis field":"-log10(P-Value)",
@@ -94,29 +115,86 @@ export default Vue.component("pigean-locus-zoom", {
                 "zoom":"true"
           },
         assocData: [],
+        geneData: [],
         searchParameters: {},
         dataComparisonConfig : {
           "key field":"Variant ID",
           "fields group data key":["phenotype"],
           "fields to compare":["-log10(P-Value)","P-Value","Beta","MAF","Standard Error","Z Score","LDS"]
-        }
+          },
+        searchingRegion: "",
+        plotMargin: {
+            leftMargin: 150,
+            rightMargin: 40,
+            topMargin: 20,
+            bottomMargin: 100,
+            bump: 11,
+          },
+        colors: [
+              "#007bff50",
+              "#04884550",
+              "#8490C850",
+              "#BF61A550",
+              "#EE312450",
+              "#FCD70050",
+              "#5555FF50",
+              "#7aaa1c50",
+              "#9F78AC50",
+              "#F8808450",
+              "#F5A4C750",
+              "#CEE6C150",
+              "#cccc0050",
+              "#6FC7B650",
+              "#D5A76850",
+              "#d4d4d450",
+          ],
       };
   },
   async mounted(){
     this.setSearchParams();
+    this.geneData = await this.getGeneData();
     this.assocData = await this.getAssocData();
+    this.setSearchingRegion();
     console.log(JSON.stringify(this.assocData));
   },
-  computed: {},
+  computed: {
+    utilsBox() {
+            let utils = {
+                Formatters: Formatters,
+                uiUtils: uiUtils,
+                alertUtils: alertUtils,
+                keyParams: keyParams,
+                dataConvert: dataConvert,
+                sortUtils: sortUtils,
+                plotUtils: plotUtils,
+                filterUtils: filterUtils,
+                regionUtils: regionUtils,
+                userUtils: userUtils,
+            };
+            return utils;
+        },
+  },
   methods: {
     async getAssocData() {
       console.log(`${this.phenotype} ${this.gene}`)
       return await query("associations", `${this.phenotype},${this.gene}`);
     },
+    async getGeneData(){
+      let data = await query("gene", this.gene);
+      return data[0];
+    },
     setSearchParams(){
       this.searchParameters = {
         "phenotype":{"type":"list","field":"phenotype","search":[this.phenotype]},
         "gene":{"type":"input","field":"gene","search":[this.gene]}}
+    },
+    setSearchingRegion(){
+      let border = 250000;
+      let data = this.geneData;
+      let start = data.start < border ? data.start - border : 0;
+      let end = data.end + border;
+      let region = `${data.chromosome}:${start}-${end}`;
+      this.searchingRegion = region;
     }
   },
   watch: {}
