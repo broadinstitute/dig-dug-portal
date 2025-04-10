efb8d7bf60ff0dc75963170d6a66ea6980140391
Annie Moriondo <amoriond@broadinstitute.org>
2025-04-10 15:56:13 -0400

    setting up pankbase differential expression page

---
+++ src/portals/PanKbase/views/DiffExp/Template.vue
@@ -0,0 +1,267 @@
+<template>
+  <div class="matkp">
+      <div class="f-col fill-height">
+          <!-- NAV -->
+          <matkp-nav></matkp-nav>
+          <!-- BODY -->
+          <div class="mat-body f-col">
+            <h2 class="matkp-static-content-title">Differential Gene Expression Browser</h2>
+            <div class="matkp-tool-documentation" v-if="$parent.documentation?.body" v-html="$parent.documentation.body"></div>
+              <div class="flex-column flex-small-gap">
+                    <div id="center-width" class="flex-gap flex-column">
+                        <div class="flex-gap flex-column" id="center-content">
+                            <research-single-cell-info v-if="!!$store.state.selectedDataset"
+                                :data="$parent.bulkMetadata"
+                            />
+
+                            <div v-if="$parent.dataReady" id="menu" class="flex-gap">
+                                <!--left tab group-->
+                                <div class="tabs-group">
+                                    <div class="tabs-wrapper">
+                                        <div class="tab">
+                                            Select a dataset
+                                        </div>
+                                    </div>
+                                    <div class="tabs-section-wrapper">
+                                        <div class="tab-section" >
+                                            <div  class="flex-gap">
+                                                <div class="top-block">
+                                                    <select v-model="$store.state.selectedDataset">
+                                                        <option value="">Select a dataset</option>
+                                                        <option v-for="dataset in $parent.datasets"
+                                                            :value="dataset">
+                                                            {{ dataset }}
+                                                        </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tabs-group">
+                                    <div class="tabs-wrapper">
+                                        <div class="tab">
+                                            Select a comparison
+                                        </div>
+                                    </div>
+                                    <div class="tabs-section-wrapper">
+                                        <div class="tab-section" >
+                                            <div  class="flex-gap">
+                                                <div class="top-block">
+                                                    <select v-model="$store.state.selectedComparison">
+                                                        <option value="">Select a comparison</option>
+                                                        <option v-for="comp in $parent.comparisons"
+                                                            :value="comp">
+                                                            {{ $store.state.currentComparisons[comp] }}
+                                                        </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tabs-group">
+                                    <div class="tabs-wrapper">
+                                        <div class="tab">
+                                            Select a gene
+                                        </div>
+                                    </div>
+                                    <div class="tabs-section-wrapper">
+                                        <div class="tab-section" >
+                                            <div  class="flex-gap">
+                                                <div class="top-block">
+                                                    <mouse-gene-select
+                                                        v-if="$parent.isMouse"
+                                                        @onGeneChange="gene => $parent.highlight(gene)">
+                                                    </mouse-gene-select>
+                                                    <gene-selectpicker v-else
+                                                        @onGeneChange="gene => $parent.highlight(gene)"
+                                                    ></gene-selectpicker>
+                                                </div>
+                                                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tabs-group">
+                                    <div class="tabs-wrapper">
+                                        <div class="tab">
+                                            Set -log10(FDR adj. p) threshold
+                                        </div>
+                                    </div>
+                                    <div class="tabs-section-wrapper">
+                                        <div class="tab-section" >
+                                            <div  class="flex-gap">
+                                                <div class="top-block">
+                                                    <input type="number" step="0.1"
+                                                        v-model="$parent.volcanoYCondition"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div v-if="$parent.dataReady">
+                                <div class="flex-gap">
+                                    <!--left tab group-->
+                                    <div class="tabs-group wide-group">
+                                        <div class="tabs-wrapper">
+                                            <div class="tab">
+                                                Top 20 Differentially Expressed Genes
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="tabs-section-wrapper">
+                                            <div class="tab-section" >
+                                                <div  class="flex-gap">
+                                                    <div class="wide-block">
+                                                        <div v-if="$parent.zNormData.length > 0">
+                                                            <bulk-heatmap
+                                                                :zNormData="$parent.zNormData"
+                                                                :samplesColumns="$parent.samplesColumns"
+                                                                :comparisonId="$parent.selectedComparison"
+                                                                :margin="$parent.margin"
+                                                                :sampleColors="$parent.colors"
+                                                                :plotHeight="$parent.plotHeight"
+                                                                :selectedGene="$parent.selectedGene"
+                                                                @highlight="gene => $parent.highlight(gene)"
+                                                            >
+                                                            </bulk-heatmap>
+                                                        </div>
+                                                        <div v-else>
+                                                            Select a dataset and a comparison to view the heatmap.
+                                                        </div>
+                                                    </div>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tabs-group wide-group">
+                                        <div class="tabs-wrapper">
+                                            <div class="tab">
+                                                Differentially Expressed Genes
+                                            </div>
+                                        </div>
+                                        <div class="tabs-section-wrapper">
+                                            <div class="tab-section" >
+                                                <div class="flex-gap">
+                                                    <div class="wide-block">
+                                                        <div v-if="$parent.bulkData19K.length> 0">
+                                                            <bulk-volcano-plot
+                                                                :renderData="$parent.bulkData19K"
+                                                                
+                                                                :renderConfig="$parent.volcanoConfig"
+                                                                :margin="$parent.margin"
+                                                                sectionId="_bulk"
+                                                                :selectedGene="$parent.selectedGene"
+                                                                @highlight="gene => $parent.highlight(gene)">
+
+                                                            </bulk-volcano-plot>
+                                                        </div>
+                                                        <div v-else>
+                                                            Select a dataset and a comparison to view the volcano plot.
+                                                        </div>
+                                                    </div>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>   
+                                <div id="table-wrapper" class="flex-gap flex-column">
+                                    <div class="flex-gap flex-column">
+                                        <bulk-table
+                                            :bulkData="$parent.bulkData19K"
+                                            
+                                            :dataset="$store.state.selectedDataset"
+                                            :config="$parent.tableConfig"
+                                            :scatterConfig="$parent.scatterplotConfig"
+                                            :highlightedGene="$store.state.selectedGene"
+                                            :regulationConditions="$parent.regulationConditions">
+                                        </bulk-table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div v-else>Loading...</div>
+                        </div>
+                    </div>
+                </div>
+          </div>
+          <!-- FOOTER -->
+          <matkp-footer></matkp-footer>
+      </div>
+  </div>
+</template>
+<style scoped>
+.tabs-group{
+  display:flex;
+  flex-direction: column;
+}
+.tabs-wrapper {
+  display: flex;
+  z-index: 1;
+}
+  .tab {
+      padding: 10px 10px;
+      margin: 0 -1px -1px 0;
+      background: #eee;
+      font-size: 16px;
+      flex: 1;
+  }
+  .tab:last-child {
+      margin-right: 0;
+  }
+  .tab.selected {
+      cursor: default;
+      background: #fff;
+      border-bottom: white;
+      font-weight: bold;
+  }
+  .mat-body {
+    max-width: 1400px; 
+    margin: 0 auto; 
+    width: -webkit-fill-available;
+  }
+  .top-block {
+    display:flex;
+    flex-direction: column;
+    min-width: 285px;
+  }
+  .wide-block {
+    display:flex;
+    flex-direction: column;
+    min-width: 600px;
+  }
+  .flex-gap {
+    display:flex;
+    gap:20px;
+  }
+  .flex-column {
+    flex-direction: column;
+  }
+  .tab-section {
+    display:flex;
+    flex-direction: column;
+    gap:20px;
+    background:white;
+    padding:20px;
+  }
+  #center-width {
+    width: 100%;
+  }
+  #center-content {
+    align-self:center;
+    background:#f8f8f8;
+    padding:20px;
+  }
+  #table-wrapper {
+    background:white;
+    padding:20px;
+    width:100%;
+  }
+  .flex-small-gap {
+    display:flex;
+    gap:10px; 
+  }
+  .wide-group {
+    width: 670px;
+  }
+</style>
---
+++ src/portals/PanKbase/views/DiffExp/store.js
@@ -0,0 +1,98 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import bioPortal from "@/modules/bioPortal";
+import bioIndex from "@/modules/bioIndex";
+import kp4cd from "@/modules/kp4cd";
+import keyParams from "@/utils/keyParams";
+import bioIndexUtils from "@/utils/bioIndexUtils";
+//import { BIO_INDEX_HOST } from "@/utils/bioIndexUtils";
+import dataConvert from "@/utils/dataConvert";
+
+Vue.use(Vuex);
+
+const BIO_INDEX_HOST = "https://matkp.hugeampkpnbi.org";
+
+export default new Vuex.Store({
+  modules: {
+    bioPortal,
+    kp4cd,
+    singleBulkZNorm: bioIndex("single-cell-bulk-z-norm"),
+  },
+  state: {
+    limit: 20,
+    singleBulkZNormData: [],
+    bulkData19K: [],
+    selectedDataset: keyParams.dataset || 'bulkRNA_Emont2022_Humans_SAT',
+    defaultComparison: "",
+    selectedComparison: keyParams.comparison || "",
+    selectedGene: keyParams.gene || "",
+    bulkFileUrl: `${BIO_INDEX_HOST}/api/raw/file/single_cell_bulk/`,
+    singleBulkZNormUrl: `${BIO_INDEX_HOST}/api/bio/query/single-cell-bulk-z-norm?q=`,
+    currentComparisons: {},
+  },
+
+  mutations: {
+    setSingleBulkZNormData(state, data) {
+      state.singleBulkZNormData = data || state.singleBulkZNormData;
+    },
+    setBulkData19K(state, data) {
+      state.bulkData19K = data || state.bulkData19K;
+    },
+    setCurrentComparisons(state, data) {
+      state.currentComparisons = data || state.currentComparisons;
+      state.defaultComparison = Object.keys(state.currentComparisons)[0];
+    },
+    setSelectedComparison(state, comparison) {
+      state.selectedComparison = comparison;
+    }
+
+  },
+
+  getters: {
+  },
+
+  actions: {
+    async queryBulk(context) {
+      let compQueryParam = context.state.currentComparisons[context.state.selectedComparison];
+      let singleBulkZNormObject = {};
+      console.log("compQueryParam", compQueryParam);
+      if (context.state.selectedDataset !== "") {
+        const query = `${context.state.singleBulkZNormUrl}${context.state.selectedDataset},${compQueryParam}&limit=${context.state.limit}`
+        const response = await fetch(query);
+        singleBulkZNormObject = await response.json();
+        console.log(query, response, singleBulkZNormObject);
+      }
+      context.commit("setSingleBulkZNormData", singleBulkZNormObject.data);
+      /*
+      await context.dispatch("singleBulkZNorm/query",
+        {
+          q: `${context.state.selectedDataset},${compQueryParam}`,
+          limit: context.state.limit
+        });
+      context.commit("setSingleBulkZNormData", context.state.singleBulkZNorm.data);
+      */
+    },
+    async queryBulkFile(context) {
+      let bulkDataObject = [];
+      let comparisons = {};
+      if (context.state.selectedDataset !== "") {
+        let datasetFile = `${context.state.bulkFileUrl
+          }${context.state.selectedDataset}/dea.tsv.gz`;
+        const response = await fetch(datasetFile);
+        const bulkDataText = await response.text();
+        bulkDataObject = dataConvert.tsv2Json(bulkDataText);
+        let bulkDataComparisons = bulkDataObject
+          .filter(item => !!item.comparison)
+          .map(item => [item.comparison_id, item.comparison]);
+        comparisons = Object.fromEntries(bulkDataComparisons);
+      }
+      context.commit("setBulkData19K", bulkDataObject);
+      context.commit("setCurrentComparisons", comparisons);
+    },
+    resetComparison(context) {
+      context.commit("setSelectedComparison", context.state.defaultComparison);
+    }
+
+  },
+});
