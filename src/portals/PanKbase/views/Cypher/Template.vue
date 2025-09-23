0229e4e4c2cd58cae44a52942d2dfe32e3bd4530
Annie Moriondo <amoriond@broadinstitute.org>
2025-09-23 14:55:54 -0400

    single search

--- src/portals/PanKbase/views/Cypher/Template.vue
+++ src/portals/PanKbase/views/Cypher/Template.vue
@@ -4,9 +4,23 @@
       <pkb-header></pkb-header>
       <!-- BODY -->
       <div class="pkb-body">
+        <div class="row front-search-section">
+          <div class="col-md-8 offset-md-2" align="center">
+              <div class="single-search-wrapper">
+                  <h4 style="color: #fff">
+                      Search for gene
+                  </h4>
+                  <research-single-search
+                      :single-search-config="$parent.searchConfig"
+                      :phenotypes="[]"
+                      :utils="$parent.utilsBox"
+                  ></research-single-search>
+              </div>
+          </div>
+      </div>
         <div v-if="!!$parent.jsonResults">
-          <div v-for="queryKey, index in $parent.queryKeys">
-            <strong>{{ index }}. {{ queryKey }}</strong>
+          <div v-for="queryKey, index in $parent.queryKeys" class="query-table">
+            <strong>{{ index + 1 }}. {{ $parent.tissueFormatter(queryKey) }}</strong>
             <b-table :items="$parent.jsonResults[queryKey]">
             </b-table>
           </div>
@@ -16,3 +30,9 @@
       <pkb-footer></pkb-footer> 
   </div>
 </template>
+<style scoped>
+  .query-table {
+    margin-bottom: 50px;
+    background-color: #efefef;
+  }
+</style>
--- src/portals/PanKbase/views/Cypher/main.js
+++ src/portals/PanKbase/views/Cypher/main.js
@@ -4,20 +4,45 @@ import store from "./store.js";
 import "../../assets/layout.css";
 import "../../assets/pkb-styles.css";
 import { pankbaseMixin } from "@/portals/PanKbase/mixins/pankbaseMixin.js";
+import ResearchSingleSearch from "@/components/researchPortal/ResearchSingleSearch.vue";
 import { cyphers, renderCypher, renderCypherCurl, runCypherQuery } from "../../utils/paragraph.js";
+import uiUtils from "@/utils/uiUtils";
+import plotUtils from "@/utils/plotUtils";
+import sortUtils from "@/utils/sortUtils";
+import alertUtils from "@/utils/alertUtils";
+import Formatters from "@/utils/formatters";
 import dataConvert from "@/utils/dataConvert";
 import keyParams from "@/utils/keyParams";
-import EventBus from "@/utils/eventBus";
+import regionUtils from "@/utils/regionUtils";
 
 new Vue({
     store,
     components: {
-        
+        ResearchSingleSearch,
     },
     mixins: [pankbaseMixin],
     data() {
         return {
-            jsonResults: null
+            jsonResults: null,
+            searchConfig: {
+            "search instruction": "Search for a gene",
+            "search examples": [
+                {
+                    parameter: "gene",
+                    value: "CFTR",
+                }
+            ],
+            "search parameters": [
+                {
+                    parameter: "gene",
+                    values: "kp genes",
+                    "target page": {
+                        label: "Search Gene",
+                        url: "/cypher.html?",
+                    },
+                }
+            ],
+        },
         };
     },
     watch: {},
@@ -30,9 +55,23 @@ new Vue({
         },
         queryKeys(){
             return Object.keys(cyphers);
-        }
+        },
+        utilsBox() {
+            let utils = {
+                Formatters: Formatters,
+                uiUtils: uiUtils,
+                alertUtils: alertUtils,
+                keyParams: keyParams,
+                dataConvert: dataConvert,
+                sortUtils: sortUtils,
+                plotUtils: plotUtils,
+                regionUtils: regionUtils,
+            };
+            return utils;
+        },
     },
     methods: {
+        tissueFormatter: Formatters.tissueFormatter,
         async runAllQueries(){
             let allResults = {};
             for (let i = 0; i < this.queryKeys.length; i++){
