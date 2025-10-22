5a933354ce1e071a198dc8933ec855e52a463dbb
Alex Shilin <shilin@broadinstitute.org>
2025-10-22 11:03:44 -0400

    updating new analysis names to 'cfde_reveal' and 'cfde_design', kcURL utility class

---
+++ src/utils/cfdeUtils.js
@@ -0,0 +1,25 @@
+export function kcURL(path){
+    //quality of life helper
+    //converts short kc urls from "/r/<pageid>?<query>""
+    //to "/research.html?pageid=<pageid>&<query>"" when on localhost
+
+    //example use 
+    //kcURL("/r/kc_landing")
+    //if on localhost, returns "/r/research.html?pageid=kc_landing"
+    //if on dev or prod, returns "/r/kc_landing"
+
+    const isLocalhost = window.location.hostname === 'localhost';
+
+    if (isLocalhost) {
+        // Extract pageid and query params
+        const match = path.match(/^\/r\/([^?]+)\?(.*)$/);
+        if (!match) return path;
+
+        const pageid = match[1];
+        const query = match[2];
+        return `/research.html?pageid=${pageid}&${query}`;
+    } else {
+        // Production URL
+        return path;
+    }
+}
