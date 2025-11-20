0abc0d1d14a27b9a1ddf6ae69ad47650efaa7cd2
Annie Moriondo <amoriond@broadinstitute.org>
2025-11-20 13:26:40 -0500

    fixed the word break issue

--- src/portals/PanKbase/views/AtacSeq/main.js
+++ src/portals/PanKbase/views/AtacSeq/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Template from "./Template.vue";
-import store from "./store.js";
 import "../../assets/layout.css";
 import "../../assets/pkb-styles.css";
 import "../../assets/phewas.css";
@@ -10,8 +9,6 @@ import { pankbaseMixin } from "@/portals/PanKbase/mixins/pankbaseMixin.js";
 import { getPankbaseContent } from "@/portals/PanKbase/utils/content";
 import AtacSeq from "../../components/AtacSeq.vue";
 new Vue({
-    // Based on HuGeAMP Gene page.
-    //store,
     components: {
         AtacSeq
     },
