a0c58ab3a9a4fe906657f7081bc93d4e23c73a9c
dkjang <dkjang@broadinstitute.org>
2025-12-01 14:58:14 -0500

    KP version of REVEAL added.

--- src/components/researchPortal/ResearchSectionComponents.vue
+++ src/components/researchPortal/ResearchSectionComponents.vue
@@ -19,11 +19,13 @@ import cfdeLanding from "@/components/researchPortal/customComponents/cfdeLandin
 import cfdeDesign from "@/components/researchPortal/customComponents/cfdeDesign.vue";
 import cfdeLanding2 from "@/components/researchPortal/customComponents/cfdeLanding2.vue";
 import cfdeMechanismDiscovery from "@/components/researchPortal/customComponents/cfdeMechanismDiscovery.vue";
+import kpReveal from "@/components/researchPortal/customComponents/kpReveal.vue";
 import cfdeExplore from "@/components/researchPortal/customComponents/cfdeExplore.vue";
 
 export default Vue.component("research-section-components", {
 	props: ["component", "phenotypesInUse", "utilsBox", "sectionConfigs"],
 	components: {
+		kpReveal,
 		cfdeEcoSystem,
 		cfdeLanding,
 		cfdeDesign,
@@ -46,6 +48,9 @@ console.log('this.component', this.component);
 				break;
 			case 'cfdeLanding':
 				this.currentComponent = cfdeLanding;
+				break;
+			case 'kpReveal':
+				this.currentComponent = kpReveal;
 				break;
 			case 'cfdeValidationPlanner'://remove after update
 				this.currentComponent = cfdeDesign;
