5df85b2b1d74912639e6d9afd139d06ccbc96d8d
Annie Moriondo <amoriond@broadinstitute.org>
2026-01-23 10:26:13 -0500

    new pages are created, placeholder text only

---
+++ src/portals/PanKbase/views/CellType/Template.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="pkb-wrapper f-col fill-height">
+        <!-- NAV -->
+        <pkb-header></pkb-header>
+        <!-- BODY -->
+        <div class="pkb-body">
+            <div class="card mdkp-card">
+                <div class="card-body">
+                    Cell Type: Coming Soon
+                </div>
+            </div>
+        </div>
+        <!-- FOOTER -->
+        <pkb-footer></pkb-footer>
+    </div>
+</template>
+<style scoped>
+.pkb-body {
+    max-width: 2000px;
+}
+.container {
+    display: flex;
+    justify-content: center;
+}
+.center {
+    padding: 10px;
+}
+
+/* basic positioning */
+.legend {
+    list-style: none;
+}
+.legend li {
+    float: left;
+    margin-right: 10px;
+}
+.legend span {
+    border: 0px;
+    float: left;
+    width: 12px;
+    height: 12px;
+    margin: 2px;
+}
+
+.mdkp-card {
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+.toc-item {
+    padding: 10px;
+    margin-bottom: 5px;
+    background-color: #efefef;
+    border: 1px solid #dddddd;
+    border-radius: 5px;
+}
+</style>
