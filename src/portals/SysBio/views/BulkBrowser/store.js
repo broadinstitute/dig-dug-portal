716359cdbbabdea0e8fb7a8dee398820bff56be5
Quy <quy@broad.dev>
2026-03-18 16:41:56 -0400

    temporary using gcp links for demo
    will decide after demo

--- src/portals/SysBio/views/BulkBrowser/store.js
+++ src/portals/SysBio/views/BulkBrowser/store.js
@@ -12,73 +12,73 @@ const BIO_INDEX_HOST = "https://sysbio.hugeampkpnbi.org";
 
 Vue.use(Vuex);
 
-
 export default new Vuex.Store({
-  modules: {
-    bioPortal,
-    kp4cd,
-  },
-  state: {
-    limit: 20,
-    bulkData19K: [],
-    selectedDataset: keyParams.dataset || 'sysbio_v1',
-    defaultComparison: "T001",
-    selectedComparison: keyParams.comparison || "",
-    selectedGene: keyParams.gene || "",
-    bulkFileUrl: `${BIO_INDEX_HOST}/api/raw/file/single_cell_bulk/`,
-    currentComparisons: {},
-    selectedAMP: ""
-  },
-
-  mutations: {
-    setBulkData19K(state, data) {
-      state.bulkData19K = data || state.bulkData19K;
+    modules: {
+        bioPortal,
+        kp4cd,
     },
-    setCurrentComparisons(state, data) {
-      state.currentComparisons = data || state.currentComparisons;
-      state.defaultComparison = Object.keys(state.currentComparisons)[0];
-    },
-    setSelectedComparison(state, comparison) {
-      state.selectedComparison = comparison;
-    },
-    setSelectedGene(state, gene){
-      state.selectedGene = gene;
+    state: {
+        limit: 20,
+        bulkData19K: [],
+        selectedDataset: keyParams.dataset || "sysbio_v1",
+        defaultComparison: "T001",
+        selectedComparison: keyParams.comparison || "",
+        selectedGene: keyParams.gene || "",
+        //bulkFileUrl: `${BIO_INDEX_HOST}/api/raw/file/single_cell_bulk/`,
+        bulkFileUrl: `https://storage.googleapis.com/sysbio-staging-broad-public/single_cell_bulk/`,
+        currentComparisons: {},
+        selectedAMP: "",
     },
 
-  },
-
-  getters: {
-  },
-
-  actions: {
-    async queryBulkFile(context) {
-      let bulkDataObject = [];
-      let comparisons = {};
-      if (context.state.selectedDataset !== "") {
-        let datasetFile = `${context.state.bulkFileUrl
-          }${context.state.selectedDataset}/dea.tsv.gz`;
-        const response = await fetch(datasetFile);
-        const bulkDataText = await response.text();
-        bulkDataObject = dataConvert.tsv2Json(bulkDataText);
-        const compTypeFinder = /\((\w+)\)/g;
-        bulkDataObject.forEach(b => {
-          b.absoluteFoldChange = Math.abs(b.logFoldChange)
-          let comp_id = b.comparison_id;
-          if (!comparisons.comp_id && !!b.comparison){
-            comparisons[comp_id] = {label: b.comparison};
-          }
-        });
-        for (const [k, v] of Object.entries(comparisons)){
-          let ampAndCellType = v.label.split(":");
-          v.amp = ampAndCellType[0];
-          v.cellType = ampAndCellType[1];
-        }
-      }
-      context.commit("setBulkData19K", bulkDataObject);
-      context.commit("setCurrentComparisons", comparisons);
+    mutations: {
+        setBulkData19K(state, data) {
+            state.bulkData19K = data || state.bulkData19K;
+        },
+        setCurrentComparisons(state, data) {
+            state.currentComparisons = data || state.currentComparisons;
+            state.defaultComparison = Object.keys(state.currentComparisons)[0];
+        },
+        setSelectedComparison(state, comparison) {
+            state.selectedComparison = comparison;
+        },
+        setSelectedGene(state, gene) {
+            state.selectedGene = gene;
+        },
     },
-    resetComparison(context) {
-      context.commit("setSelectedComparison", context.state.defaultComparison);
+
+    getters: {},
+
+    actions: {
+        async queryBulkFile(context) {
+            let bulkDataObject = [];
+            let comparisons = {};
+            if (context.state.selectedDataset !== "") {
+                let datasetFile = `${context.state.bulkFileUrl}${context.state.selectedDataset}/dea.tsv.gz`;
+                const response = await fetch(datasetFile);
+                const bulkDataText = await response.text();
+                bulkDataObject = dataConvert.tsv2Json(bulkDataText);
+                const compTypeFinder = /\((\w+)\)/g;
+                bulkDataObject.forEach((b) => {
+                    b.absoluteFoldChange = Math.abs(b.logFoldChange);
+                    let comp_id = b.comparison_id;
+                    if (!comparisons.comp_id && !!b.comparison) {
+                        comparisons[comp_id] = { label: b.comparison };
+                    }
+                });
+                for (const [k, v] of Object.entries(comparisons)) {
+                    let ampAndCellType = v.label.split(":");
+                    v.amp = ampAndCellType[0];
+                    v.cellType = ampAndCellType[1];
+                }
+            }
+            context.commit("setBulkData19K", bulkDataObject);
+            context.commit("setCurrentComparisons", comparisons);
+        },
+        resetComparison(context) {
+            context.commit(
+                "setSelectedComparison",
+                context.state.defaultComparison
+            );
+        },
     },
-  },
 });
--- src/portals/SysBio/views/Comparator/store.js
+++ src/portals/SysBio/views/Comparator/store.js
@@ -12,75 +12,71 @@ const BIO_INDEX_HOST = "https://sysbio.hugeampkpnbi.org";
 
 Vue.use(Vuex);
 
-
 export default new Vuex.Store({
-  modules: {
-    bioPortal,
-    kp4cd,
-  },
-  state: {
-    limit: 20,
-    bulkData19K: [],
-    selectedDataset: keyParams.dataset || 'sysbio_v1',
-    selectedComp1: keyParams.comp1 || "",
-    selectedComp2: keyParams.comp2 || "",
-    selectedGene: keyParams.gene || "",
-    bulkFileUrl: `${BIO_INDEX_HOST}/api/raw/file/single_cell_bulk/`,
-    currentComparisons: {},
-  },
-
-  mutations: {
-    setBulkData19K(state, data) {
-      state.bulkData19K = data || state.bulkData19K;
+    modules: {
+        bioPortal,
+        kp4cd,
     },
-    setCurrentComparisons(state, data) {
-      state.currentComparisons = data || state.currentComparisons;
-
-    },
-    setSelectedComp1(state, comparison) {
-      state.selectedComp1 = comparison;
-    },
-    setSelectedComp2(state, comparison) {
-      state.selectedComp2 = comparison;
-    },
-    setSelectedGene(state, gene){
-      state.selectedGene = gene;
+    state: {
+        limit: 20,
+        bulkData19K: [],
+        selectedDataset: keyParams.dataset || "sysbio_v1",
+        selectedComp1: keyParams.comp1 || "",
+        selectedComp2: keyParams.comp2 || "",
+        selectedGene: keyParams.gene || "",
+        //bulkFileUrl: `${BIO_INDEX_HOST}/api/raw/file/single_cell_bulk/`,
+        bulkFileUrl: `https://storage.googleapis.com/sysbio-staging-broad-public/single_cell_bulk/`,
+        currentComparisons: {},
     },
 
-  },
-
-  getters: {
-  },
-
-  actions: {
-    async queryBulkFile(context) {
-      let bulkDataObject = [];
-      let comparisons = {};
-      if (context.state.selectedDataset !== "") {
-        let datasetFile = `${context.state.bulkFileUrl
-          }${context.state.selectedDataset}/dea.tsv.gz`;
-        const response = await fetch(datasetFile);
-        const bulkDataText = await response.text();
-        bulkDataObject = dataConvert.tsv2Json(bulkDataText);
-        const compTypeFinder = /\((\w+)\)/g;
-        bulkDataObject.forEach(b => {
-          b.absoluteFoldChange = Math.abs(b.logFoldChange)
-          let comp_id = b.comparison_id;
-          if (!comparisons.comp_id && !!b.comparison){
-            comparisons[comp_id] = {label: b.comparison};
-          }
-        });
-      }
-      context.commit("setBulkData19K", bulkDataObject);
-      context.commit("setCurrentComparisons", comparisons);
-      context.dispatch("resetComparison");
+    mutations: {
+        setBulkData19K(state, data) {
+            state.bulkData19K = data || state.bulkData19K;
+        },
+        setCurrentComparisons(state, data) {
+            state.currentComparisons = data || state.currentComparisons;
+        },
+        setSelectedComp1(state, comparison) {
+            state.selectedComp1 = comparison;
+        },
+        setSelectedComp2(state, comparison) {
+            state.selectedComp2 = comparison;
+        },
+        setSelectedGene(state, gene) {
+            state.selectedGene = gene;
+        },
     },
-    resetComparison(context) {
-      let comps = Object.keys(context.state.currentComparisons);
-      let comp1 = comps[0];
-      let comp2 = comps[1];
-      context.commit("setSelectedComp1", comp1);
-      context.commit("setSelectedComp2", comp2);
+
+    getters: {},
+
+    actions: {
+        async queryBulkFile(context) {
+            let bulkDataObject = [];
+            let comparisons = {};
+            if (context.state.selectedDataset !== "") {
+                let datasetFile = `${context.state.bulkFileUrl}${context.state.selectedDataset}/dea.tsv.gz`;
+                const response = await fetch(datasetFile);
+                const bulkDataText = await response.text();
+                bulkDataObject = dataConvert.tsv2Json(bulkDataText);
+                const compTypeFinder = /\((\w+)\)/g;
+                bulkDataObject.forEach((b) => {
+                    b.absoluteFoldChange = Math.abs(b.logFoldChange);
+                    let comp_id = b.comparison_id;
+                    if (!comparisons.comp_id && !!b.comparison) {
+                        comparisons[comp_id] = { label: b.comparison };
+                    }
+                });
+            }
+            context.commit("setBulkData19K", bulkDataObject);
+            context.commit("setCurrentComparisons", comparisons);
+            context.dispatch("resetComparison");
+        },
+        resetComparison(context) {
+            let comps = Object.keys(context.state.currentComparisons);
+            let comp1 = comps[0];
+            let comp2 = comps[1];
+            context.commit("setSelectedComp1", comp1);
+            context.commit("setSelectedComp2", comp2);
+        },
     },
-  },
 });
