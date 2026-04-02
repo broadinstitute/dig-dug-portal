6cbaeef15996df31fd1f4de81b516ec9a0831678
Annie Moriondo <amoriond@broadinstitute.org>
2026-04-02 16:36:24 -0400

    start with raw files

--- src/portals/PanKbase/views/Functional/main.js
+++ src/portals/PanKbase/views/Functional/main.js
@@ -10,13 +10,16 @@ import { getPankbaseContent } from "@/portals/PanKbase/utils/content";
 import ResearchSingleSearch from "@/components/researchPortal/ResearchSingleSearch.vue";
 import keyParams from "@/utils/keyParams";
 import regionUtils from "@/utils/regionUtils";
-const HUGEAMP_GENE_BIOINDEX = "https://bioindex.hugeamp.org/api/bio/query/gene?q=";
+import BIO_INDEX_HOST from "@/utils/bioIndexUtils";
+const PANKBASE_BIOINDEX = BIO_INDEX_HOST.BIO_INDEX_HOST.replace("hugeamp", "pankbase");
 new Vue({
     components: {
     },
     mixins: [pankbaseMixin],
     data() {
-        return {};
+        return {
+            rawFilesLocation: `${PANKBASE_BIOINDEX}/api/raw/file/functional_data/functional_dataset_v1/`
+        };
     },
     async created() {
     },
