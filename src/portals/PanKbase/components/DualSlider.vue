7e0bd30d33d96d3a19441326270cdef4ddb70d87
Annie Moriondo <amoriond@broadinstitute.org>
2026-05-12 16:01:12 -0400

    slider functionality is working, UI needs a lot of work

---
+++ src/portals/PanKbase/components/DualSlider.vue
@@ -0,0 +1,158 @@
+<template>
+    <div class="outer-wrapper">
+        <div class="slide-container">
+            <div class="sliders-control" :id="`filter_${sliderId}`" v-if="ready">
+                <input style="padding:0;" class="slider from-slider" type="range" 
+                    :id="`filter_${sliderId}_from_slider`"
+                    :value="sliderRange.from" 
+                    :min="sliderRange.min" 
+                    :max="sliderRange.max" 
+                    :step="sliderRange.step"
+                    @input="setSliderTip($event, `filter_${sliderId}_from`)" 
+                    @change="filterDataSlider($event)"/>
+                <input style="padding:0;" class="slider to-slider" type="range" 
+                    :id="`filter_${sliderId}_to_slider`"
+                    :value="sliderRange.to" 
+                    :min="sliderRange.min" 
+                    :max="sliderRange.max" 
+                    :step="sliderRange.step"
+                    @input="setSliderTip($event, `filter_${sliderId}_to`)" 
+                    @change="filterDataSlider($event)"/>
+
+                    <output class="range-slider-tip range-from-value" 
+                    :id="`filter_${sliderId}_from`" name="rangeFromValue"
+                    >{{ Math.round(sliderRange.from * 10000) / 10000 }}</output>
+                    <output class="range-slider-tip range-to-value" 
+                    :id="`filter_${sliderId}_to`" name="rangeToValue"
+                    >{{ Math.round(sliderRange.to * 10000) / 10000 }}</output>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.component("dual-slider", {
+    props: [
+        "sliderId", "rangeMin", "rangeMax"
+    ],
+    data() {
+        return {
+            sliderRange : {
+                min: this.rangeMin,
+                max: this.rangeMax,
+                from: Math.round(this.rangeMin * 10000) / 10000,
+                to: Math.round(this.rangeMax * 10000) / 10000,
+                step: (this.rangeMax - this.rangeMin) / 10000
+            },
+            lastFilter: {},
+            filtersIndex: {},
+            ready: false
+        };
+    },
+    mounted(){
+        this.getRange();
+    },
+    computed: {
+    },
+    methods: {
+        getRange() {
+			if(!!document.getElementById(`filter_${this.sliderId}_from`)) {
+				document.getElementById(`filter_${this.sliderId}_from`).value = range.from;
+			}
+			if (!!document.getElementById(`filter_${this.sliderId}_to`)) {
+				document.getElementById(`filter_${this.sliderId}_to`).value = range.to;
+			}
+            this.ready = true;
+		},
+        setSliderTip(EVENT,ID) {
+			document.getElementById(ID).value = Math.round(EVENT.target.value * 10000) / 10000;
+		},
+		filterDataSlider(EVENT) {
+			let searchValueFrom = document.getElementById(`filter_${this.sliderId}_from`).value
+            let searchValueTo = document.getElementById(`filter_${this.sliderId}_to`).value
+            this.sliderRange.from = Number(searchValueFrom);
+            this.sliderRange.to = Number(searchValueTo);
+            
+            let searchRange = [searchValueFrom, searchValueTo];
+            this.$emit("filterChanged", searchRange);
+		},
+    },
+});
+</script>
+<style scoped>
+/* slider UI from DK's BYOR slider*/
+.slide-container {
+  width: 175px;
+    position: relative;
+    margin: 10px;
+}
+
+.sliders-control {
+    position: relative;
+    min-height: 30px;
+    border-top: solid 2px #cccccc;
+    margin-top: 10px;
+    margin-bottom: -10px;
+}
+
+.slider {
+	position: absolute;
+	left: 0;
+	top: 0px;
+  -webkit-appearance: none;
+  width: 100%;
+  height: 2px !important;
+  background: #d3d3d300;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+  border-radius: 5px;
+  z-index:1;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 10px;
+  height: 10px;
+  margin-top: -10px;
+  background: #666666;
+  border-radius: 15px;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 10px;
+  height: 10px;
+  margin-top: -10px;
+  background: #666666;
+  border-radius: 15px;
+  cursor: pointer;
+}
+
+.range-slider-tip {
+	position: absolute;
+	top: 5px;
+	font-size: 12px;
+}
+
+.range-from-value {
+	left: 0;
+}
+
+.range-to-value {
+	right: 0;
+}
+.outer-wrapper {
+    display: flex;
+    padding: 5px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    border: 1px solid lightgray;
+}
+</style>
---
+++ src/portals/PanKbase/components/FilterSlider.vue
@@ -0,0 +1,50 @@
+<template>
+  <filter-slider-template
+    class="filter-col-md"
+    :field="field"
+    :rangeMin="rangeMin"
+    :rangeMax="rangeMax"
+    :placeholder="placeholder"
+    :predicate="predicate"
+    :pillFormatter="pillFormatter"
+    @input-change="$emit('input-change', $event)"
+    :color="color"
+    :multiple="false"
+    :inclusive="false"
+    :splitBy="splitBy"
+    :computedField="computedField"
+  >
+    <slot> </slot>
+  </filter-slider-template>
+</template>
+<script>
+import Vue from "vue";
+import FilterSliderTemplate from "./FilterSliderTemplate";
+export default Vue.component("filter-slider", {
+  props: {
+    field: String,
+    placeholder: String,
+    color: String,
+    rangeMin: Number,
+    rangeMax: Number,
+    splitBy: String,
+    inclusive: {
+      type: Boolean,
+      default: true,
+    },
+    predicate: {
+      type: Function,
+      default: (item, thresholdArray) => item >= thresholdArray[0] && item <= thresholdArray[1],
+    },
+    pillFormatter: {
+      type: Function,
+      default: (filterDefinition) =>
+        `${filterDefinition.field} = ${filterDefinition.threshold}`,
+    },
+    computedField: Function,
+  },
+  components: {
+    FilterSliderTemplate,
+  },
+});
+</script>
